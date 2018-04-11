ddd4d37e6d3e8765e3364e791b2a34829f20bd79
MfonobongD <umondiamfonobong@gmail.com>
2018-04-12 01:50:02 +0200

    added testimonies to Hngfun

--- testimonies.php
+++ testimonies.php
@@ -22,9 +22,15 @@ include_once("header.php");
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+          <p>Bella - I learnt how to implement my design using HTML/CSS and displaying time using PHP. I also learnt remote collaboration, mentoring people remotely to achieve results and working with other team members remotely. Its just awesome.</p>
+          <p>Abigail - One of the most intriguing things about the hng internship for me is the ‘forced mentorship’. You have to help others make substantial progress before you can get stage promotion. I have learnt patience from this. I am learning to not be presumptuous (like assuming everyone is working with a laptop) I am learning to communicate one idea in four, five even six different ways based on whom I am speaking to. I am unlearning and relearning.</p>
+          <p>TechHajiya- It's been a great opportunity working with different people and using various tools in accomplishing the tasks given. Thanks to hng internship I can now use Figma for designs and TeamViewer also. Looking forward to more of such experiences.</p>
+          <p>Oluchi- HNG Internship has been going well so far. Worked with figma for the first time. Heard about pivotaltracker for the first time too and remote team collaboration. Sweet!</p>
+          <p>Iam__dharmy - This internship has been WOW,  I've been able to greatly improve my html/css and design skills, and I now know how to use github. Thanks to hng for this platform.</p>
+          <p>Rachel - Wrote my first line of code in php and that felt good, got a hang of how figma works, been using other design tools, but figma is really awesome.</p>
+          <p>Fotes - Phew!!! Learnt how to ask specific questions. Thanks to all who have helped me thus far</p>
+          <p>Interactive_bee -  I learnt how to push codes to github for the first time. I'm now using that knowledge to push other codes i had left vulnerable. I actually coded with php for the first time.... and did something practical with html/css. it is just amazing.</p>
+          <p>Didicodes - I learnt how to design a web page with Figma, implemented the designs with HTML,CSS and PHP. I used TeamViewer for the first time yesterday and now I just had a better understanding about Git. Isn't this internship just amazing. I cant wait to learn more.</p>
         </div>
       </div>
     </div>
