9655d422cc7a94f4d48c0ce3d6b114ef339dc4dd
Emmanuel Joseph <jekayode@live.com>
2018-04-11 23:57:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hng.fun");
+define ('DB_USER', "tkhubcyuzm");
+define ('DB_PASSWORD', "arKBk9TbQr");
+define ('DB_DATABASE', "tkhubcyuzm");
 define ('DB_HOST', "localhost");
 
 ?>
