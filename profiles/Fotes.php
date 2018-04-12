2328820fab128cda7e0892355d1f4d156cd8b14e
Tolueek <ogunmoyelatoluwalope@yahoo.com>
2018-04-12 09:41:32 +0100

    Fotes-Tolueek

---
+++ profiles/Fotes.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+    <title>HNG INTERNSHIP #1</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="design.css">
+                <link rel="icon" href="worldtime.jpg" type="image/png" sizes="16x16">
+		
+    </head>
+
+<body>
+<h1>HNG Internship 4.0</h1>
+ 
+ <div class="time">
+ <?php date_default_timezone_set("Africa/Lagos");
+   echo 'The future is now_'  . date("h:i:sa") ;
+   ?>
+</div>
+</body>
+</html>
