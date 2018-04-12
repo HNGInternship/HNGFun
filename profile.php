72db0339ce9f7a021a4be31a83015868012fc2bb
Melody Okunuga <mokunuga@gmail.com>
2018-04-12 07:34:53 +0100

    profile changes

--- profile.php
+++ profile.php
@@ -5,7 +5,10 @@ include_once("header.php");
 $profile_name = $_GET['id'];
 ?>
 <!-- Page Content -->
+<div class="container">
     <?php include_once('profiles/' . $profile_name. '.php'); ?>
+
+</div>
 
 <!-- Footer -->
 <?php
