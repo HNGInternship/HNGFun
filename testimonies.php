e0959e3a81a33d4be6e061fa061043e98b3aba46
iam-dharmy <soyombooluwadamilola@gmail.com>
2018-04-12 02:40:23 +0100

    made necessary inputs

--- testimonies.php
+++ testimonies.php
@@ -11,7 +11,7 @@ include_once("header.php");
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="page-heading">
               <h1>Testimonies</h1>
-              <span class="subheading">This is what I do.</span>
+              <span class="subheading">Hear what the interns say.</span>
             </div>
           </div>
         </div>
@@ -19,21 +19,103 @@ include_once("header.php");
     </header>
 
     <!-- Main Content -->
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <p>Bella - I learnt how to implement my design using HTML/CSS and displaying time using PHP. I also learnt remote collaboration, mentoring people remotely to achieve results and working with other team members remotely. Its just awesome.</p>
-          <p>Abigail - One of the most intriguing things about the hng internship for me is the ‘forced mentorship’. You have to help others make substantial progress before you can get stage promotion. I have learnt patience from this. I am learning to not be presumptuous (like assuming everyone is working with a laptop) I am learning to communicate one idea in four, five even six different ways based on whom I am speaking to. I am unlearning and relearning.</p>
-          <p>TechHajiya- It's been a great opportunity working with different people and using various tools in accomplishing the tasks given. Thanks to hng internship I can now use Figma for designs and TeamViewer also. Looking forward to more of such experiences.</p>
-          <p>Oluchi- HNG Internship has been going well so far. Worked with figma for the first time. Heard about pivotaltracker for the first time too and remote team collaboration. Sweet!</p>
-          <p>Iam__dharmy - This internship has been WOW,  I've been able to greatly improve my html/css and design skills, and I now know how to use github. Thanks to hng for this platform.</p>
-          <p>Rachel - Wrote my first line of code in php and that felt good, got a hang of how figma works, been using other design tools, but figma is really awesome.</p>
-          <p>Fotes - Phew!!! Learnt how to ask specific questions. Thanks to all who have helped me thus far</p>
-          <p>Interactive_bee -  I learnt how to push codes to github for the first time. I'm now using that knowledge to push other codes i had left vulnerable. I actually coded with php for the first time.... and did something practical with html/css. it is just amazing.</p>
-          <p>Didicodes - I learnt how to design a web page with Figma, implemented the designs with HTML,CSS and PHP. I used TeamViewer for the first time yesterday and now I just had a better understanding about Git. Isn't this internship just amazing. I cant wait to learn more.</p>
+     <div class="container marketing">
+	  
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="images/Didicodes.png" alt="Didicodes image" width="140" height="140">
+            <h2>Didicodes</h2>
+            <p>I learnt how to design a web page with Figma, implemented the designs with HTML,CSS and PHP. I used TeamViewer for the first time yesterday and now I just had a better understanding about Git. Isn't this internship just amazing. I can't wait to learn more</p>
+          </div><!-- /.col-lg-4 -->
+		  
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="images/iam__dharmy.png" alt="iam__dharmy image" width="140" height="140">
+            <h2>iam__dharmy</h2>
+            <p>This internship has been WOW,  I've been able to greatly improve my html/css and design skills, and I now know how to use github.</p>
+          </div><!-- /.col-lg-4 -->
+		  
+		   <div class="col-lg-4">
+            <img class="rounded-circle" src="images/Osawaru.png" alt="Osawaru image" width="140" height="140">
+            <h2>Osawaru</h2>
+            <p> I got to work with Figma for first time. As a designer, this is definitely a nice tool to add to my toolbox. Been a challenging and good experience so far. looking forward to more.
+			</p>
+          </div><!-- /.col-lg-4 -->
+		  
+		  <div class="col-lg-4">
+            <img class="rounded-circle" src="images/Dtechtor.png" alt="Dtechtor image" width="140" height="140">
+            <h2>D'techtor</h2>
+            <p> I wrote my first php code and I've had github but never used it or bothered to learn, thanks to this internship for the push out of my comfort zone, now I can make use of github and implement a php file.
+
+			</p>
+          </div><!-- /.col-lg-4  -->
+		  
+		  <div class="col-lg-4">
+            <img class="rounded-circle" src="images/TechHajiya.png" alt="TechHajiya image" width="140" height="140">
+            <h2>TechHajiya</h2>
+            <p> It's been a great opportunity working with different people and using various tools in accomplishing the tasks given. Thanks to hng internship I can now use Figma for designs and TeamViewer also. Looking forward to more of such experiences.
+
+			</p>
+          </div><!-- /.col-lg-4 -->
+		  
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="images/Fotes.png" alt="Fotes image" width="140" height="140">
+            <h2>Fotes</h2>
+            <p>Phew!!! Learnt how to ask specific questions. Thanks to all who have helped me thus far </p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+		
+ <main role="main">
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>interactive_bee</h1>
+                <p>It is getting more interesting... I learnt how to push codes to github for the first time. I'm now using that knowledge to push other codes i had left vulnerable. 
+				I actually coded with php for the first time.... and did something practical with html/css. it is just amazing. </p>
+                <p><a class="btn btn-lg btn-primary" href="learn.php" role="button">ready to learn more!</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+               <h1>Adetutu</h1>
+                <p>I haven't heard about Figma until this Internship started. I've playing around with designs since then. Also, I'm learning the hows and whats of GitHub.
+					I'm having an amazing time</p>
+                <p><a class="btn btn-lg btn-primary" href="learn.php" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>charlek</h1>
+                <p>Definitely took me out of my comfort zone. Was wonderful to learn how to use Docker. I liked designing using figma. Wish I had known about it previously</p>
+                <p><a class="btn btn-lg btn-primary" href="learn.php" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
         </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-    </div>
 
     <hr>
 
