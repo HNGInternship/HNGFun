1622b992786aefbd1b0a883010dd17ddb79a86b3
Melody Okunuga <mokunuga@gmail.com>
2018-04-12 07:28:11 +0100

    profile changes

--- admin.php
+++ admin.php
@@ -123,7 +123,8 @@ if(isset($_POST['submit']) && isset($_FILES["file"]["type"])){
     }?>
 
     <?php if($success) {
-        echo "<div class='alert alert-success'>Successful! Click <a href='profiles/". $username . ".php'>here</a> to view your profile</div>";
+        echo "<div class='alert alert-success'>Successful! Click <a href='profile.php?id=". $username . "'>
+          <button class='btn btn-success'>Here</button></a> to view your profile</div>";
     }?>
 
     <form action="admin.php" method="post" enctype="multipart/form-data">
