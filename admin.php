dd99927dd3fab4ffc66ef736ded9d44fc0247f9e
mercyikpe <ikpemercy1@gmail.com>
2018-04-13 12:08:01 +0100

    Update on filetype

--- admin.php
+++ admin.php
@@ -95,7 +95,7 @@ if(isset($_POST['submit']) && isset($_FILES["file"]["type"])){
             //Insert Data
             $name = $_POST['name'];
             $username = $_POST['username'];
-            $imageName = $_FILES['file']['name'];
+            $mageName  =  $_POST['image_filename'];
 
             $intern_data = array(':name' => $name,
                 ':username' => $username,
