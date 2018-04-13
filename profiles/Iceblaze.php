5aa6c8db94feef72e7e7de0b679444138c5050f8
black-fyre <dahunsi.fehinti@gmail.com>
2018-04-13 14:10:03 +0000

    Modified footer

--- profiles/Iceblaze.php
+++ profiles/Iceblaze.php
@@ -1,57 +1,107 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Dahunsi Fehintoluwa</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<style>
-		body {
-			background-image: url("https://res.cloudinary.com/iceblaze/image/upload/v1523624386/DSC_1101.jpg");
-			background-size: cover;
-		}
-		
-		.fa:hover {
-    		color: #536DFE;
-		}
+    <link href='https://fonts.googleapis.com/css?family=Alegreya|Allura|Almendra SC|Romanesco' rel='stylesheet'>
+    <style type="text/css">
+        .container{
+            width: 100%;
+            min-height: 100%
+        }
+        html, body {
+            height: 100%;
+            background-image: url('https://wallup.net/wp-content/uploads/2016/01/155420-water-dark-calm-nature.jpg');
+            background-repeat:no-repeat;
+            background-size:cover;
+            margin: 0px;
+        }
 
-		.fa {
-			float: right;
-			font-size: 25px;
-			color: #ccc;
-			padding: 10px;
-		}
-			
-		#clock {
-			float: right;
-			font-size: 4em;
-			display: inline;
-			color: #ccc;
-			text-align: center;
-			margin: 150px 10px 0 0;
-		}	
-	</style>
+        span {
+            display: inline-block;
+            vertical-align: middle;
+            line-height: normal;
+        }
+
+        .main {
+            position: absolute;
+            top:20px;
+            width: 100%;
+            padding-top: 300px;
+            height: 130px;
+            font-family: "Romanesco";
+            line-height: 130px;
+            font-size: 96px;
+            text-align: center;
+        }
+        .text {
+            background: -webkit-linear-gradient(0deg, #FF0F00, rgba(17, 26, 240, 0.55), #EC0F13);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        .under {
+            position: absolute;
+            top:450px;
+            height: 50px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .under1 {
+            position: absolute;
+            top:500px;
+            height: 40px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .under2 {
+            position: absolute;
+            top:540px;
+            height: 49.71px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 313.66px;
+            height: 48.63px;
+            padding-bottom: 30px;
+            font-family: "Almendra SC";
+            line-height: normal;
+            font-size: 24px;
+            text-align: center;
+            color: #7ED7F9;
+        }
+    </style>
 </head>
-
 <body>
+<div class="container">
+    <div class="offset-md-3 col-md-6">
+        <div class="col-md-2">
+        </div>
+        <img class="img-fluid rounded" style="padding-top: 10px" onerror="this.src='images/default.jpg'" src="https://res.cloudinary.com/iceblaze/image/upload/v1523624386/DSC_1101.jpg" >
+        <div class="main"><span class="text">Dahunsi Fehintoluwa</span></div>
+        <div class="under"><span>Full Stack Web Developer</span></div>
+        
+        <div class="under2"><span>Lagos | NG</span></div>
+
+    </div>
 
 
-	<header>
-			<div>
-				<a href="https://github.com/black-fyre"><i class="fa fa-github"></i></i></a>
-				<a href="https://twitter.com/#"><i class="fa fa-twitter"></i></i></a>
-				<a href="https://medium.com/#"><i class="fa fa-medium"></i></i></a>
-				<a href="https://web.facebook.com/dahunsi.fehinti"><i class="fa fa-facebook"></i></i></a>	
-			</div>
-		
-	</header>
-    
-	<p id="clock">
-		<?php
-			echo "The time is </br>" . date("h:i:sa");
-		?> 
-	</p>
+
+</div>
 
 </body>
 
+</html>
