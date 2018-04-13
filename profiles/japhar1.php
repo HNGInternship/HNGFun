e39db86d7282c2fe28872c2707f096dde98f668e
japhar <33649289+japhar1@users.noreply.github.com>
2018-04-13 18:15:39 +0100

    Update japhar1.php

--- profiles/japhar1.php
+++ profiles/japhar1.php
@@ -6,32 +6,18 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <style type="text/css">
-        	* {
+   /*     	* {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
+}*/
 #bodypage {
     font-family: 'Varela Round', sans-serif;
     font-size: 100%;
-}
-#mainbox {
-    background: url("https://res.cloudinary.com/dnx217f5z/image/upload/v1523182907/pc-1207834_1280.jpg") no-repeat;
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 0;
-    background-position: 50% 50%;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
+    background: url("https://res.cloudinary.com/dnx217f5z/image/upload/v1523182907/pc-1207834_1280.jpg");
 }
 #profileavatar img {
 	width: 20%;
@@ -48,7 +34,7 @@
 #innercontent p{
     text-align: center;
     line-height: 2em;
-    color: #fff;
+    color: #8E143D;
     font-size: 3.5em;
     font-weight: 600;
 }
@@ -57,22 +43,20 @@
 	</head>
 
 	<body id="bodypage">
-        <div id="mainbox">
         	<figure id="profileavatar">
         		<img class="img-responsive text-center" src="https://res.cloudinary.com/dnx217f5z/image/upload/v1523621896/IMG-20180410-WA0000.jpg">
         	</figure>
             <div id="innercontent">
-                <p>User - @japhar1 | Name: Balogun Olusegun</p>
-                <p>HNG Internship 4.0 - Web Dev Intern</p>
+                <p>Balogun Olusegun | @japhar1</p>
+                <p>HNG 4.0 Intern | Web/Front-End Dev</p>
+                <p><a href="https://www.facebook.com/balowtelly"><span class="fa fa-facebook"></span></a> | <a href="#"><span class="fa fa-instagram"></span></a> | <a href="https//www.twitter.com/balo_telly"><span class="fa fa-twitter"></a> </p>
                 <?php
                 $time = time();
                 $actual_time = date('F jS Y h:i:s a' );
                 ?>
                <p><?php //echo $actual_time;?></p>
                <p id="date_time"></p>
-                <p>Task - Stage 4</p>
             </div>
-        </div>
        <script type="text/javascript" src="https://cdn.rawgit.com/japhar1/japhar1.github.io/95e8eb17/date_time.js"></script>
        <script type="text/javascript">window.onload = date_time('date_time');</script>
 	</body>
