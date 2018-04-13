d733874714d7cd5a8d3c2abf1dff9c0a2d3dc5a5
Melody Okunuga <mokunuga@gmail.com>
2018-04-13 21:15:48 +0100

    Profile changes

--- profiles/melody.php
+++ profiles/melody.php
@@ -106,22 +106,16 @@
     <?php
 
 
-//    try {
-//        $sql = 'SELECT * FROM interns_data';
-//        $q = $conn->query($sql);
-//        $q->setFetchMode(PDO::FETCH_ASSOC);
-//        $data = $q->fetchAll();
-//    } catch (PDOException $e) {
-//
-//        var_dump($e->getMessage());
-//        throw $e;
-//    }
-//
-//
-//    var_dump($data);
+    try {
+        $sql = 'SELECT * FROM secret_word';
+        $q = $conn->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $q->fetchAll();
+    } catch (PDOException $e) {
+        throw $e;
+    }
+    $secret_word = $data['secret_word'];
     ?>
-
-
 
 </div>
 
