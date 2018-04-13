efa363cd1e280e5d09ebbb098ef45645226a0b08
Melody Okunuga <mokunuga@gmail.com>
2018-04-13 18:56:14 +0100

    Config changes

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
 
 define ('DB_USER', "root");
+
 //define ('DB_PASSWORD', "");
 //define ('DB_DATABASE', "hng.fun");
 define ('DB_PASSWORD', "!TB%7L6QHPbhTcWq");
 define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
+
 ?>
