2bd4383f0f529ea64c5e2640874ee1262c2b928a
johnayeni <john.ayeni@stu.cu.edu.ng>
2018-04-13 10:11:02 +0100

    edit my profile

--- profiles/johnayeni.php
+++ profiles/johnayeni.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Mbah Clinton</title>
+  <title>John Ayeni</title>
   <meta name="theme-color" content="#2f3061">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One|Ubuntu" rel="stylesheet">
