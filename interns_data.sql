3859e0467ed99e25388e62147b42502d6a38cf82
Damilare Daniel <danieldamilare18@gmail.com>
2018-04-13 14:35:14 +0100

    created DDan.php and added to profiles

--- interns_data.sql
+++ interns_data.sql
@@ -1,23 +1,68 @@
--- Database Manager 4.2.5 dump
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2018 at 03:19 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS `interns_data`;
-CREATE TABLE `interns_data` (
-  `intern_id` int(10) NOT NULL AUTO_INCREMENT,
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `interns_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interns_data`
+--
+
+CREATE TABLE IF NOT EXISTS `interns_data` (
+`intern_id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `image_filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`intern_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image_filename` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `interns_data`
+--
 
 INSERT INTO `interns_data` (`intern_id`, `name`, `username`, `image_filename`) VALUES
-(9,	'Okunuga melody',	'melody',	'Screenshot-2018-3-21 Twitter.png'),
-(11,	'ikpe mercy michael',	'mercyikpe',	'IMG-20180322-WA0010.jpg'),
+(9, 'Okunuga melody', 'melody', 'Screenshot-2018-3-21 Twitter.png'),
+(11, 'ikpe mercy michael', 'mercyikpe', 'IMG-20180322-WA0010.jpg'),
 (12, 'Mbah Clinton', 'mclint_', 'mclint_.jpg'),
-(13, 'John Ayeni', 'johnayeni', 'johnayeni.jpg');
+(13, 'John Ayeni', 'johnayeni', 'johnayeni.jpg'),
+(14, 'Solanke Damilare', 'D.dan', 'https://res.cloudinary.com/damilare1957/image/upload/v1523622655/dan.jpg');
 
--- 2018-04-13 06:08:02
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `interns_data`
+--
+ALTER TABLE `interns_data`
+ ADD PRIMARY KEY (`intern_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `interns_data`
+--
+ALTER TABLE `interns_data`
+MODIFY `intern_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
