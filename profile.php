df29d68545f1ff462808bf64a48186ed53955beb
Melody Okunuga <mokunuga@gmail.com>
2018-04-13 21:11:47 +0100

    Profile changes

--- profile.php
+++ profile.php
@@ -4,37 +4,32 @@ include_once("header.php");
 
 $profile_name = $_GET['id'];
 
-//require 'db.php';
-//
-//$sql = 'SELECT * FROM interns_data';
-//$q = $conn->query($sql);
-//$q->setFetchMode(PDO::FETCH_ASSOC);
-//$data = $q->fetchAll();
-//
-//var_dump($data);
-
-?>
-
-<?php
 require 'db.php';
-require 'profiles/' . $profile_name. '.php';
 
-try {
-    $sql = 'SELECT * FROM secret_word';
-    $q = $conn->query($sql);
-    $q->setFetchMode(PDO::FETCH_ASSOC);
-    $data = $q->fetch();
-} catch (PDOException $e) {
-
-    throw $e;
-}
 ?>
 <!-- Page Content -->
-<body class = 'profile'>
-<div class="container">
-    <?php if(!isset($secret_word) || $secret_word != $data['secret_word']) { echo "<div class='alert alert-danger'>Secret key mismatch. Insert your secret key</div>"; }?>
-    <?php include_once('profiles/' . $profile_name. '.php'); ?>
+<div class='alert alert-danger ' id="secret">Secret key mismatch. Insert your secret key</div>
 </div>
+<body class = 'profile'>
+
+<div class="container">
+    <?php include_once('profiles/' . $profile_name. '.php');
+    try {
+        $sql = 'SELECT * FROM secret_word';
+        $q = $conn->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $q->fetch();
+    } catch (PDOException $e) {
+
+        throw $e;
+    }?>
+</div>
+<?php if(!isset($secret_word) || $secret_word != $data['secret_word']) { ?>
+    <script type="text/javascript">document.getElementById('secret').style.display = 'block';</script>
+<?php } else {  ?>
+    <script type="text/javascript">document.getElementById('secret').style.display = 'none';</script>
+<?php }?>
+
 </body>
 
 
