60191fc1d7bfebbd801009ba28ef5937a7c1c7ed
iamjosh247 <starboi247@gmail.com>
2018-04-13 22:32:04 +0100

    Added my profile page

---
+++ profiles/joat.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>HNG FUN</title>
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Custom styles for this template -->
+      <link href="../css/style2.css" rel="stylesheet">
+      <link href="../css/style1.css" rel="stylesheet">
+      <link href="../css/style.css" rel="stylesheet">
+	  <link href="../css/carousel.css" rel="stylesheet">
+	<style type="text/css">
+		a:hover{
+		  text-decoration: none;
+		}
+	</style>
+</head>
+<body style='background-color:#3f4447;'>
+	 <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">HNG FUN</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../learn.php">Learn</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../listing.php">Interns</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../admin.php">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../testimonies.php">Testimonies</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+<div class='container'>
+	<br><br>
+	<div class='col-sm-6' >
+		<center><img height='100%' class='img-responsive' src="http://res.cloudinary.com/dlsfrelbb/image/upload/v1523639668/josh.png"></center>
+	</div>
+	<div class='col-sm-6'>
+		<div>
+			<br><br><br><br><br><br>
+			<h1 style='font-family: "proxima-nova"; color:#fff; font-size: 22px; font-weight: 600; letter-spacing: .14em; line-height: 1em;'>JOSHUA ROBINSON.</h1>
+			<h2 style='font-family: "proxima-nova"; color:#a3a3a3; font-size: 22px; line-height: 1.15em; text-transform: none;letter-spacing: .01em; margin-bottom:26px;'>I am a website devloper, android app devloper ,an animator and also love gaimg. Follow me anywhere.</h2>
+			<h2><a href="mailto:starboi247@gmail.com" style='font-family: "proxima-nova"; color:#fff; font-size: 22px; font-weight: 600; letter-spacing: .14em; line-height: 1em;'>STARBOI247@GMAIL.COM</a></h2>
+			<h2><a href="http://itsjoat.com" style='font-family: "proxima-nova"; color:#fff; font-size: 22px; font-weight: 600; letter-spacing: .14em; line-height: 1em;'>WEBSITE</a></h2>
+			<div>
+				<a href="https://instagram.com/its_joat"><img style='margin-right:10px;' width='40' height='40' src="http://res.cloudinary.com/dlsfrelbb/image/upload/v1523643336/instagram-icon-white.png"></a>
+				<a href="https://twitter.com/its_joat"><img style='margin-right:10px;' width='40' height='40' src="http://res.cloudinary.com/dlsfrelbb/image/upload/v1523643336/icon-twitter-white-1.png"></a>
+				<a href="https://m.youtube.com/channel/UCvLacR6r37O6N_dWEXDGUyQ"><img width='40' height='40' src="http://res.cloudinary.com/dlsfrelbb/image/upload/v1523643336/white-youtube-2-512.png"></a>
+			</div>
+		</div>
+	</div>
+</div>
+<br><br><br><br><br><br>
+
+<!-- Custom scripts for this template -->
+    <script src="../js/hng.min.js"></script>
+</body>
+</html>
