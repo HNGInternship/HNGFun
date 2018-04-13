9c9fedcc5cbb060624caad65951795354c35db4a
adeteju <adeleyeadeteju@yahoo.com>
2018-04-13 17:24:47 +0100

    adeteju.php
    I have added my file

---
+++ profiles/adeteju.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title></title>
+<style>
+h2 {
+    border-style: solid;
+    border-color: coral;}
+
+</style>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body style ="background-color: 87ceeb;}" >
+<div class="jumbotron jumbotron-fluid" style= "jumbotron{ color: red;}">
+  <div class="container">
+    <h1 class="display-4">Hi Guys!</h1>
+    <p class="lead">This is a summary of my profile and my skills</p>
+  </div>
+</div>
+<div class = "col-md-4">
+<h2> PROFILE</h2>
+<h3>My Name is Adeteju Adeleye </h4>
+</div>
+<div class = "col-md-4">
+<h2> SKILLS</h2>
+<h3> Software Development </h3>
+</div>
+<div class = "col-md-4" >
+<h2> CONTACT </h2>
+<h3> Slack: @adeteju </h3>
+</div>
+
+</body>
+</html>
