0f1ae498809c40986d2b97932d12f8f03d74954b
Justice Otuya <jotuya2@gmail.com>
2018-04-13 10:58:00 +0100

    added Justice Otuya profile

---
+++ profiles/justiceotuya.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Justice Otuya</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style>
+		body {
+			background-image: url("http://res.cloudinary.com/justiceotuya/image/upload/v1523611901/IMG_2183.jpg");
+			background-size: cover;
+		}
+
+		.fa:hover {
+			color: #536DFE;
+		}
+
+		.fa {
+			float: left;
+			font-size: 25px;
+			color: #000;
+			padding: 10px;
+		}
+
+		header {
+			margin: 200px;
+			float: right;
+		}
+
+		#name {
+			font-family: cursive;
+			font-size: 44px;
+		}
+	</style>
+</head>
+
+<body>
+
+
+	<header>
+		<div>
+			<h1 id="name">OTUYA JUSTICE</h1>
+			<a href="https://github.com/justiceotuya">
+				<i class="fa fa-github"></i>
+				</i>
+			</a>
+			<a href="https://twitter.com/justiceotuya">
+				<i class="fa fa-twitter"></i>
+				</i>
+			</a>
+			<a href="https://facebook.com/justiceotuya">
+				<i class="fa fa-facebook"></i>
+				</i>
+			</a>
+		</div>
+
+	</header>
+
+
+
+</body>
