e475fea87f5f2574e1f9f91756b8e3b41192006d
Emmanuel Joseph <jekayode@live.com>
2018-04-13 07:08:57 +0100

    Imported latest database
    Imported latest database

---
+++ interns_data.sql
@@ -0,0 +1,21 @@
+-- Database Manager 4.2.5 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `interns_data`;
+CREATE TABLE `interns_data` (
+  `intern_id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `image_filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`intern_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `interns_data` (`intern_id`, `name`, `username`, `image_filename`) VALUES
+(9,	'Okunuga melody',	'melody',	'Screenshot-2018-3-21 Twitter.png'),
+(11,	'ikpe mercy michael',	'mercyikpe',	'IMG-20180322-WA0010.jpg');
+
+-- 2018-04-13 06:08:02
