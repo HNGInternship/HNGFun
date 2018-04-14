247e18d31aa4aafdb8d595aedb42707b3ab51eb5
Oluwatobi Adenekan <oluwatobi.adenekan@student.uniosun.edu.ng>
2018-04-14 21:25:35 +0100

    @uncle tee ready for production

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-define ('DB_USER', "tobi.adenekan");
-define ('DB_PASSWORD', "school");
-define ('DB_DATABASE', "hngfun");
+
+define ('DB_USER', "root");
+define ('DB_PASSWORD', "");
+define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
 
 ?>
