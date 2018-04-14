a4977040d67ff1c7f8544580f1e0d189965df9f7
Akinduko Akinwumi <akindukooa@Akindukos-MacBook-Air.local>
2018-04-14 23:39:25 +0100

    hng-v-1.0

--- hng_fun.sql
+++ hng_fun.sql
@@ -31,7 +31,6 @@ SET time_zone = "+00:00";
 
 /*!40101 SET NAMES utf8 */;
 
-/*!40101 SET NAMES utf8mb4 */;
 
 
 --
@@ -57,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `interns_data_` (
 --
 
 INSERT INTO `interns_data_` (`intern_id`, `name`, `username`, `image_filename`) VALUES
-(14, 'Solanke damilare', 'D.dan', 'https://res.cloudinary.com/damilare1957/image/upload/v1523622655/dan.jpg');
+(14, 'Akinduko Olugbenga', 'dev_geaks', 'http://res.cloudinary.com/devgeaks/image/upload/v1523731563/2017-03-02_08.30.03.jpg');
 
 -- --------------------------------------------------------
 
@@ -168,7 +167,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Database: `hng_fun`
