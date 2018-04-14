064d716d59ca26738cf704b48fe9b9c1844e1dc4
Sheyilaaw <Sheyilaaw98@gmail.com>
2018-04-14 17:55:53 +0100

    changed path to config file

--- profiles/OluwaseyiSam.php
+++ profiles/OluwaseyiSam.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-include_once "";"../config.php";
+include_once "../config.php";
 include_once "../config.example.php";
 $connection  =  mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE)
                 or die("Error Connecting to Database");
