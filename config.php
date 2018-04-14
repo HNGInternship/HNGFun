b370210f3eb4ff62b2e9df4a125042294a268983
roqak <roqak1@gmail.com>
2018-04-14 12:49:07 -1100

    akinkunmi

--- config.php
+++ config.php
@@ -1,12 +1,4 @@
 define ('DB_USER', "root");
-<<<<<<< HEAD
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hngfun");
-define ('DB_HOST', "localhost");
-
-?>
-=======
 define ('DB_PASSWORD', "ayokunumi");
 define ('DB_DATABASE', "hng");
 define ('DB_HOST', "");
->>>>>>> 2d46cf28919020815e4b3c2f7119437ec0a85c44
