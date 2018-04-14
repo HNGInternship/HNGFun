fa3ef811a475c83d13526d845b8f4d639ec45533
Chigozie Ekwonu <chygoz2@gmail.com>
2018-04-14 16:29:06 +0100

    updated config.php

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
+<?php
+
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "ayokunumi");
-define ('DB_DATABASE', "hng");
-define ('DB_HOST', "");
+define ('DB_PASSWORD', "");
+define ('DB_DATABASE', "hng fun");
+define ('DB_HOST', "localhost");
+
+?>
