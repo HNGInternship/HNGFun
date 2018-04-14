6c6d655b3d3563e0b0832ebb2e25ae5d9fd44160
Chigozie Ekwonu <chygoz2@gmail.com>
2018-04-14 12:23:43 +0100

    Fixed merge conflict on config.php

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
 <?php
 
 define ('DB_USER', "root");
-<<<<<<< HEAD
-define ('DB_PASSWORD', "root");
-define ('DB_DATABASE', "hngfun");
-define ('DB_HOST', "127.0.0.1");
-=======
 define ('DB_PASSWORD', "");
 define ('DB_DATABASE', "hng fun");
 define ('DB_HOST', "localhost");
->>>>>>> bef2a4ac0121a05d3c0a321483d75b351f2fcc7b
 
 ?>
