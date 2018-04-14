b060f86f3cb90a91b14583ee2b0717f60d2a49e4
oriechinedu <oriechinedu>
2018-04-14 12:55:08 +0100

    update my profile

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "");
-define ('DB_HOST', "");
+define ('DB_PASSWORD', "c@lculus2689");
+define ('DB_DATABASE', "hngfun");
+define ('DB_HOST', "localhost");
 
 ?>
