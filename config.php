8210a5c377caf0d0bf5b5aa7c911dbd165f11397
Faith Odonghanro <toriboi.fo@gmail.com>
2018-04-14 12:00:13 +0100

    Add toriboi profile

--- config.php
+++ config.php
@@ -5,13 +5,4 @@ define ('DB_PASSWORD', "\$tori58622685");
 define ('DB_DATABASE', "hngfun");
 define ('DB_HOST', "localhost");
 
-<<<<<<< HEAD
 ?>
-=======
-define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hngfun");
-define ('DB_HOST', "localhost");
-
-?>
->>>>>>> 44420bc2258abdbad869abc306061b3cccae6c15
