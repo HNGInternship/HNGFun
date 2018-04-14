17fb45c5265cae1f868013f1528eda79a2e5019a
Efa-Iwa Eleng <gistend@gmail.com>
2018-04-14 10:19:29 +0100

    Update efa.php

--- profiles/efa.php
+++ profiles/efa.php
@@ -12,13 +12,13 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 
 <style>
-body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
-.w3-row-padding img {margin-bottom: 12px}
+body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif;}
+.w3-row-padding img {margin-bottom: 12px;}
 .w3-sidebar {width: 120px;background: #222;}
 
-#main {margin-left: 120px}
+#main {margin-left: 120px;}
 
-@media only screen and (max-width: 600px) {#main {margin-left: 0}}
+@media only screen and (max-width: 600px) {#main {margin-left: 0;}}
 </style>
   
 </head>
