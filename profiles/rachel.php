3b377a990a3b52c231853a8251ee569ef52a54bd
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2018-04-14 17:11:59 +0100

    created rachel.php file

---
+++ profiles/rachel.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open
+		+Sans|Kaushan+Script">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous">
+  <style type="text/css">
+    h1 {
+      font-family: Advent Pro;
+      font-weight: bold;
+    }
+    body{
+      font-family: Advent Pro;
+      letter-spacing: 2px;
+    }
+    a {
+      color:black;
+      font-size: 25px;
+      padding: 5px
+    }
+    h5, h6 {
+      font-weight: bold;
+      font-family: Advent Pro;
+    }
+    h1 {
+      color: white;
+    }
+    .bg {
+      background: linear-gradient(rgba(0, 0, 0, 0.816), rgba(0, 0, 0, 0.816)),url('https://res.cloudinary.com/rachelabaniwo/image/upload/v1523703428/6tag_010118-020231_vqigfc.jpg') no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+      width: 100%;
+      height: auto;
+      top: 0;
+      left: 0;
+    }
+  </style>
+  <title>Rachel's Profile</title>
+</head>
+
+<body class="bg">
+
+  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1>Rachel Abaniwo</h1>
+  </div>
+
+  <div class="container row mt-4 justify-content-center mx-auto">
+    <div class="text-center col-sm-5">
+      <div class="card mb-4 box-shadow text-center">
+        <img class="card-img-top" src='https://res.cloudinary.com/rachelabaniwo/image/upload/v1523703428/6tag_010118-020231_vqigfc.jpg' alt="Card image cap">
+        <div class="card-body">
+          <h6 class="mt-2">BIO</h6>
+          <p class="card-text">I found fufilment in programming and I've been stuck on ever since, I'm a full stack web developer and an aspiring 'badass' designer. I love photograpy and I believe the only constant thing in life should be learning.</p>
+        </div>
+        
+        
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <h6>SLACK USERNAME</h6>@Rachel</li>
+            <li class="list-group-item">
+              <h6>SOCIAL MEDIA LINKS</h6>
+              <a href="https://github.com/RachelAbaniwo"><i class="fa fa-github"></i></a>
+              <a href="https://www.linkedin.com/in/rachel-abaniwo/"><i class="fa fa-linkedin"></i></a>
+              <a href="https://twitter.com/raeabaniwo"><i class="fa fa-twitter"></i></a>
+            </li>
+          </ul>
+        
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
