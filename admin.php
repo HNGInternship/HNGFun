ec94318bdf57e5d2b6c53a1ef4e2e454a9a08f6d
avatech <techissued@gmail.com>
2018-04-14 14:26:01 +0100

    Update

--- admin.php
+++ admin.php
@@ -55,41 +55,41 @@ if(isset($_POST['submit']) ){
         }
     }
 
-    /** Upload File and Insert Data into Database
-    if ($nameError == "" && $usernameError == "" && $keyError == "") {
-        //Upload file
-        $max_size = 500 * 1024; // 500 KB
-        $destination_directory = "images/";
-        $validExtensions = array("jpeg", "jpg", "png", "JPEG", "JPG", "PNG");
-        $temporary = explode(".", $_FILES["file"]["name"]);
-        $file_extension = end($temporary);
+    // /** Upload File and Insert Data into Database
+    // if ($nameError == "" && $usernameError == "" && $keyError == "") {
+    //     //Upload file
+    //     $max_size = 500 * 1024; // 500 KB
+    //     $destination_directory = "images/";
+    //     $validExtensions = array("jpeg", "jpg", "png", "JPEG", "JPG", "PNG");
+    //     $temporary = explode(".", $_FILES["file"]["name"]);
+    //     $file_extension = end($temporary);
 
-        if ( (($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") ||
-                ($_FILES["file"]["type"] == "image/jpeg")) && in_array($file_extension, $validExtensions)) {
-            if ( $_FILES["file"]["size"] < ($max_size) ) {
-                if ( $_FILES["file"]["error"] > 0 ) {
-                    $uploadError = "Error: <strong>" . $_FILES["file"]["error"] . "</strong>";
-                }
-                else if (file_exists($destination_directory . $_FILES["file"]["name"]) ) {
-                    $uploadError = "Error: File <strong>" . $_FILES["file"]["name"] . "</strong> already exists.";
-                }
-                else {
-                    $sourcePath = $_FILES["file"]["tmp_name"];
-                    $targetPath = $destination_directory . $_FILES["file"]["name"];
-                    if(move_uploaded_file($sourcePath, $targetPath)) {
-                        $imageSuccess = true;
-                    } else {
-                        $uploadError = "Upload failed";
-                    }
-                }
-            }
-            else {
-                $uploadError = "The size of image you are attempting to upload is " . round($_FILES["file"]["size"]/1024, 2) . " KB, maximum size allowed is " . round($max_size/1024, 2) . " KB";
-            }
-        }
-        else {
-            $uploadError = "Invalid image format. Allowed formats: JPG, JPEG, PNG.";
-        } **/
+    //     if ( (($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") ||
+    //             ($_FILES["file"]["type"] == "image/jpeg")) && in_array($file_extension, $validExtensions)) {
+    //         if ( $_FILES["file"]["size"] < ($max_size) ) {
+    //             if ( $_FILES["file"]["error"] > 0 ) {
+    //                 $uploadError = "Error: <strong>" . $_FILES["file"]["error"] . "</strong>";
+    //             }
+    //             else if (file_exists($destination_directory . $_FILES["file"]["name"]) ) {
+    //                 $uploadError = "Error: File <strong>" . $_FILES["file"]["name"] . "</strong> already exists.";
+    //             }
+    //             else {
+    //                 $sourcePath = $_FILES["file"]["tmp_name"];
+    //                 $targetPath = $destination_directory . $_FILES["file"]["name"];
+    //                 if(move_uploaded_file($sourcePath, $targetPath)) {
+    //                     $imageSuccess = true;
+    //                 } else {
+    //                     $uploadError = "Upload failed";
+    //                 }
+    //             }
+    //         }
+    //         else {
+    //             $uploadError = "The size of image you are attempting to upload is " . round($_FILES["file"]["size"]/1024, 2) . " KB, maximum size allowed is " . round($max_size/1024, 2) . " KB";
+    //         }
+    //     }
+    //     else {
+    //         $uploadError = "Invalid image format. Allowed formats: JPG, JPEG, PNG.";
+    //     } **/
 
         //if ($imageSuccess) {
             //Insert Data
