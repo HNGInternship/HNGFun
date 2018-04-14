7be1cd2a3d25a54d869ed19e04234804a84c82e1
Emmanuel Joseph <jekayode@live.com>
2018-04-14 09:18:08 +0100

    Repo clean up

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand" href="/index.php">HNG.FUN</a>
+        <a class="navbar-brand" href="/index.php">HNG.jFUN</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
