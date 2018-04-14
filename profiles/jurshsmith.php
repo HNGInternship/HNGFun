4ab8f5420e930faf62955607a942572de98ba631
Jurshsmith <jurshsmith@gmail.com>
2018-04-13 18:51:34 -0700

    I added my Profile

---
+++ profiles/jurshsmith.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>
+HNG | Jurshsmith
+</title>
+</head>
+
+<body>
+	<br>
+	<center><h2 style = "font-family: montserrat">HNG 4.0 Internship Profile Page</h2></center><br>
+	<center>
+<img 
+style = "border-radius: 3%"
+src = "https://res-console.cloudinary.com/jurshsmith/thumbnails/v1/image/upload/v1523643164/SU1HXzIwMTcwMjI0XzE1MDkxNA==/grid" alt = ""><br><br><br>
+<font style = "color : grey; font-family: montserrat">Username : &nbsp;&nbsp;&nbsp;&nbsp;JURSHSMITH</font><br><br>
+<font style = "color : grey; font-family: montserrat">Skills : &nbsp;&nbsp;&nbsp;&nbsp;HTML, CSS, PHP, mySQL, Bootstrap, Jquery, MJML</font>
+
+</center>
+
+
+</body>
+
+</html>
