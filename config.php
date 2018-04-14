2894eccfdbb6f53e0b4643385a44a7afb81e8196
Bosun Egberinde <bosunski@gmail.com>
2018-04-14 17:05:30 +0100

    Removed config.php

--- config.php
+++ config.php
@@ -1,8 +1,16 @@
 <?php
+    define ('DB_USER', "root");
+    define ('DB_PASSWORD', "gabriel10");
+    define ('DB_DATABASE', "hngfun");
+    define ('DB_HOST', "localhost");
+?>
 
-define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hng_fun");
-define ('DB_HOST', "localhost");
+
+<?php
+
+// define ('DB_USER', "root");
+// define ('DB_PASSWORD', "29gE9t*dJ2#2f-BS");
+// define ('DB_DATABASE', "hng_fun");
+// define ('DB_HOST', "localhost");
 
 ?>
