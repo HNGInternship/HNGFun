5adebda3b2204f305789e474362cffbaf255fe22
Hammed01 <shotolahammed01@gmail.com>
2018-04-16 00:07:34 +0100

    changed the Username

--- profiles/woleo.php
+++ profiles/woleo.php
@@ -1,3 +1,21 @@
+<?php
+    try {
+        $profile = 'SELECT * FROM interns_data_ WHERE username="woleo"';
+        $select = 'SELECT * FROM secret_word';
+    
+        $query = $conn->query($select);
+        $profile_query = $conn->query($profile);
+    
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $profile_query->setFetchMode(PDO::FETCH_ASSOC);
+    
+        $get = $query->fetch();
+        $user = $profile_query->fetch();
+    } catch (PDOException $e) {
+        throw $e;
+    }
+    $secret_word = $get['secret_word'];
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,17 +60,7 @@ a {
     <a href="https://www.fb.com/S.Hammed"><i class="fa fa-facebook"></i></a> 
  </div>
  <p><button>Contact</button></p>
- <?php
-    try {
-        $sql = 'SELECT * FROM secret_word';
-        $q = $conn->query($sql);
-        $q->setFetchMode(PDO::FETCH_ASSOC);
-        $data = $q->fetch();
-    } catch (PDOException $e) {
-        throw $e;
-    }
-    $secret_word = $data['secret_word'];
-    ?>
+
 </body>
 </html>
 
