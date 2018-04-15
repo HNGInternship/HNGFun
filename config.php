eaea0d9c637f82640b007c7bbe219cc555adf10c
nedu <iamnedu912@gmail.com>
2018-04-15 15:06:35 +0100

    i've added my profile

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define ('DB_USER', "root");
 define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "");
-define ('DB_HOST', "");
+define ('DB_DATABASE', "hng_fun");
+define ('DB_HOST', "localhost");
 
 ?>
