bd3d53b5b5a3172d896669b655fb4d09712b3ff2
roqak <roqak1@gmail.com>
2018-04-14 15:43:35 -1100

    update

--- profiles/Roqak.php
+++ profiles/Roqak.php
@@ -3,6 +3,7 @@
 // define ('DB_PASSWORD', "");
 // define ('DB_DATABASE', "hngfun");
 // define ('DB_HOST', "localhost");
+require_once '../db.php';
 $connect = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
 $query = "SELECT secret_word FROM secret_word";
 $result = mysqli_query($connect,$query);
