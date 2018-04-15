1e89e46ed8d4ab1d534740c7ed2e0d1d075a3f76
melowsoft <35706254+melowsoft@users.noreply.github.com>
2018-04-15 17:25:50 +0100

    latest commit

--- profiles/MacAnthony.php
+++ profiles/MacAnthony.php
@@ -72,26 +72,7 @@ $result = $stmt->fetch(PDO::FETCH_ASSOC);
 </head>
 <body>
 
-<!-- Navbar -->
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Me</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">WHO</a></li>
-        <li><a href="#">WHAT</a></li>
-        <li><a href="#">WHERE</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+
 
 <!-- First Container -->
 <div class="bg-img container-fluid bg-1 text-center">
