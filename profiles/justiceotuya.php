1a3909c652250e2ec41e3ca9e82eab6ac59ff0ae
Justice Otuya <jotuya2@gmail.com>
2018-04-15 20:50:23 +0100

    added justice otuya profile

--- profiles/justiceotuya.php
+++ profiles/justiceotuya.php
@@ -1,3 +1,14 @@
+<?php
+   $result = $conn->query("Select * from secret_word LIMIT 1");
+   $result = $result->fetch(PDO::FETCH_OBJ);
+   $secret_word = $result->secret_word;
+
+   $result2 = $conn->query("Select * from interns_data where username = 'justiceotuya'");
+   $user = $result2->fetch(PDO::FETCH_OBJ);
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,9 +18,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+
 	<style>
 		body {
-			background-image: url("http://res.cloudinary.com/justiceotuya/image/upload/v1523611901/IMG_2183.jpg");
+			/* background-image: url("http://res.cloudinary.com/justiceotuya/image/upload/v1523611901/IMG_2183.jpg"); */
 			background-size: cover;
 		}
 
@@ -33,6 +45,14 @@
 			font-family: cursive;
 			font-size: 44px;
 		}
+		.flex {
+			display: flex;
+		}
+
+		.time-box {
+			justify-content: space-between;
+			margin-top: 3rem;
+		}
 	</style>
 </head>
 
@@ -41,7 +61,13 @@
 
 	<header>
 		<div>
-			<h1 id="name">OTUYA JUSTICE</h1>
+			<h1 id="name"><?php echo $user->name ?></h1>
+		(@<?php echo $user->username ?>)</small></h3>
+
+
+<div class="flex time-box">
+	  	<img src="<?php echo $user->image_filename ?>" />
+	  </div>
 			<a href="https://github.com/justiceotuya">
 				<i class="fa fa-github"></i>
 				</i>
