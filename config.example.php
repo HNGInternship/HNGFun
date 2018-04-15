5c930653783e5793d67e0c64e60992e87b2b5124
mikoloxtra <mikoloxtra@gmail.com>
2018-04-15 22:03:51 +0100

    New Commit

--- config.example.php
+++ config.example.php
@@ -1,9 +1,7 @@
 <?php
 
-define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hngfun");
-define ('DB_HOST', "");
-
-
+define ('DB_USER', "admin");
+define ('DB_PASSWORD', "mikoloxtra");
+define ('DB_DATABASE', "hng");
+define ('DB_HOST', "localhost");
 ?>
