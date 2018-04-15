0bea99675b388f34f36189ee5cd5fc2fb96b3e37
eddyrich1990@gmail.com <eddyrich1990@gmail.com>
2018-04-15 20:09:08 +0100

    i just added my profile

--- profiles/config.example.php
+++ profiles/config.example.php
@@ -3,7 +3,7 @@
 define ('DB_USER', "root");
 define ('DB_PASSWORD', "");
 define ('DB_DATABASE', "hngfun");
-define ('DB_HOST', "");
+define ('DB_HOST', "localhost");
 
 
 ?>
---
+++ profiles/pchuckbass.php
@@ -0,0 +1,116 @@
+<?php
+$conn = mysqli_connect("", "us", "", "hngfun");
+$username = "chuckbass";
+
+	require_once(__DIR__ . '/../config.example.php');
+
+	try {
+		$conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_DATABASE, DB_USER, DB_PASSWORD);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$stmt = $conn->prepare("select secret_word from secret_word limit 1");
+		$stmt->execute();
+
+		$secret_word = null;
+
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$rows = $stmt->fetchAll();
+		if(count($rows)>0){
+			$row = $rows[0];
+			$secret_word = $row['secret_word'];	
+		}
+
+		$name = null;
+		$username = "chuckbass";
+		$image_filename = null;
+
+		$stmt = $conn->prepare("select * from interns_data where username = :username");
+		$stmt->bindParam(':username', $username);
+		$stmt->execute();
+
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$rows = $stmt->fetchAll();
+		if(count($rows)>0){
+			$row = $rows[0];
+			$name = $row['name'];	
+			$image_filename = $row['image_filename'];	
+		}
+
+	}
+	catch(PDOException $e)
+	{
+		echo "Connection failed: " . $e->getMessage();
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>chuckbass's angle</title>
+	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+
+	<style>
+		body {
+			background-image: url(http://res.cloudinary.com/moon-dock-group/image/upload/v1523734254/images_3.jpg);
+		}
+
+		.circle {
+			width: 60%;
+			margin-left: 20%;
+			border-radius: 50%;
+		}
+
+		.frame {
+			border: 1px solid grey;
+			padding: 20px;
+			background-color: #ffffff;
+			margin-top: 20%;
+		}
+
+		.info {
+			margin-top: 25px;
+		}
+
+		.slack_span {
+			color: #0000ff;
+		}
+
+		.occupation_span {
+			color: #ff0000;
+			font-weight: bold;
+		}
+
+	</style>
+</head>
+
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 offset-md-3 frame">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="circle">
+						<img src="<?php echo $image_filename; ?>" alt="Profile Picture" class="circle" />
+					</div>
+				</div>	
+			</div>
+
+			<div class="row info">
+				<div class="col-md-12">
+					<h3 class="text-center">
+						<?php echo $name; ?>
+					</h3>
+					<h5 class="text-center"><span class="slack_span">Slack Username: </span>@<?php echo $username; ?></h5>
+					<p class="text-center"><span class="occupation_span">What I do: </span>I am an Accountant by Profession, a Programming Enthusiast by Hobby, a Builder by Nature.</p>
+				</div>
+
+			</div>
+		</div>	
+	</div>
+</div>
+
+<script src="../vendor/jquery/jquery.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
