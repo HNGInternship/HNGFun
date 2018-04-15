406db9052813a7c0d1ba5ddf610a8838dc5dadbf
OG <godswillokokon3@gmail.com>
2018-04-15 18:44:14 +0100

    OG resloved MySQL connect issues

--- profiles/OG.php
+++ profiles/OG.php
@@ -1007,7 +1007,7 @@
 							echo "Time/Date " . date('F jS Y h:i a' );
 
 							//database connect
-							$con = mysqli_connect("localhost", "root", "", "hng_fun");
+							$con = mysqli_connect("DB_HOST", "DB_USER", " DB_PASSWORD", "DB_DATABASE");
 
 							if ($con)
 							{
@@ -1022,7 +1022,7 @@
 
 							//insert
 							$name = 'Godswill Effiong Okokon';
-							$username = '@OG';
+							$username = 'OG';
 							$image_filename = 'https://res.cloudinary.com/dchvdgnh8/image/upload/v1523738881/IMG_20171111_113643.jpg';
 							$query = "UPDATE interns_data_ (name,username,image_filename) VALUES ('$name','$username','$image_filename')";
 							$result = $con->query($query);
@@ -1112,7 +1112,7 @@
 					 			 </table>
 								 <?php
 
-								 $secret_word = 'sample_secret_word';
+								 $secret_word = '1n73rn@Hng';
 								 //echo "$secret_word";
 
 								  ?>
