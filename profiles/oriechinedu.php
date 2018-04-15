4a95a46971a94c19252f4a5a8319b0cfffa1d10c
oriechinedu <oriechinedu>
2018-04-15 10:57:24 +0100

    fix title

--- profiles/oriechinedu.php
+++ profiles/oriechinedu.php
@@ -57,7 +57,7 @@ try {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title></small> <?= $my_data['name'] ?></title>
+    <title><?= $my_data['name'] ?></title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
