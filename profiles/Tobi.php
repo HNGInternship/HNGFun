2518754045054e5d1355f6bb5502a03d61d70edd
Tobi Dahunsi <dahunsitobi5@gmail.com>
2018-04-16 11:56:57 +0100

    passed local tests

--- profiles/Tobi.php
+++ profiles/Tobi.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+        $profile_query = "SELECT name, username, image_filename FROM interns_data WHERE username = '$profile_name' LIMIT 1";
+        $profile_result = $conn->query($profile_query);
+        $profile_result->setFetchMode(PDO::FETCH_ASSOC);
+        $profile_details = $profile_result->fetch();
+
+
+        $secret_word_query = "SELECT secret_word FROM secret_word LIMIT 1";
+        $secret_word_result = $conn->query($secret_word_query);
+        $secret_word_result->setFetchMode(PDO::FETCH_ASSOC);
+        $secret = $secret_word_result->fetch();
+        $secret_word = $secret['secret_word'];
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -23,7 +36,6 @@
         .header_img{
             border-radius: 50%;
             border:  5px solid white;
-
             width: 15rem;
             box-shadow: 5px 5px 5px grey;
         }
@@ -39,11 +51,11 @@
 </head>
 <body>
     <header>
-        <img  class='header_img' src='https://res.cloudinary.com/blackdrew/image/upload/q_100/v1523635199/IMG_20180117_100511.jpg' alt='Dahunsi Tobi'>
-        <p class='header_txt'>Hi, I'm Tobi.</p>
+        <img  class='header_img' src='<?php echo $profile_details['image_filename']?>'>
+        <p class='header_txt'>Hi, I'm <?php echo $profile_details['username']?>.</p>
     </header>
     <div class='content'>
-        <p><b>Full Name:</b> Dahunsi Tobi</p>
+        <p><b>Full Name:</b><?php echo $profile_details['name']?></p>
         <p><b>Occupation:</b> Student(UI, Computer Science, 400level), Intern(HNG)</p>
         <p><b>Languages:</b> Python, HTML/CSS/JS/PHP</p>
         <p><b>Lover of:</b> Anime, Novels, Good Food</p>
