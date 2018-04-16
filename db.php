4143c7cdac151c34db39b65b4a6c4278aee80596
Egberinde Olatunbosun <bosunski@gmail.com>
2018-04-16 18:42:58 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,29 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 
+require_once '../config.php';
 
-require_once 'config.example.php';
-//=======
-//require_once '../config.php';
-
-
-require_once 'config.example.php';
-
-require_once 'config.php';
-
-require_once 'config.example.php';
-
-// require_once '../config.php';
-
-
-require_once 'config.php';
-
-
-=======
-require_once 'config.php';
-
->>>>>>> b2bbe46b15fc26a9eb4bb2d3c3786788727f0b54
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
     echo "Connected to ". DB_DATABASE . " successfully.</br>";
