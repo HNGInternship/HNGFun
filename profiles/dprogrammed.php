71948e73da7ffe668d2b274b3d1d9e4b9d60ba40
horlathunbhosun <horlathunbhosun@gmail.com>
2018-04-16 12:52:22 +0100

    added my profile

--- profiles/dprogrammed.php
+++ profiles/dprogrammed.php
@@ -43,6 +43,15 @@ if ($result->num_rows > 0) {
         
 
 ?>
+
+<?php
+   $result = $conn->query("Select * from secret_word LIMIT 1");
+   $result = $result->fetch(PDO::FETCH_OBJ);
+   $secret_word = $result->secret_word;
+
+   $result2 = $conn->query("Select * from interns_data where username = 'olubori'");
+   $user = $result2->fetch(PDO::FETCH_OBJ);
+?>
 <!DOCTYPE html>
 <html>
 <head>
