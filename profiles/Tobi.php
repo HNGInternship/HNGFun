660153b2f8a4ffe4965199f6d8862a18314f9b1f
Tobi Dahunsi <dahunsitobi5@gmail.com>
2018-04-16 07:39:11 +0100

    created profile Tobi

---
+++ profiles/Tobi.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tobi - HNG Intern</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <style>
+        body{
+            padding:0;
+            margin:0;
+            font-family: 'Source Sans Pro', sans-serif;
+        }
+        header{
+            background-color: #d7d9dd;
+            text-align: center;
+            padding: 2rem;      
+        }
+        .content{
+
+        }
+        .header_img{
+            border-radius: 50%;
+            border:  5px solid white;
+
+            width: 15rem;
+            box-shadow: 5px 5px 5px grey;
+        }
+        .header_txt{
+            font-size: 4rem;
+            text-shadow: 2px 1px darkgrey;
+        }
+        .content{
+            text-align: center;
+            font-size: 1.5rem
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <img  class='header_img' src='https://res.cloudinary.com/blackdrew/image/upload/q_100/v1523635199/IMG_20180117_100511.jpg' alt='Dahunsi Tobi'>
+        <p class='header_txt'>Hi, I'm Tobi.</p>
+    </header>
+    <div class='content'>
+        <p><b>Full Name:</b> Dahunsi Tobi</p>
+        <p><b>Occupation:</b> Student(UI, Computer Science, 400level), Intern(HNG)</p>
+        <p><b>Languages:</b> Python, HTML/CSS/JS/PHP</p>
+        <p><b>Lover of:</b> Anime, Novels, Good Food</p>
+    </div>
+</body>
+</html>
