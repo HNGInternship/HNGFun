f2b6b3d76c6ffea80fa634be1fadf41f2764b0a1
mchardex <bukunmiadebisi@gmail.com>
2018-04-16 11:56:09 +0100

    setting up db

--- db.php
+++ db.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 require_once 'config.example.php';
-=======
-require_once '../config.php';
->>>>>>> 1a63d6d5d02d8fe9a4544287ddac79956b89b341
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
