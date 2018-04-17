653a7c95d588da8d362798aab838bba3033e58d7
Onesiphorusgod <Joan.miltz007@gmail.com>
2018-04-17 09:23:22 +0100

    Profile update

--- profiles/Ayo.php
+++ profiles/Ayo.php
@@ -89,7 +89,7 @@ button:hover, a:hover {
 <h2 style="text-align:center">HNG Internship Profile</h2>
 
 <div class="card">
-  <img src="https://cloudinary.com/console/media_library/asset/image/upload/IMG_20171023_180642_440" alt="Ayomide Apantaku" style="width:100%">
+  <img src="http://res.cloudinary.com/onesiphorus/image/upload/v1523619252/IMG_20171023_180642_440.jpg" alt="Ayomide Apantaku" style="width:100%">
   <h1><?php echo $name; ?></h1>
   <p class="title">Student, UI/UX designer, Web Developer</p>
   <p><a href="#">HNG Internship 4.0</a></p>
