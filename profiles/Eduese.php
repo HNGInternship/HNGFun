a900dac12696e28bd0afc3784fe7c2e6967cfef6
unknown <iniubong.eduese@gmail.com>
2018-04-17 12:21:20 +0100

    myeditedfilr

--- profiles/Eduese.php
+++ profiles/Eduese.php
@@ -69,29 +69,6 @@ p, h4{color:blue}
 
 </style>
 
-<?php
-
-/*/ This file provides the information for accessing the database.and connecting 
-//to MySQL. It also sets the language coding to utf-8
-
-// First we define the constants: 
-
-DEFINE ('DB_USER', 'Eduese');
-DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', '');
-
-// Next we assign the database connection to a variable that we will call $dbcon: 
-$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-OR die("Could not connect to server: " .mysqli_connect_error());
-
-// Finally, we set the language encoding.as utf-8
-mysqli_set_charset($dbcon, 'utf8'); */
-
-?>
-
-
-
 
 <div id = "wrapper" class = "row">
 
@@ -125,8 +102,8 @@ $result = mysqli_query($dbcon, $q); // Run the query. #7
 	
 /*$p = "SELECT secret_word FROM secret_word ;";	
 $result2 = mysqli_query($dbcon, $q); // Run the query. #7
-
-	*/
+$secret_word = $row['secret_word']
+echo $secret_word;*/
 	
 	?> 
 	
