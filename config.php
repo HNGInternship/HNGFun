d93919e5c016196695d346a0eba8af76230dd2b4
Jurshsmith <34186192+Jurshsmith@users.noreply.github.com>
2018-04-16 22:20:57 -0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define ('DB_USER', "root");
 
 //define ('DB_PASSWORD', "");
 //define ('DB_DATABASE', "hng.fun");
-define ('DB_PASSWORD', "");
+define ('DB_PASSWORD', "!TB%7L6QHPbhTcWq");
 define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
 
