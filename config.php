4422c15f85032aaaf341faf6277bdf7847c04970
mchardex <bukunmiadebisi@gmail.com>
2018-04-17 14:02:17 +0100

    pushing my username.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define ('DB_USER', "admin");
 define ('DB_PASSWORD', "@H0ttestt");
-define ('DB_DATABASE', "hng-fun");
-define ('DB_HOST', "localhost");
+define ('DB_DATABASE', "hng_fun");
+define ('DB_HOST', "127.0.0.1");
 
 ?>
