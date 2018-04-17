5e461914901146f6f2f187f868e7f0bf8913f1dc
mercyikpe <ikpemercy1@gmail.com>
2018-04-18 00:47:42 +0100

    Update mercyikpe.php

--- profiles/mercyikpe.php
+++ profiles/mercyikpe.php
@@ -48,6 +48,8 @@
 <html>
 <head>
 	<title>Mercy Ikpe | Jamila</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	
 	<link rel="stylesheet" type="text/css" href="../css/jamila.css">
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
