bd8ac2d76b7f1d54433f655e2969350cdb76e600
Salem Nzeukwu <sjimfender@gmail.com>
2018-04-17 12:20:23 +0100

    Add Jimie (Jim's chatbot) V1.0

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hng-fun");
+define ('DB_USER', "hngfun");
+define ('DB_PASSWORD', "funfun");
+define ('DB_DATABASE', "hngfun");
 define ('DB_HOST', "localhost");
 
 ?>
