f6cb03174320cd98b3b1bd95e2eb64570996d2e1
Mbah Clinton <clintonmbah44@gmail.com>
2018-04-17 12:38:35 +0000

    fixed merge issue in db.php

--- db.php
+++ db.php
@@ -1,15 +1,5 @@
 <?php
-<<<<<<< HEAD
-
-//<<<<<<< HEAD
-require_once 'config.example.php';
-//=======
-
-//require_once '../config.php';
-//>>>>>>> 830da70f6eb97dd08e0d23fc6b3fd57279d8d22a
-=======
 require_once '../config.php';
->>>>>>> 572965041b1dcf43d51a6911134c5afcbd2796e4
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
