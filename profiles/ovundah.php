cd39ce903009667950aee08cbce278e63ec9b874
OvundahKing <ovundahnyenke@gmail.com>
2018-04-17 17:24:05 +0100

    m

--- profiles/ovundah.php
+++ profiles/ovundah.php
@@ -40,7 +40,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <title>Ovundah King new</title>
+        <title>Ovundah King</title>
         <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
     </head>
     <body style="padding: 100px; font-size: 120%" >
@@ -49,7 +49,7 @@
                 <img src="http://res.cloudinary.com/ovu/image/upload/c_scale,e_grayscale,o_100,r_100,w_200/a_349/v1523814132/Ovundah.png" alt="Ovundah King" >
             </div>
             <div style="font-family: 'Exo 2', sans-serif;'">
-                <h1><strong>Ovundah King</strong></h1>
+                <h1><strong>Ovundah King new</strong></h1>
                 <p>Tech Enthusiast</p>
                 <p>Figma, HTML, CSS, JS, MEAN</p>
 	               <a href="https://twitter.com/OvundahKing" style='color: #5697ff'>
