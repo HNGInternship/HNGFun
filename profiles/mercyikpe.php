b40f7734504868a09ca9c256b044d44185c88335
mercyikpe <ikpemercy1@gmail.com>
2018-04-18 00:15:50 +0100

    Update mercyikpe.php

--- profiles/mercyikpe.php
+++ profiles/mercyikpe.php
@@ -22,6 +22,12 @@
 	session_unset();
 
 
+        $sql = DB::prepare('SELECT intern_id, name, username, image_filename FROM interns_data WHERE username="mercyikpe"');
+        $q->execute();
+        extract($sql->fetch(PDO::FETCH_ASSOC));
+
+
+
 ?>
 
 
@@ -344,5 +350,14 @@
 	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../js/ripples.min.js"></script>
 	<script type="text/javascript" src="../js/jamila.js"></script>
+
+
+	<?php
+	$sql = DB::prepare('SELECT secret_word FROM secret_word');
+        $q->execute();
+       $secret_word = sql->fetch(PDO::FETCH_ASSOC)['secret_word'];
+
+
+	?>
 </body>
 </html>
