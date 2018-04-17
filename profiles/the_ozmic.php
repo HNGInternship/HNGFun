a9c2a47e57d5e8692ac89e508c1a8d6100e52047
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-04-17 15:28:45 +0100

    Update the_ozmic.php

--- profiles/the_ozmic.php
+++ profiles/the_ozmic.php
@@ -81,7 +81,6 @@
     $password = explode("#", trim($question));
 
     if (isTraining($question)) {
-
       if (isset($password[2])) {
         if ($password[2] !== "trainpwforhng") {
           echo "Invalid authorization, you are not allowed to train me.";
