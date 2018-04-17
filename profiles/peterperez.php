4df18af7abefb8427f63a85abe5a8409faf424cb
Peter Pere <perezpeter32@gmail.com>
2018-04-17 22:21:57 +0000

    Update peterperez.php

--- profiles/peterperez.php
+++ profiles/peterperez.php
@@ -18,6 +18,15 @@
 	<title><?php echo $data['username']; ?> | HNGInternship4</title>
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	<style type="text/css">
 	body{
