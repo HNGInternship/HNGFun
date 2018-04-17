504bfa62a9736b7a883a42b80528485c6eb62c51
Mbah Clinton <clintonmbah44@gmail.com>
2018-04-17 11:38:33 +0000

    temporarily removed require answers.php

--- profiles/mclint_.php
+++ profiles/mclint_.php
@@ -13,7 +13,7 @@
 ?>
 
 <?php
-    require "../answers.php";
+    //require "../answers.php";
     $noIdeaResponses = array("Ha. Turns out that I'm not that smart after all. Train me, yoda! Please?", 
     "Maybe you humans might win after all. I have no idea what you just said. Please train me.",
     "Ugh. If only my creator trained me better I'd know what to say in reply to what you just said. Please train me?");
