6268031552dee5d6d3458a8a39778e823ceb7888
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-04-17 11:09:34 +0100

    Update the_ozmic.php

--- profiles/the_ozmic.php
+++ profiles/the_ozmic.php
@@ -1,12 +1,15 @@
 <?php
   if(isset($_POST['payload']) ){
     require "../../config.php";
+    require "../../answers.php";
     try {
         $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
     } catch (PDOException $pe) {
         die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
     }
   }
+
+  
   $date_time = new DateTime('now', new DateTimezone('Africa/Lagos'));
   try {
     $sql = 'SELECT * FROM secret_word';
