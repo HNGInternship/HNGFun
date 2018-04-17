5ae42a0f12a22004652e0a64d4f37999a6a3b698
Everistus Olumese <everistusolumese@gmail.com>
2018-04-17 22:49:49 +0100

    Modified my bot after review by @mark

--- profiles/bytenaija.php
+++ profiles/bytenaija.php
@@ -108,8 +108,14 @@ function training($question, $answer){
 
 if (isset($_GET["query"])) {
     include_once realpath(__DIR__ . '/..') . "/answers.php"; 
-    $file = realpath(__DIR__ . '/..') . "/db.php"; 
-require_once $file;
+    if(!defined('DB_USER')){
+        require "../../config.php";
+      }
+      try {
+        $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+      } catch (PDOException $pe) {
+        die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+      }
 global $conn;
 $image_filename = '';
 $name = '';
