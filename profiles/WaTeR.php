b63d8faab707b526ae8f2af627d8863e1c01d74b
Olowoyeye Oluwafemi Titus <olowoyeyeoluwafemi93@yahoo.com>
2018-04-17 19:34:30 +0100

    Update WaTeR.php

--- profiles/WaTeR.php
+++ profiles/WaTeR.php
@@ -1,9 +1,8 @@
 <?php 
  require 'db.php';
    $result = $conn->query("Select * from secret_word LIMIT 1");
-   $result = $result->fetch(PDO::FETCH_OBJ);
+    $result->execute();
    $secret_word = $result->secret_word;
-$username="water";
 
 $query1 = "SELECT * FROM interns_data_ WHERE username='WaTeR'";;
 
