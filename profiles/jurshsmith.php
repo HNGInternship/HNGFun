1347651fcc91b31ab6c0ac46a71e5affcb011f29
Jurshsmith <34186192+Jurshsmith@users.noreply.github.com>
2018-04-16 21:48:29 -0700

    Update jurshsmith.php

--- profiles/jurshsmith.php
+++ profiles/jurshsmith.php
@@ -1,11 +1,4 @@
-<?php 
-		require 'db.php';
-		$result = $conn->query("Select * from secret_word LIMIT 1");
-		$result = $result->fetch(PDO::FETCH_OBJ);
-		$secret_word = $result->secret_word;
-		$result2 = $conn->query("Select * from interns_data where username = 'olubori'");
-		$user = $result2->fetch(PDO::FETCH_OBJ);
-	?>
+
 <html>
 <head>
 	<meta charset="utf-8" />
