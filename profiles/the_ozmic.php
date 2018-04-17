b1aa06e84bf35b5df2c4429c5eb00b923fb3df02
Michael Ozoemena <michaelozoemena2@gmail.com>
2018-04-17 10:55:47 +0100

    Update the_ozmic.php

--- profiles/the_ozmic.php
+++ profiles/the_ozmic.php
@@ -1,6 +1,11 @@
 <?php
-  require_once realpath(__DIR__) . "/db.php";
-  require_once realpath(__DIR__) . "/answers.php";
+  require "../../config.php";
+  try {
+    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+  } catch (PDOException $pe) {
+    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+  }
+
   global $conn;
 
   $date_time = new DateTime('now', new DateTimezone('Africa/Lagos'));
