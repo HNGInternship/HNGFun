7eadc2a1d48970e779d1dd1cde42470c7bc56e7a
Ajiva-D <davidenochaji@gmail.com>
2018-04-17 14:07:07 +0100

    added my page

---
+++ profiles/ajiva.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang ="en-US"> 
+<head>
+    <title>Ajiva Profile</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <style>
+        body{
+            background-image: url("https://res.cloudinary.com/nedy123/image/upload/v1523911955/brainstorm_l87u78.png");
+            min-height: 100vh;
+            background-repeat: no-repeat;
+            background-size: cover;
+            border-radius: 50%;
+        } 
+        .card{
+            max-width: 30%;
+            margin-top: 10%;
+            border-radius: 30%;
+        }       
+    </style>
+    
+</head>
+
+<body>
+
+    <div class="d-flex justify-content-center">
+        <div class="card rounded p-0">
+            <img src="https://res.cloudinary.com/nedy123/image/upload/v1523911950/profilePic_xilm0r.jpg" class="card-img-top img-fluid m-0">
+            <div class="card-body text-center">
+                <p class="card-title h2 "> David Enoch Aji</p>
+                <p class="card-text h4 mb-4">Android | Graphics | UI/UX</p>
+                <a href="facebook.com/David Enoch Aji">
+                    <i class="fab fa-facebook-f fa-fw text-primary fa-2x"></i>
+                </a>
+                <a href="twitter.com/daveaji">
+                    <i class="fab fa-twitter fa-fw text-primary fa-2x"></i>  
+                </a>
+                <a href="github.com/Ajiva-D">
+                    <i class="fab fa-github fa-fw text-dark fa-2x"></i>          
+                </a>
+            </div>
+        </div>
+    </div>
+
+    
+</body>
+
+
+</html
