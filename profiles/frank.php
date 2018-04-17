be1cb2bf5950b88780d26150202b3d13bb02f389
frank <itaforfrancis@gmail.com>
2018-04-17 00:51:00 -0700

    Update frank.php

--- profiles/frank.php
+++ profiles/frank.php
@@ -14,7 +14,7 @@ body{
 }
  .container{
 position: relative;
-margin-top: 50px;
+margin-top: 100px;
 width: 300px;
 height: 300px;
 margin-left: 300px;
@@ -38,18 +38,185 @@ text-align: center;
 color: #F2F2F2;
  }
  
+ .header-fixed {
+    background-color:#292c2f;
+    box-shadow:0 1px 1px #ccc;
+    padding: 20px 40px;
+    height: 100px;
+    color: #ffffff;
+    box-sizing: border-box;
+    top: -6px;
+   position: fixed;
+   width:100%;
+   z-index: 10;
+    -webkit-transition:top 0.3s;
+    transition:top 0.3s;
+    
+}
+
+.header-fixed .header-limiter {
+    max-width: 1200px;
+    text-align: center;
+    margin: 0 auto;
+}
+
+/*    The header placeholder. It is displayed when the header is fixed to the top of the
+    browser window, in order to prevent the content of the page from jumping up. */
+
+.header-fixed-placeholder{
+    height: 80px;
+    display: none;
+}
+
+/* Logo */
+
+.header-fixed .header-limiter h1 {
+    float: left;
+    font: normal 28px Cookie, Arial, Helvetica, sans-serif;
+    line-height: 40px;
+    margin: 0;
+}
+
+.header-fixed .header-limiter h1 .schname {
+    color: #5383d3;
+    margin-top: 100px;
+}
+
+.header-fixed .header-limiter h1 a img {
+    color: #5383d3;
+    margin-top:1px;
+}
+/* The navigation links */
+
+.header-fixed .header-limiter a {
+    color: #ffffff;
+    text-decoration: none;
+}
+
+.header-fixed .header-limiter nav {
+    font:16px Arial, Helvetica, sans-serif;
+    line-height: 70px;
+    float: right;
+    background-color:#292c2f;
+    margin-top:-12px;
+    
+}
+
+.header-fixed .header-limiter nav a{
+    display: inline-block;
+    padding: 0 5px;
+    text-decoration:none;
+    color: #ffffff;
+    opacity: 0.9;
+}
+
+.header-fixed .header-limiter nav a:hover{
+    opacity: 1;
+}
+
+.header-fixed .header-limiter nav a.selected {
+    color: #608bd2;
+    pointer-events: none;
+    opacity: 1;
+}
+
+/* Fixed version of the header */
+
+body.fixed .header-fixed {
+    padding: 10px 40px;
+    height: 50px;
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: 1;
+}
+
+body.fixed .header-fixed-placeholder {
+    display: block;
+}
+
+body.fixed .header-fixed .header-limiter h1 {
+    font-size: 24px;
+    line-height: 30px;
+}
+
+body.fixed .header-fixed .header-limiter nav {
+    line-height: 28px;
+    font-size: 13px;
+}
+
+
+/* Making the header responsive */
+
+@media all and (max-width: 600px) {
+
+    .header-fixed {
+        padding: 20px 0;
+        height: 75px;
+    }
+
+    .header-fixed .header-limiter h1 {
+        float: none;
+        margin: -8px 0 10px;
+        text-align: center;
+        font-size: 24px;
+        line-height: 1;
+    }
+
+    .header-fixed .header-limiter nav {
+        line-height: 1;
+        float:none;
+    }
+
+    .header-fixed .header-limiter nav a {
+        font-size: 13px;
+    }
+
+    body.fixed .header-fixed {
+        display: none;
+    }
+
+}
 </style>   
 </head>
 <body>
+<header class="header-fixed">
+
+    <div class="header-limiter">
+
+        
+        <nav>
+            <a href="#">Home</a>
+            <a href="">Learning PHP </a>
+            <a href="#"></a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="">HNG INTERNSHIP 4 #STAGE3 TASK </a>
+            <a href="#"></a>
+        </nav>
+
+    </div>
+
+</header>
 
 <div class="container" >
     <span class="title">HNG INTERNSHIP 4 #STAGE3 TASK
                  </span>
     
   
+         <?php
+  define("DB_SERVER", "localhost");
+  define("DB_USER", "root");
+  define("DB_PASS", "");
+  define("DB_NAME", "hngfun");     
+$connection=mysqli_connect(DB_SERVER,DB_USER,"",DB_NAME);
+if(mysqli_connect_errno()){
+    die("database connection failed: ".mysqli_connect_error());
+}
+?>
  </br>   </br>    
   <?php 
-  $query=mysqli_query($conn,"SELECT * FROM secret_word ");
+  $query=mysqli_query($connection,"SELECT * FROM secret_word ");
     if(!$query){
         echo "Selecting code from secret word failed";
          } else{
@@ -57,7 +224,7 @@ color: #F2F2F2;
              $secret_word=$row['secret_word'];
               }
        
-             $sel =mysqli_query($conn,"SELECT * FROM interns_data where username='frank'");
+             $sel =mysqli_query($connection,"SELECT * FROM interns_data where username='frank'");
      ?>
         <?php   WHILE($row=mysqli_fetch_array($sel)) {?>
         <?php
