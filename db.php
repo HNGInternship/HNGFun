c8c224a59c4b4f81c5903b995ff938618bd5e1a2
ogedengbe samuel <samoged123@gmail.com>
2018-04-17 09:13:30 +0100

    dprogrammed

--- db.php
+++ db.php
@@ -19,7 +19,6 @@ require_once 'config.example.php';
 // require_once '../config.php';
 
 require_once '../config.php';
-d
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
