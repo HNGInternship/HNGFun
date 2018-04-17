70b381690ae4cdaa8a4f50c46ea3934765a53634
frank <itaforfrancis@gmail.com>
2018-04-17 01:14:00 -0700

    Update frank.php

--- profiles/frank.php
+++ profiles/frank.php
@@ -6,7 +6,6 @@ $q->setFetchMode(PDO::FETCH_ASSOC);
 $data = $q->fetchAll();
 $frank = array_shift($data);
 
-// Secret word
 $select_key = "SELECT * FROM secret_word";
 $q = $conn->query($select_key);
 $q->setFetchMode(PDO::FETCH_ASSOC);
@@ -20,7 +19,7 @@ $secret_word = $key['secret_word'];
 	<h1>FRANK</h1>
 	<?php else: ?>
 	<div class="photo-wrap">
-		<img src="<?php echo $frank['image_filename']; ?>" alt="" style="width:170px;height:170px; />
+		<img src="<?php echo $frank['image_filename']; ?>" alt="" style="width:400px;height:400px; />
 	</div>
 	<h1><?php echo $frank['name']; ?></h1>
 	<h3>HNG INTERNSHIP 4 #STAGE3 TASK</h3>
