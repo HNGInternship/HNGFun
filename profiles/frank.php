9891b3be1e5d2c7e530c28b144152bd4f4b76e16
frank <itaforfrancis@gmail.com>
2018-04-16 23:21:38 -0700

    Update frank.php

--- profiles/frank.php
+++ profiles/frank.php
@@ -70,9 +70,9 @@ background: #828282;
     <?php
   define("DB_SERVER", "localhost");
   define("DB_USER", "root");
-  define("DB_PASS", "frank");
-  define("DB_NAME", "hngfun");
-$connection=mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+  define("DB_PASS", "");
+  define("DB_NAME", "hng_fun");
+$connection=mysqli_connect(DB_SERVER,DB_USER,"",DB_NAME);
 if(mysqli_connect_errno()){
     die("database connection failed: ".mysqli_connect_error());
 }
