e9b6ca21e7fe995a5f4549855a8fd01aa7569b23
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-18 01:36:26 -0700

    chatbot

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -105,6 +105,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	<title>  <?php echo $user->name ?> </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<style type="text/css">
 		body{
