45fd5358d8709fdba0722b56603fc35bd76867bd
julietezekwe <julietezekwe@gmail.com>
2018-04-18 23:37:21 +0100

    juliet

--- profiles/juliet.php
+++ profiles/juliet.php
@@ -317,7 +317,7 @@ function decider($string){
                 var message = $("#msg").val();
                     var dataString = 'msg=' + msg + '&page=chat';
                     jQuery.ajax({
-                        url: "profiles/juliet.php",
+                        url: "/profiles/juliet.php",
                         data: dataString,
                         type: "POST",
                          cache: false,
