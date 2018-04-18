b5c43e645ca092c3e7d2d567b7e60f7f6c859979
julietezekwe <julietezekwe@gmail.com>
2018-04-18 20:44:30 +0100

    juliet edited the config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
+define ('DB_PASSWORD', "29gE9t*dJ2#2f-BS");
 define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
 
