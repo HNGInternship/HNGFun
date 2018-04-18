c180b354966e66d02e09aa2f5ae1692ed6edc602
oriechinedu <oriechinedu>
2018-04-18 11:23:20 +0100

    second bot milestone

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-    define ('DB_USER', "root");
-    define ('DB_PASSWORD', "");
-    define ('DB_DATABASE', "hng-fun");
-    define ('DB_HOST', "localhost");
+define ('DB_USER', "root");
+define ('DB_PASSWORD', "c@lculus2689");
+define ('DB_DATABASE', "hng_fun");
+define ('DB_HOST', "localhost");
 ?>
