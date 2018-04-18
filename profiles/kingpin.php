5d277371ee3f5577558c287cb6785e0e966f1d4b
saraencci <franksaraencci@gmail.com>
2018-04-19 04:10:26 +0530

    updated my profile

--- profiles/kingpin.php
+++ profiles/kingpin.php
@@ -86,9 +86,9 @@ body
 <body>
   <?php 
 require '../db.php';
-  $result = $conn->query("Select * from secret_word LIMIT 1");
-   $result = $result->fetch(PDO::FETCH_OBJ);
-   $secret_word = $result->secret_word;
+  //$result = $conn->query("Select * from secret_word LIMIT 1");
+  // $result = $result->fetch(PDO::FETCH_OBJ);
+ //  $secret_word = $result->secret_word;
    $result2 = $conn->query("Select * from interns_data where username = 'kingpin'");
    $user = $result2->fetch(PDO::FETCH_OBJ);
    $yy='<img  src="';
