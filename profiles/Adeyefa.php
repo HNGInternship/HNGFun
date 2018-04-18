9e9f2a33ee7a56b71a5864dff962cba8a7bcb743
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-17 21:54:05 -0700

    chatbot

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -1,32 +1,10 @@
 <?php
-if(!defined('DB_USER')){
-  require "../../config.php";		
-  try {
-      $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-  } catch (PDOException $pe) {
-      die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-  }
-}
-//Fetch User Details
-try {
-    $query = "SELECT * FROM interns_data WHERE username ='adeyefa'";
-    $resultSet = $conn->query($query);
-    $resultData = $resultSet->fetch(PDO::FETCH_ASSOC);
-} catch (PDOException $e){
-    throw $e;
-}
-$user = $resultData['username'];
-$fullName = $resultData['name'];
-//Fetch Secret Word
-try{
-    $querySecret =  "SELECT * FROM secret_word LIMIT 1";
-    $resultSet   =  $conn->query($querySecret);
-    $resultData  =  $resultSet->fetch(PDO::FETCH_ASSOC);
-    $secret_word =  $resultData['secret_word'];
-}catch (PDOException $e){
-    throw $e;
-}
-$secret_word =  $resultData['secret_word'];
+$result = $conn->query("Select * from secret_word LIMIT 1");
+$result = $result->fetch(PDO::FETCH_OBJ);
+$secret_word = $result->secret_word;
+
+$result2 = $conn->query("Select * from interns_data_ where username = 'adeyefa'");
+$user = $result2->fetch(PDO::FETCH_OBJ);
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
