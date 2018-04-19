657d05b5fae5fa38fdb458555e0d05e541a49a6f
Travis CI <travis@travis-ci.org>
2018-04-19 17:21:23 +0000

    committed at b48c486 [ci skip]

--- profiles/bamii.php
+++ profiles/bamii.php
@@ -1,18 +1,18 @@
 <?php
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		require "../answers.php";
-    //require '../db.php';
+    require '../db.php';
     # User input
     $data = $_POST['question'];
 
-    if(!defined('DB_USER')){
+    /* if(!defined('DB_USER')){
 			require "../../config.php";		
 			try {
 			    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 			} catch (PDOException $pe) {
 			    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
 			}
-    }
+    } */
     
     
     # Functions to get the data from db
