64c8367f3e9db8ab6b11ac8cd769d0a94553344c
Everistus Olumese <everistusolumese@gmail.com>
2018-04-19 10:19:13 +0100

    Fixing

--- profiles/juliet.php
+++ profiles/juliet.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once realpath(__DIR__ . '/..') . "/answers.php"; 
-require("../../config.php");
+require_once "../../config.php";
 // Create connection
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 // Check connection
