89642959044d2dec4dcbafb9e0303d18036e9c80
Adokiye <38386612+Adokiye@users.noreply.github.com>
2018-04-19 20:03:45 +0100

    ssss

--- profiles/Adokiye.php
+++ profiles/Adokiye.php
@@ -2,7 +2,7 @@
 include "../answers.php";
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 if(!defined('DB_USER')){
-    require "../../config.php";
+    require "../config.php";
     try {
         $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
     } catch (PDOException $pe) {
