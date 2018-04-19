dc57d7733734438860306f9a50c53feb86dc71e1
Dennis W. Otugo <otugodennis@gmail.com>
2018-04-19 16:04:58 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -2,12 +2,12 @@
 rev=$(git rev-parse --short HEAD)
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git clone --branch master=https://dennisotugo:${AUTOBUILD_TOKEN}@github.com/HNGInternship/HNGFun 
-git checkout prod
-git checkout master/profiles/*
-git checkout master/answer*
-git add ./profiles/*
-git add . answers*
+git clone -b master https://dennisotugo:${AUTOBUILD_TOKEN}@github.com/HNGInternship/HNGFun master
+git clone -b prod https://dennisotugo:${AUTOBUILD_TOKEN}@github.com/HNGInternship/HNGFun prod
+yes | cp -rf master/profiles/* prod/
+yes | cp -rf master/answer* prod/
+cd prod
+git add .
 git commit -m "committed at ${rev} [ci skip]"  
 git push origin prod
 echo -e" Done  "
