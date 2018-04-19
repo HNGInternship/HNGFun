bf7afac185ce9df1b3d2305b168549929e461c17
Jurshsmith <34186192+Jurshsmith@users.noreply.github.com>
2018-04-18 21:12:56 -0700

    Update jurshsmith.php
    edits

--- profiles/jurshsmith.php
+++ profiles/jurshsmith.php
@@ -1,3 +1,4 @@
+
 <?php 
 		require 'db.php';
 		$result = $conn->query("Select * from secret_word LIMIT 1");
@@ -7,90 +8,255 @@
 		$user = $result2->fetch(PDO::FETCH_OBJ);
 	?>
 
-
+<!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>
-HNG | Jurshsmith
+HNG 4.0 | Jurshsmith
 </title>
-<!--font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-  <!--javascripts assets-->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous">
-    </script>
-     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-	<script>
-	$(document).ready(function(){
-	$('.col-lg-8').hide();
-	
-	
-	});
-	</script>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"
+  integrity="sha256-xNjb53/rY+WmG+4L6tTl9m6PpqknWZvRt0rO1SRnJzw="
+  crossorigin="anonymous"></script>
 
-<style type="text/css">
-	html,body{
-	height : 100%;}
-li {
-	display: inline;
-	padding: 20px;
-	font-size: 32px;
-	
-}
-li a:hover{
- font-size: 28px;
-}
-.j-list li a{
-	color: orange;
-}
-b{
-	letter-spacing: 0.9px;
-	
+<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/color/jquery.color-2.1.2.js"
+  integrity="sha256-1Cn7TdfHiMcEbTuku97ZRSGt2b3SvZftEIn68UMgHC8="
+  crossorigin="anonymous"></script>
 
-}
-</style>
 
-</head>
 
-<body style = "background-image : url(https://wallpapercave.com/wp/58eyKEt.jpg); background-size:cover; background-attachment : fixed;">
-	<br>
-	<br><br>
-	<center><h2 style = "font-family: montserrat; color: white; letter-spacing: 1px"><font style = "color: orange">HNG 4.0.0</font> Internship Profile Page</h2></center>
-	<hr style = "background-color : purple;color:  purple; width : 50%">
-	<br>
-	<br>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+<!--Javascript starts here-->
+<script>
 
-	<center>
-<img  style = "border-radius: 7%;max-width : 100%; width: 380px" class = "myimage"
-src = "http://res.cloudinary.com/jurshsmith/image/upload/v1523643164/IMG_20170224_150914.jpg"  alt = ""><br><br><br>
-<script type="text/javascript"> $('.myimage').hide();</script>
-<font style = "color : white; font-family: montserrat">Username &nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;<b>JURSHSMITH</b></font><br><br>
-<font style = "color : white; font-family: montserrat">Skills&nbsp; : <b>&nbsp;&nbsp;&nbsp;&nbsp;HTML,&nbsp; CSS, &nbsp;PHP, &nbsp;mySQL,&nbsp; Bootstrap,&nbsp; Jquery,&nbsp; MJML,&nbsp; Adobe-Illustrator.</b></font>
-<br><br>
-<br><br>
-<br><br>
-<div id = "social">
-<ul class = "j-list" style = "display: inline; list-style-type: none;color: white">
-	<li><a href = "https://github.com/Jurshsmith"><i class="fab fa-github"></i></a></li>
-	<li><a href = "https://instagram.com/jurshsmith"><i class="fab fa-instagram"></i></a></li>
-	<li><a href = "https://twitter.com/jurshsmith"><i class="fab fa-twitter"></i></a></li>
-</ul>
-</div>
-<script type="text/javascript"> $('#social').hide();</script>
-
-</center><br><br>
-<img height = "100px" align = "right" style = "position: relative; top : -80px;" src="http://res.cloudinary.com/jurshsmith/image/upload/v1523649287/sign.png" alt = "jurshsmith">
-
-</body>
-<script type="text/javascript">
-$(document).ready(function(){
-$('.myimage').fadeIn(2500);
-$('#social').fadeIn(2800);
-});
 </script>
 
-</html>
+<!--my css starts here-->
+<style>
 
+#j-background{
+		background-color: #f4f4f4;
+				}
+
+#j-image{
+		border: 7px solid grey;
+		border-radius: 45px;
+		max-width: 100%;
+}
+#j-firstdiv{
+		border-right: 1px solid grey;
+		text-align: center;
+		font-family: montserrat;
+		color : grey;
+}
+.j-header{
+		font-family: montserrat;
+		color: grey;
+		font-weight: 370;
+}
+.j-center{
+		text-align: center;
+}
+
+
+/**chatbot css here**/
+#show-chatbot{
+	
+	background: grey;
+	color: white;
+	position: fixed;
+	right: 4px;
+	bottom: 4px;
+	padding: 5px;
+	display: inline;
+	z-index: 250;
+
+}
+#chat-interface{
+
+	background-color: red;
+	height: 300px;
+	width: 300px;
+	position: fixed;
+	right: 4px;
+	bottom: 33px;
+	z-index: 23;
+
+}
+.add-transparency{
+	background-color: #d0d0d0 !important;
+}
+#chatbot-header{
+	background-color: #a39c9c !important;
+	font-size: 16px;
+}
+#chatbot-header span{
+	padding : 13px;
+}
+#chatbot-footer{
+	position: fixed;
+	right: 4px;
+	bottom: 33px;
+	background-color: #a39c9c;
+
+}
+#chatbot-footer span{
+
+	padding : 10px;
+
+}
+.chatbot-text-area{
+	position: fixed;
+	bottom:  20px;
+	width: 264px;
+}
+</style>
+</head>
+<body>
+<!--chatbot html here-->
+<a id = "j-show-chatbot" style = "color:white" href = "#">
+<div id = "show-chatbot">
+	<div class = "row">
+		
+		<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
+ <span class="glyphicon glyphicon-user"></span> &nbsp;&nbsp;Click toggle chatbot
+		</div>
+	</div>
+</div>
+</a>
+<div id = "chat-interface" style = "background-color: transparent">
+
+	<div id = "chatbot-header">
+		<span class="glyphicon glyphicon-fullscreen"></span>
+	 	<span style = "position: relative; left : 200px" class="glyphicon glyphicon-remove-sign"></span>
+	</div>
+
+
+	<form class = "chatbot-text-area">
+			<div class="form-group">
+  
+  <input type="text" class="form-control" id="usr">
+				</div></form>
+	<div id = "chatbot-footer">
+	
+		<span class="glyphicon glyphicon-send"></span>
+		
+
+	</div>
+
+
+</div>
+<!-- chatbot 
+html 
+ends here -->
+
+<div id = "j-background">
+	<div class = "row">
+		<div id = "j-firstdiv" class = "col-lg-5 col-md-5 col-sm-12 col-xs-12">
+			<br><br>
+			<center><img  src = "images/josh.png" width = "350px" id = "j-image"></center><br><br>
+			<font class = "j-profile">  <span class="glyphicon glyphicon-user"></span>  &nbsp;</font>
+			<font class = "j-profile" style = "font-weight: 600;">OLADELE JOSHUA</font><br>
+			 <font class = "j-header" style = "font-weight: 250;">
+			 <span class="glyphicon glyphicon-ok-sign"></span> &nbsp;
+			 	HNG INTERN</font><br>
+			<br><br>
+			<br><br>
+			<br><br>
+			
+
+			 	<a href = "https://github.com/Jurshsmith"><i class="fab fa-github j-profile"></i></a>
+			 	<a href = "https://github.com/Jurshsmith"><i class="fab fa-instagram j-profile"></i></a>
+			 	<a href = "https://github.com/Jurshsmith"><i class="fab fa-twitter j-profile"></i></a>
+
+			<br><br>
+		</div>
+		<div class = "col-lg-7 col-md-7 col-sm-12 col-xs-12">
+			<br><br>
+			<div id = "j-story">
+			<font class = "j-header">ABOUT ME &nbsp;&nbsp;    <span class="glyphicon glyphicon-pencil"></span></font>
+			<br><br><br><br><br><br>
+			<br><br><br><br><br><br>
+			<font class = "j-header">MY QUALIFICATIONS&nbsp;&nbsp;<span class="glyphicon glyphicon-wrench"></span></font>
+			<br><br><br><br><br><br>
+			<br><br><br><br><br><br>
+			<font class = "j-header">MY SKILLS &nbsp;&nbsp;<span class="glyphicon glyphicon-briefcase"></span></font>
+			<br><br><br><br><br><br>
+			<br><br><br><br><br><br>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+
+</body>
+</html>
+<!--css js-->
+<script type = "text/javascript">
+$(document).ready(
+function(){
+  setInterval(function(){
+var width = $(document).innerWidth();
+if(width < 1000 )
+{
+  $('.j-profile').css({"font-size": "20px"});
+  $('.j-header').css({"font-size": "16px"});
+  $('#j-story').css({"text-align":"center"});
+
+
+
+
+}
+else{
+  $('.j-profile').css({"font-size": "26px"});
+  $('#j-story').css({"text-align":"left"});
+
+
+
+}
+
+  },6);
+}
+  );
+</script>
+
+
+
+
+<!-- chatbot js -->
+<script type="text/javascript">
+$('#chat-interface').hide();
+
+
+$('#show-chatbot').click(function(){
+$('#chat-interface').addClass('add-transparency');
+
+$('#chat-interface').toggle(500);
+
+
+});
+
+</script>
