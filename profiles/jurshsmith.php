631bc45fc4ecf8e1a147c7d9318175394f9bbbee
Jurshsmith <jurshsmith@gmail.com>
2018-04-19 06:19:31 -0700

    he

--- profiles/jurshsmith.php
+++ profiles/jurshsmith.php
@@ -33,14 +33,16 @@ HNG 4.0 | Jurshsmith
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 <!--Javascript starts here-->
 <script>
-
+$('document').ready(function(){
+	$('.list-inline-items').hide();
+});
 </script>
 
 <!--my css starts here-->
 <style>
 html,body{
 		background-color: #f4f4f4;
-	
+
 }
 
 #j-background{
