55c13dd487cb354f47d577311689c5b2f7afddc8
Jamez <jamezblack19@gmail.com>
2018-04-19 23:42:43 +0100

    new

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hnfun");
+define ('DB_PASSWORD', "fdf");
+define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
 
 ?>
