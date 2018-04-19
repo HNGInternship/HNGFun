3cf034cb92a013ce27dc3d060ef71552e2f9f67f
Travis CI <travis@travis-ci.org>
2018-04-19 15:36:20 +0000

    committed at 4b5fe7c [ci skip]

--- profiles/Adokiye.php
+++ profiles/Adokiye.php
@@ -1,6 +1,13 @@
 <?php
-require __DIR__ . '\..\db.php';
-include __DIR__ . '\..\answers.php';
+include "../answers.php";
+	if(!defined('DB_USER')){
+			require "../../config.php";		
+			try {
+			    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+			} catch (PDOException $pe) {
+			    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+			}
+		}
 ?>
 <!doctype html>
 <html>
