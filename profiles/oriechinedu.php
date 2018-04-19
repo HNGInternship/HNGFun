ea0b3a4d2b258692fcdb9efd6a6087bde3ebf8d8
oriechinedu <oriechinedu>
2018-04-19 11:13:25 +0100

    modify

--- profiles/oriechinedu.php
+++ profiles/oriechinedu.php
@@ -1,13 +1,9 @@
 <?php 
     date_default_timezone_set('Africa/Lagos');
 
-    preg_match('/\(\((.*)\)\)/', 'are you ((sure))', $matches);
-
-    die(var_dump($matches[0]));
-
         if (!defined('DB_USER')){
             
-            require "../config.php";
+            require "../../config.php";
         }
         try {
             $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
