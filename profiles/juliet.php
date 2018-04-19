2d0a9f77713b7c77b88d39932547e13923e3d9aa
julietezekwe <julietezekwe@gmail.com>
2018-04-19 22:11:21 +0100

    chidimma

--- profiles/juliet.php
+++ profiles/juliet.php
@@ -1,6 +1,6 @@
 <?php
 
- include_once realpath(__DIR__ . '/..') . "/answers.php"; 
+ include_once("../answers.php"); 
 
 if (!defined('DB_USER')){
             
@@ -80,7 +80,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   if ($_POST['msg'] == 'commands') {
     $reply= 'These are my commands <p>1. what is my location, 2. tell me about your author, 3. open facebook, 6. open twitter, 7. open linkedin, 8. shutdown my pc, 9. get my pc name.</p>';
   } 
-      else if($reply==""){
+      if($reply==""){
        
      $reply = assistant($_POST['msg']);
        
