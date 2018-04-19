f3784c8618700a01824ae128233f9a2b81d4bb99
Lois <lois.idzi@gmail.com>
2018-04-19 15:35:18 +0100

    Modified

--- profiles/techHajiya.php
+++ profiles/techHajiya.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title><?$user->name;?></title>
+ <title>Lois Thomas</title>
     <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../vendor/font-awesome/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Josefin%20Sans:400,500,600,700" rel='stylesheet' type='text/css' />
