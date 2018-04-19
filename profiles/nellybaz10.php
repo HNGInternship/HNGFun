85d6601653932aaa59d1a989729556f5b1937b7b
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-04-19 17:27:09 +0200

    update

--- profiles/nellybaz10.php
+++ profiles/nellybaz10.php
@@ -105,7 +105,7 @@ while ($row2 = mysqli_fetch_array($result)) {
 				//
 				var input = $('#input').val();
 				//alert(input);
-				$('#bot-display').load('/profiles/nnzzion.php', {
+				$('#bot-display').load('nnzzion.php', {
 					question: input
 				});
 
