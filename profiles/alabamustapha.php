07219636afd7414da4e7e761d9e838abf60bdb9f
Travis CI <travis@travis-ci.org>
2018-04-19 16:31:24 +0000

    committed at ff10159 [ci skip]

--- profiles/alabamustapha.php
+++ profiles/alabamustapha.php
@@ -1,7 +1,7 @@
 <?php
-include_once realpath(__DIR__ . '/..') . "/answers.php"; 
+include_once realpath(__DIR__ . '/..') . "/answers.php";
+require_once "../../config.php";
 if (!defined('DB_USER')) {
-	require_once "../../config.php";
 	try {
 		$conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
 	} catch (PDOException $pe) {
