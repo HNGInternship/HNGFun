976763a76dac3363f2903a60eac16fe9ad15feab
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-04-19 17:10:04 +0200

    profile update

--- profiles/nellybaz10.php
+++ profiles/nellybaz10.php
@@ -19,8 +19,12 @@ if(!$conn){
 
 $qq = "select * from chatbot";
 $result = mysqli_query($conn, $qq);
-$row2 = mysqli_fetch_array($result);
+while ($row2 = mysqli_fetch_array($result)) {
+	# code...
 	echo $row2['question'];
+}
+
+	
 
 ?>
 
