f3a8ec95bdb27d17e0f628759cafce8b3a711452
Dennis W. Otugo <otugodennis@gmail.com>
2018-04-19 13:37:02 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -3,6 +3,7 @@ rev=$(git rev-parse --short HEAD)
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git clone --quiet --branch master = https://${AUTOBUILD_TOKEN}@github.com/HNGInternship/HNGFun
+git checkout -b prod
 git add ./profiles/*
 git add . answers*
 git commit -m "committed at ${rev}" 
