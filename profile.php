dada1c9f58938077e27588e41bd3111af06bf85e
mercyikpe <ikpemercy1@gmail.com>
2018-04-19 09:47:07 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 <?php
     include_once("header.php");
 
-   echo $profile_name = $_GET['id'];
+   $profile_name = $_GET['id'];
 	$secret_word = "sample_secret_word";
 
     require 'db.php';
@@ -12,6 +12,8 @@
 <body class = 'profile'>
 
 <div class="container">
+	
+	<?php readfile('profiles/' . $profile_name. '.php'); ?>
     <?php include_once('profiles/' . $profile_name. '.php');
 
   try {
