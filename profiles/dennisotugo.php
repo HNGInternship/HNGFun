d796c21b84b3f553510c9896b893382dc7dae566
Travis CI <travis@travis-ci.org>
2018-04-20 09:38:28 +0000

    committed at b6d736fc [ci skip]

--- profiles/dennisotugo.php
+++ profiles/dennisotugo.php
@@ -324,6 +324,61 @@ $(document).ready(function(){
     })
   }
 });
+	
+	
+	
+var trigger = [
+	["aboutbot"]
+];
+var reply = [
+	["v1.1.0"]
+];
+var alternative = ["Haha...", "Eh..."];
+document.querySelector("#input").addEventListener("keypress", function(e){
+	var key = e.which || e.keyCode;
+	if(key === 13){ //Enter button
+		var input = document.getElementById("input").value;
+		document.getElementById("user").innerHTML = input;
+		output(input);
+	}
+});
+function output(input){
+	try{
+		var product = input + "=" + eval(input);
+	} catch(e){
+		var text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, ""); //remove all chars except words, space and 
+		text = text.replace(/ a /g, " ").replace(/i feel /g, "").replace(/whats/g, "what is").replace(/please /g, "").replace(/ please/g, "");
+		if(compare(trigger, reply, text)){
+			var product = compare(trigger, reply, text);
+		}
+	}
+	document.getElementById("message bot").innerHTML = product;
+	speak(product);
+	document.getElementById("message-box").value = ""; //clear input value
+}
+function compare(arr, array, string){
+	var item;
+	for(var x=0; x<arr.length; x++){
+		for(var y=0; y<array.length; y++){
+			if(arr[x][y] == string){
+				items = array[x];
+				item =  items[Math.floor(Math.random()*items.length)];
+			}
+		}
+	}
+	return item;
+}
+document.getElementById("message bot").innerHTML;
+function speak(string){
+	var utterance = new SpeechSynthesisUtterance();
+	utterance.voice = speechSynthesis.getVoices().filter(function(voice){return voice.name == "Agnes";})[0];
+	utterance.text = string;
+	utterance.lang = "en-US";
+	utterance.volume = 1; //0-1 interval
+	utterance.rate = 1;
+	utterance.pitch = 1; //0-2 interval
+	speechSynthesis.speak(utterance);
+}
 </script>
 <?php } 
 ?>
