b695aadbbcd851ee634d6a4c4b552da2d4ea2062
Adokiye <38386612+Adokiye@users.noreply.github.com>
2018-04-20 13:49:49 +0100

    Update Adokiye.php

--- profiles/Adokiye.php
+++ profiles/Adokiye.php
@@ -1,6 +1,15 @@
 <?php
 global $conn;
+if(!defined('DB_USER')){
+    require "../../config.php";
+}
+try {
+     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+} catch (PDOException $pe) {
+    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+}
 function processAskedQuestion($input){
+    
     if(validateTrain_function($input)){
         list($trim, $question) = explode(":", $input);
         $question = trim($question, " ");
