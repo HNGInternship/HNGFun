e5e6563ebcb6a0d72895537995de658246310a34
Abolarin Olanrewaju Olabode <jabilegbe@gmail.com>
2018-04-20 09:48:53 +0100

    fixed my bot

--- db.php
+++ db.php
@@ -16,17 +16,12 @@
 # LOCAL ENVIRONMENT
 # require_once 'config.php';
 
-<<<<<<< HEAD
-//<<<<<<< HEAD
-require_once 'config.php';
+
+require_once '../config.php';
 //=======
 # PRODUCTION
 //require_once '../config.php';
-//>>>>>>> 7b1b8d9655ab6d4304112710d1afcc7a66ae1d35
-=======
-# PRODUCTION
-require_once '../config.php';
->>>>>>> fd9b122a5b6f212003a947cab91714cde2dd93da
+
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
