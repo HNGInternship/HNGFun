2ef9f51b7ec28850328c1c97877370c9f82e39a9
Etibless <etiblessbiz@gmail.com>
2018-04-20 15:04:46 +0100

    profiles/etibless.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hng");
+define ('DB_PASSWORD', "root");
+define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
 
 ?>
