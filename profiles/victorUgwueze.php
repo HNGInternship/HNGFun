b12ea86c57c50d884ac727d95758c993814b21fb
Travis CI <travis@travis-ci.org>
2018-04-20 10:43:43 +0000

    committed at 147ec64 [ci skip]

--- profiles/victorUgwueze.php
+++ profiles/victorUgwueze.php
@@ -1,16 +1,17 @@
 
 <?php
 try {
-    if (!isset($conn)){
+    if (!defined('DB_USER')){
             
         require "../../config.php";
     }
     try {
         $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-    } catch (PDOException $pe) {
+      } catch (PDOException $pe) {
         die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-    }
-    global $conn;
+      }
+       global $conn;
+
 
     $sql ="SELECT * FROM interns_data WHERE username = 'victorUgwueze' LIMIT 1";
     $q = $conn->query($sql);
