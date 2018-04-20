925b3fe7836647a3d6cdab64a261cfb66779325b
Abolarin Olanrewaju Olabode <jabilegbe@gmail.com>
2018-04-20 16:20:55 +0100

    fixed db again, dont change this, move your config outside this directory

--- db.php
+++ db.php
@@ -1,16 +1,5 @@
 <?php
-<<<<<<< HEAD
 
-
-require_once 'config.php';
-
-
-
-
- require_once 'config.php';
-
-
-require_once '../config.php';
 //=======
 # PRODUCTION
 //require_once '../config.php';
@@ -18,10 +7,6 @@ require_once '../config.php';
 
 require_once '../config.php';
 
-=======
-
-require_once '../config.php';
->>>>>>> 947d1a10467c2048c1a746058f139e30db52e43c
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
