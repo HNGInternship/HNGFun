3d55b5914c11d949551914ec8742ee9cdd59500b
victorsteven <chikodi543@gmail.com>
2018-04-20 08:51:48 +0100

    Steven's Bot

--- profiles/Steven.php
+++ profiles/Steven.php
@@ -1,5 +1,15 @@
-<?php
-    require_once('../db.php');
+<?php 
+    date_default_timezone_set('Africa/Lagos');
+
+        if (!defined('DB_USER')){
+            
+            require "../../config.php";
+        }
+        try {
+            $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+          } catch (PDOException $pe) {
+            die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+          }
 
     try {
     $sql = 'SELECT * FROM secret_word';
