e0f52d58e35d2b6405847449e57db2eb7964d607
James John James <jamesjay4199@gmail.com>
2018-04-20 13:22:37 +0100

    debugging

--- profiles/jaycodes.php
+++ profiles/jaycodes.php
@@ -31,9 +31,7 @@ if($_SERVER['REQUEST_METHOD'] === "GET"){
             return $data;
         }
         function chatMode($ques){
-
             require '../../config.php';
-
             $ques = test_input($ques);
             $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE );
             if(!$conn){
@@ -43,15 +41,72 @@ if($_SERVER['REQUEST_METHOD'] === "GET"){
                 ]);
                 return;
             }
-        
             $query = "SELECT answer FROM chatbot WHERE question LIKE '$ques'";
             $result = $conn->query($query)->fetch_all();
-            
             echo json_encode([
                 'status' => 1,
                 'answer' => $result
             ]);
             return;
+        }
+        function trainerMode($ques){
+            $questionAndAnswer = substr($ques, 6); //get the string after train
+            $questionAndAnswer =test_input($questionAndAnswer); //removes all shit from 'em
+            $questionAndAnswer = preg_replace("([?.])", "", $questionAndAnswer);  //to remove all ? and .
+            $questionAndAnswer = explode("#",$questionAndAnswer);
+            if((count($questionAndAnswer)==3)){
+                $question = $questionAndAnswer[0];
+                $answer = $questionAndAnswer[1];
+                $password = $questionAndAnswer[2];
+            }
+            if(!(isset($password))|| $password != 'password'){
+                echo json_encode([
+                    'status'    => 1,
+                    'answer'    => "Please insert the correct training password"
+                ]);
+                return;
+            }
+            if(isset($question) && isset($answer)){
+                //Correct training pattern
+                $question = test_input($question);
+                $answer = test_input($answer);
+                if($question == "" ||$answer ==""){
+                    echo json_encode([
+                        'status'    => 1,
+                        'answer'    => "empty question or response"
+                    ]);
+                    return;
+                }
+                $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE );
+                if(!$conn){
+                    echo json_encode([
+                        'status'    => 1,
+                        'answer'    => "Could not connect to the database " . DB_DATABASE . ": " . $conn->connect_error
+                    ]);
+                    return;
+                }
+                $query = "INSERT INTO `chatbot` (`question`, `answer`) VALUES  ('$question', '$answer')";
+                if($conn->query($query) ===true){
+                    echo json_encode([
+                        'status'    => 1,
+                        'answer'    => "trained successfully"
+                    ]);
+                }else{
+                    echo json_encode([
+                        'status'    => 1,
+                        'answer'    => "Error training me: ".$conn->error
+                    ]);
+                }
+                
+
+                return;
+            }else{ //wrong training pattern or error in string
+            echo json_encode([
+                'status'    => 0,
+                'answer'    => "Wrong training pattern<br> PLease use this<br>train: question # answer"
+            ]);
+            return;
+            }
         }
 
         //end of function definition
