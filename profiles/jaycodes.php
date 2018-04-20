9ad85050f9201935bad8d6ea4f0079ce014c330e
James John James <jamesjay4199@gmail.com>
2018-04-20 12:42:14 +0100

    debugging

--- profiles/jaycodes.php
+++ profiles/jaycodes.php
@@ -47,6 +47,13 @@ if($_SERVER['REQUEST_METHOD'] === "GET"){
         }
 
         //end of function definition
+        require_once '../config.php';
+
+        try {
+            $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+        } catch (PDOException $pe) {
+            die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+        }
         $ques = test_input($_POST['ques']);
         if(strpos($ques, "train:") !== false){
             trainerMode($ques);
