74ef914f18611a9a2a702724d8d1138def98998b
Travis CI <travis@travis-ci.org>
2018-04-21 07:09:06 +0000

    committed at 0331ddd [ci skip]

--- profiles/Adokiye.php
+++ profiles/Adokiye.php
@@ -1,4 +1,10 @@
 <?php
+
+session_start();
+
+if (!isset($_SESSION["all"])){
+    $_SESSION["all"] = [];
+}
 if(!defined('DB_USER')){
     require "../../config.php";
     $servername = DB_HOST;
@@ -12,10 +18,18 @@ if(!defined('DB_USER')){
         die("Connection failed: " . mysqli_connect_error());
     }}
 global $conn;
+
+$solution = '';
+if (isset($_POST['restart'])){
+    session_destroy();
+}
 if (isset($_POST['button'])) {
     if (isset ($_POST['input']) && $_POST['input'] !== "") {
         $asked_question_text = $_POST['input'];
-        $solution = askQuestion($asked_question_text);}}
+        $solution = askQuestion($asked_question_text) . "<br/>";
+        $_SESSION["all"][] = array($solution, $asked_question_text);
+    }
+}
 function askQuestion($input)
 {
     $split = preg_split("/(:|#)/", $input, -1);
@@ -23,7 +37,7 @@ function askQuestion($input)
     $action = "train";
     if ($split[0] !== $action && !isset($split[1]) && !isset($split[2])) {
         $question = strtolower($split[0]);
-        $sql = 'SELECT answer FROM chatbot WHERE question = "' . $question . '"';
+        $sql = 'SELECT answer FROM chatbot WHERE LOWER(question) = "' . $question . '"';
         $result = $conn->query($sql);
             $fetched_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
             $row_cnt = $result->num_rows;
@@ -32,16 +46,20 @@ function askQuestion($input)
             } else
             return "ENTER TRAIN: QUESTION#ANSWER TO ADD MORE QUESTIONS TO THE DATABASE";
     }else if  ($split[0] == $action && isset($split[1]) && isset($split[2])) {
-            $sql = "INSERT INTO chatbot(question, answer) VALUES ('" . $split[1] . "', '" . $split[2] . "')";
-            $the_queried = $conn->query($sql);
-            if ($the_queried){
+            $question = strtolower($split[1]);
+            $sql1 = 'SELECT question FROM chatbot WHERE LOWER(question) = "' . $question . '"';
+            $query = $conn->query($sql1);
+            if ($query) {
+                return "Question already exists, please train again";
+            }else
+                $sql = "INSERT INTO chatbot(question, answer) VALUES ('" . $split[1] . "', '" . $split[2] . "')";
+                $the_queried = $conn->query($sql);
+                if ($the_queried){
                 $saved_message = "Saved " . $split[1] ." -> " . $split[2];
                 return $saved_message;
-            }else
+                }else
                 return "Please try again";
     }
-
-
 }
 
 ?>
@@ -51,6 +69,39 @@ function askQuestion($input)
     <meta charset="utf-8">
     <title>Adokiye ---- Stage 4</title>
     <style type="text/css">
+
+                              .mycss
+                              {
+                                  text-shadow:1px 3px 1px rgba(255,255,255,1);font-weight:bold;text-transform:uppercase;color:#000000;border: 5px ridge #FFFFFF;letter-spacing:5pt;word-spacing:2pt;font-size:20px;text-align:center;font-family:arial, helvetica, sans-serif;line-height:1;
+                              }
+
+        .bot-css {
+            display: inline-block;
+            -webkit-box-sizing: content-box;
+            -moz-box-sizing: content-box;
+            box-sizing: content-box;
+            width: 300px;
+            height: fit-content;
+            padding: 18px 0;
+            border: none;
+            -webkit-border-radius: 2px;
+            border-radius: 2px;
+            font: normal normal bold 16px/1 "Times New Roman", Times, serif;
+            color: rgba(10,6,6,1);
+            text-align: center;
+            -o-text-overflow: ellipsis;
+            text-overflow: ellipsis;
+            background: rgba(190,216,226,1);
+            -webkit-box-shadow: 2px 3px 0 2px rgba(201,201,211,1) ;
+            box-shadow: 2px 3px 0 2px rgba(201,201,211,1) ;
+            text-shadow: 0 1px 2px rgba(255,255,255, 0.5) , 3px 2px 1px rgba(0,0,0,0.2) ;
+            -webkit-transition: font-size 200ms cubic-bezier(0.42, 0, 0.58, 1) 10ms;
+            -moz-transition: font-size 200ms cubic-bezier(0.42, 0, 0.58, 1) 10ms;
+            -o-transition: font-size 200ms cubic-bezier(0.42, 0, 0.58, 1) 10ms;
+            transition: font-size 200ms cubic-bezier(0.42, 0, 0.58, 1) 10ms;
+            -webkit-transform: rotateX(-1.64deg) rotateY(-3.4377467707849396deg)   ;
+            transform: rotateX(-1.64deg) rotateY(-3.4377467707849396deg)   ;
+        }
         #div_main {
             width: 980px;
             margin-right: auto;
@@ -59,8 +110,9 @@ function askQuestion($input)
             text-align: center;
             background-image: url(http://res.cloudinary.com/gorge/image/upload/v1523960257/Internships-1.png);
             height: auto;
+            padding-bottom: 1px;
+            //
         }
-
         #header {
             background-color: #FFFFFF;
             width: 980px;
@@ -142,21 +194,29 @@ try {
         <p style="font-style: normal; font-weight: bold;">NAME : <?php echo $name ?></p>
         <p style="font-weight: bold">USERNAME : <?php echo $username ?></p>
     </div>
-    Chatbot by Adokiye<br />
+    <p class="mycss"> Chatbot by Adokiye</p><br />
     <form name = "askMe" method="post">
         <p>
             <label>
                 <input name="input" type="text" class="tb5" placeholder="Chat with me! Press Ask to send.">
             </label><label>
-                <input name="button" type="submit" class="fb7" id="button" value="ASK">
+                <input name="button" type="submit" class="fb7" id="button" value="ASK"><label>
+                    <input name="restart" type="submit" class="fb7" id="button" value="Restart">
+                </label>
             </label>
             <br />
 
         </p>
         <p>&nbsp;</p>
     </form>
-    <p style=" color: #000000;font-family: arial, sans-serif; font-size: 14px;font-weight: bold;letter-spacing: 0.3px;"><span style="font-weight: bolder">BOT :::  <?php echo $solution?></span></p>
+            <div class="bot-css"> <?php foreach($_SESSION["all"] as list($asked,$soln )){ ?>
+                <span style="color:blue"><?=  "YOU : $soln <br/>";echo "</span>";
+                echo "BOT : $asked<br/>" ?><br/><?php } ?><br/>
+        </div>
+    <p>
 
+
+    </p>
 </div>
 </body>
 </html>
