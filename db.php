8c14514894c6628be43239325e8cbdd80baee9c2
Tosin <alldots2005@gmail.com>
2018-04-21 15:53:51 -0300

    Minor merge

--- db.php
+++ db.php
@@ -1,23 +1,8 @@
 <?php
-<<<<<<< HEAD
+
 
 require_once 'config.php';
 
-
-
-
- require_once 'config.php';
-
-
-require_once '../config.php';
-//=======
-# PRODUCTION
-//require_once '../config.php';
-
-
-=======
-require_once '../config.php';
->>>>>>> b674a50fe54b0cbef9201a73a3e01f2ad9de9118
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
