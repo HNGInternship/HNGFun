e07461eabe04178251363b11507cb813cba397bc
Travis CI <travis@travis-ci.org>
2018-04-21 19:29:13 +0000

    committed at 66ea40ea [ci skip]

--- profiles/orinayo.php
+++ profiles/orinayo.php
@@ -3,20 +3,13 @@
 $user_input = "";
 $possible_questions = array();
 $sorted_possible_questions = array();
+require_once "../db.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // validate input
     $user_input = validate_input($_POST["userInput"]);
     try {
-        include_once "../config.php";
         include_once "../answers.php";
-        try {
-            $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE, DB_USER, DB_PASSWORD);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        } 
-        catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
 
         if (strpos($user_input, 'train') === 0) {
             $user_input = substr_replace($user_input, '', 0, 5);
