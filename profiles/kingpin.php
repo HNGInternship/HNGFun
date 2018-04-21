3466e71474f22621b3e1b72ed4275d2da3fc21fb
saraencci <franksaraencci@gmail.com>
2018-04-21 18:01:13 +0530

    reverti  rieta

--- profiles/kingpin.php
+++ profiles/kingpin.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once('../db.php');
-
  if (isset($_POST))
  {
 
@@ -74,15 +72,6 @@ require_once('../db.php');
 <link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Overpass">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-
-
-
-
-
-
-
-
 
 <style type="text/css">
 
