723f9264c39cc2ce85d1f86f4f94b7e9f5f2dc0e
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-21 12:10:19 -0700

    MY CHATBOT

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -310,9 +310,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 					type: 'POST',
 					data: {question: question},
 					dataType: 'json',
-					success: function(answer){
-			        //$("#ans").append("<li>" + answer.result +  "</li>");
-			        alert(answer.result);
+					success: (response)=> {
+			        $("#ans").append("<li>" + response.result +  "</li>");
+			        //alert(answer.result);
 					},
 					error: function(error){
 						console.log(error);
