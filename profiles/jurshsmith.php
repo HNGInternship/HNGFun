e62e3737944616c827cef75ac5cb36507292a03d
Travis CI <travis@travis-ci.org>
2018-04-21 16:39:41 +0000

    committed at e19e862 [ci skip]

--- profiles/jurshsmith.php
+++ profiles/jurshsmith.php
@@ -432,7 +432,7 @@ reply = "<div href = 'index.php#chat-interface#bott"+ theScrollCounter +"' class
 else{
 
         //using ajax
-   $.post('j-replies.php',{phpques : chat}, function(data){
+   $.post('profiles/j-replies.php',{phpques : chat}, function(data){
    		reply = data;
    		reply = "<div href = 'index.php#chat-interface#bott"+ theScrollCounter +"' class = 'bot' id = 'bott" + theScrollCounter +"'><font class = 'bot-reply'>"+ reply + "</font><br></div>";
         $(".chats").append(reply);//should be after bot chat    
