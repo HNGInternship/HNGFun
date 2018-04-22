8f4bc97956bb0ad26bba40cccd4fe2f6148aee83
uncle-tee <tadenekan@byteworks.com.ng>
2018-04-22 09:18:29 +0100

    fully functional front end

--- profiles/uncletee.php
+++ profiles/uncletee.php
@@ -33,15 +33,14 @@ try {
 $response = [];
 $abusiveWords = ["crazy","stupid","4r5e", "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass", "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta", "bugger", "bum", "bunny fucker", "butt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "horniest", "horny", "hotsex", "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lust", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", "pussy", "pussys", "rectum", "retard", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen", "sex", "sh!+", "sh!t", "sh1t", "shag", "shagger", "shaggin", "shagging", "shemale", "shi+", "shit", "shitdick", "shite", "shited", "shitey", "shitfuck", "shitfull", "shithead", "shiting", "shitings", "shits", "shitted", "shitter", "shitters", "shitting", "shittings", "shitty", "skank", "slut", "sluts", "smegma", "smut", "snatch", "son-of-a-bitch", "spac", "spunk", "s_h_i_t", "t1tt1e5", "t1tties", "teets", "teez", "testical", "testicle", "tit", "titfuck", "tits", "titt", "tittie5", "tittiefucker", "titties", "tittyfuck", "tittywank", "titwank", "tosser", "turd", "tw4t", "twat", "twathead", "twatty", "twunt", "twunter", "v14gra", "v1gra", "vagina", "viagra", "vulva", "w00se", "wang", "wank", "wanker", "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx"];
 $apiKey = 'f9cca98bdc5344ce8508b4a6b8110c59';
+$botVersion = "agbero v1.0";
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
         header('Content-Type: application/json');
         if(isset($_POST["data"])){
-
+            $reponse = (!performTraining($_POST["data"]))?askQuestion($_POST["data"]):performTraining($_POST["data"]);
+            echo(json_encode($reponse));
         }
-         echo json_encode(askQuestion($_POST["data"]));
-
-
 }
 
 
@@ -58,11 +57,21 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 
 
-
-//print_r(askQuestion("hello"));
+//
+//print_r(askQuestion("botVersion"));
 function askQuestion($string){
-
+    global $botVersion;
     $questionAsked =  prepareInputParams($string);
+    $isInputEmpty = isInputEmpty($questionAsked);
+    if($isInputEmpty){
+        return $isInputEmpty;
+    }else if(strtolower($questionAsked) =='botversion'){
+        return [
+                'code'=>200,
+                'response'=>$botVersion
+        ];
+    }
+
     $questionHasAbusiveWords    =   _profanityCheck($questionAsked);
     $foundQuestion = findByQuestion($questionAsked);
 
@@ -252,6 +261,7 @@ function isTrainable($array){
 function prepareInputParams($string){
 
     $semiPrepared =  preg_replace('([\s]+)', ' ', trim($string));
+//    echo($semiPrepared)
     $prepared     =  preg_replace("([?.-])", "",trim($semiPrepared) );
     return    $prepared;
 }
@@ -417,20 +427,29 @@ function whiteSpaceNotExistInFunction($answer){
 
 
 /**
- * fuction can explode if there are many delimeters
+ * function can explode if there are many delimeters
  * @param $delimiters
  * @param $string
  * @return array
  */
-function multiexplode ($delimiters,$string) {
+function multiexplode($delimiters,$string) {
     $ready = str_replace($delimiters, $delimiters[0], $string);
     $ready = trim($ready,$delimiters[0]);
     $launch = explode($delimiters[0], $ready);
     return  $launch;
 }
 
+function isInputEmpty($input){
+    $errorMesaage = [
+            'You have to tell me what to do',
+            'o!!!!! Just tell me what to do, that makes more sense',
+            'I am a bot, perhaps I can only work based on instruction'
+    ];
 
 
+    return !$input?['code' => 204, 'response'=>$errorMesaage[rand(0,count($errorMesaage)-1)]]:false;
+
+}
 
 
 
@@ -728,7 +747,7 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
               box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.07);
           }
           .msg p {
-              font-size: 0.8rem;
+              font-size: 1.0rem;
               margin: 0 0 0.2rem 0;
               color: #777;
           }
@@ -845,6 +864,7 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
               position: relative;
               overflow: scroll;
               max-height: 500px;
+              min-height: 500px;
           }
 
           .default-col-md-4{
@@ -920,79 +940,23 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
              <div class="col-md-4 default-col-md-4"></div>
              <div class="col-md-4">
                  <div class="chat-bot-area">
-                     <ol class="chat">
+                     <ol id='chatBoard' class="chat">
                          <li class="other">
                              <div class="avatar"><img src="https://mybluerobot.com/wp-content/uploads/2013/10/MBR_avatar_Mathew-Halpern.jpg" draggable="false"/></div>
                              <div class="msg">
-                                 <p>Hola! Te vienes a cenar al centro? Te vienes a cenar al centro?</p>
+                                 <p>Hello, My name is Agbeto v.1, am pretty smart so do not doubt me</p>
+                                 <p>Less I forget please do not use any abusive word it irritates me so bad</p>
+                                 <p>To train, please use the format train:question#aswer#password</p>
+                                 <p>EXAMPLE:train:what is your name#my name is ((get_function)#password</p>
                                  <time>20:17</time>
                              </div>
                          </li>
-                         <li class="self">
-                             <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <p>Puff...</p>
-                                 <p>Aún estoy haciendo el contexto de Góngora... <emoji class="books"/></p>
-                                 <p>Mejor otro día</p>
-                                 <time>20:18</time>
-                             </div>
-                         </li>
-                         <li class="other">
-                             <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <p>Qué contexto de Góngora? <emoji class="suffocated"/></p>
-                                 <time>20:18</time>
-                             </div>
-                         </li>
-                         <li class="self">
-                             <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <p>El que mandó Marialu</p>
-                                 <p>Es para mañana...</p>
-                                 <time>20:18</time>
-                             </div>
-                         </li>
-                         <li class="other">
-                             <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <p><emoji class="scream"/></p>
-                                 <p>Pásamelo! <emoji class="please"/></p>
-                                 <time>20:18</time>
-                             </div>
-                         </li>
-                         <li class="self">
-                             <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <img src="https://i.imgur.com/QAROObc.jpg" draggable="false"/>
-                                 <time>20:19</time>
-                             </div>
-                         </li>
-                         <li class="other">
-                             <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <p>Gracias! <emoji class="hearth_blue"/></p>
-                                 <time>20:20</time>
-                             </div>
-                         </li>
-                         <div class="day">Hoy</div>
-                         <li class="self">
-                             <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
-                             <div class="msg">
-                                 <p>Te apetece jugar a Minecraft?</p>
-                                 <time>18:03</time>
-                             </div>
-                         </li>
-
-
-
-
-
 
                      </ol>
 
                  </div>
 
-                     <input id="textString" class="textarea" type="text" placeholder="Type here!"/>
+                     <input name="inputData" id="textString" class="textarea" type="text" placeholder="Write your message and press enter"/>
                      <div class="emojis"></div
 
 
@@ -1003,19 +967,68 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
     </section>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
   <script>
-      // Js function will get the input text.
-      $(document).keypress(function(event) {
-          var keycode = event.keyCode || event.which;
-          if(keycode == '13') {
-              var textString = $("#textString").val();
-              if (textString){
-                  console.log("you have to type something");
-              }else{
-                  console.log(textString);
-              }
 
+      $(document).ready(function() {
+          // process the form
+          function getCurrentTime(){
+              var today = new Date();
+              var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+              var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+              var dateTime = date+' '+time;
+              return dateTime;
           }
+          $(document).keypress(function(event) {
+              var keycode = event.keyCode || event.which;
+              if(keycode == '13'){
+                  // get the form data
+                  var formData = {'data': $('input[name=inputData]').val()};
+                  console.log(formData.data);
+                  var time = getCurrentTime();
+                  $('#chatBoard').append('<li class="self"><div class="avatar"> <img src="https://i.imgur.com/HYcn9xO.png" draggable="false" /></div><div id="response" class="msg"><p>'+formData.data+'</p> <time>'+time+'</time></div></li>')
+                  // process the form
+                  $.ajax({
+                      type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+                       url         : 'profiles/uncletee.php', // the url where we want to POST
+                       data        : formData, // our data object
+                      dataType    : 'json', // what type of data do we expect back from the server
+                      encode          : true
+
+                  })
+                  // using the done promise callback
+                      .done(function(data) {
+
+                          // log data to the console so we can see
+
+                          var time = getCurrentTime();
+                          console.log(data.response);
+                          if(data.response == 204){
+                              $('#chatBoard').fadeIn(400).append('<li class="other"><div class="avatar"> <img src="https://mybluerobot.com/wp-content/uploads/2013/10/MBR_avatar_Mathew-Halpern.jpg" draggable="false" /></div><div class="msg"><p>'+data.response+'</p> <time>'+time+'</time></div></li>')
+                              $('#chatBoard').fadeIn(400).append('<li class="other"><div class="avatar"> <img src="https://mybluerobot.com/wp-content/uploads/2013/10/MBR_avatar_Mathew-Halpern.jpg" draggable="false" /></div><div class="msg"><p>To train, please use the format train:question#aswer#password</p><p>EXAMPLE:train:what is your name#my name is ((get_function)#password</p><p>Less I forget please do not use any abusive word it irritates me so bad</p> <time>time</time></div></li>')
+                          }else{
+                              $('#chatBoard').fadeIn(400).append('<li class="other"><div class="avatar"> <img src="https://mybluerobot.com/wp-content/uploads/2013/10/MBR_avatar_Mathew-Halpern.jpg" draggable="false" /></div><div class="msg"><p>'+data.response+'</p> <time>'+time+'</time></div></li>')
+                          }
+
+
+
+                          // here we will handle errors and validation messages
+                      })
+                      // using the fail promise callback
+                      .fail(function(data) {
+
+                          // log data to the console so we can see
+                          console.log(data);
+
+                          // here we will handle errors and validation messages
+                      });
+
+
+                  // stop the form from submitting the normal way and refreshing the page
+                  event.preventDefault();
+              }
+          });
       });
+
+
 
 
   </script>
