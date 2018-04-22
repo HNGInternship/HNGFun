1a438240605166629bb34f40351d100db4db4f66
Travis CI <travis@travis-ci.org>
2018-04-22 16:04:52 +0000

    committed at a6b1ac2 [ci skip]

--- profiles/Adokiye.php
+++ profiles/Adokiye.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION["all"])){
     $_SESSION["all"] = [];
 }
 if(!defined('DB_USER')){
-    require_once __DIR__."../../config.php";
+    require "../../config.php";
     $servername = DB_HOST;
     $username_ = DB_USER;
     $password = DB_PASSWORD;
@@ -37,7 +37,7 @@ function askQuestion($input)
     $action = "train";
     $question = strtolower($split[0]);
     if ($split[0] !== $action && !isset($split[1]) && !isset($split[2])) {
-        $result =  mysqli_query($conn,"SELECT answer FROM chatbot WHERE LOWER(question) like '%$question%'" );
+        $result =  mysqli_query($conn,"SELECT * FROM chatbot WHERE LOWER(question) like '%$question%'" );
         $fetched_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
         $row_cnt = $result->num_rows;
         $rand = rand(0,$row_cnt-1);
@@ -245,7 +245,7 @@ try {
         <p style="font-style: normal; font-weight: bold;">NAME : <?php echo $name ?></p>
         <p style="font-weight: bold">USERNAME : <?php echo $username ?></p>
     </div>
-    <p class="mycss"> Chatbot by Adokiye!!!<br />Click on show below to display the password for training me</p><br /><button onclick="show_function()" class = "fb7" >SHOW</button>
+    <p class="mycss"> Chatbot by Adokiye<br />Click on show below to display the password for training me</p><br /><button onclick="show_function()" class = "fb7" >SHOW</button>
     <form name = "askMe" method="post">
         <p>
             <label>
