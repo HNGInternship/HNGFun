21a967f4510001a2df9bcdf72b9f7cc252dffd65
bukola <bukola@wemove.co>
2018-04-22 11:46:22 +0100

    adjust case sensitivity

--- profiles/bukola.php
+++ profiles/bukola.php
@@ -56,9 +56,9 @@
             }
 
             switch(true){
-                case "ussd:" === substr($question, 0, 5):
-                case "Ussd:" === substr($question, 0, 5):
-                case "USSD:" === substr($question, 0, 5):
+                case "ussd " === substr($question, 0, 5):
+                case "Ussd " === substr($question, 0, 5):
+                case "USSD " === substr($question, 0, 5):
                     sendReply(getUSSD(substr($question, 6)));
             }
 
