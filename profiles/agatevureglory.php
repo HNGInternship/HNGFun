34854f243d7e4a6b7fc2f87ea065c4f9acba4d67
Travis CI <travis@travis-ci.org>
2018-04-22 23:52:41 +0000

    committed at f0f1a8e [ci skip]

--- profiles/agatevureglory.php
+++ profiles/agatevureglory.php
@@ -1,8 +1,7 @@
 <?php
 
 		require_once '../../config.php';
-		require "../answers.php";
-
+		
 		try {
 		    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
