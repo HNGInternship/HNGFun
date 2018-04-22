c1cefb695af7999eb45a160153d884734bd70772
Travis CI <travis@travis-ci.org>
2018-04-22 13:26:30 +0000

    committed at ed429c7 [ci skip]

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -105,12 +105,40 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 		        $index = rand(0, count($rows)-1);
 		        $row = $rows[$index];
 		        $answer = $row['answer'];
-		        
-		        echo json_encode([
-		        	'status' => 1,
-		        	'answer' => $answer
-		        ]);
-		        return;
+		        // check if answer is a function.
+		        $index_of_parentheses = stripos($answer, "((");
+		        if($index_of_parentheses === false){// if answer is not to call a function
+		        	echo json_encode([
+			        	'status' => 1,
+			        	'answer' => $answer
+			        ]);
+			        return;
+		        }else{//otherwise call a function. but get the function name first
+		            $index_of_parentheses_closing = stripos($answer, "))");
+		            if($index_of_parentheses_closing !== false){
+		                $function_name = substr($answer, $index_of_parentheses+2, $index_of_parentheses_closing-$index_of_parentheses-2);
+		                $function_name = trim($function_name);
+		                if(stripos($function_name, ' ') !== false){ //if method name contains spaces, do not invoke method
+		                   echo json_encode([
+		                    'status' => 0,
+		                    'answer' => "The function name should not contain white spaces"
+		                  ]);
+		                  return;
+		                }
+			            if(!function_exists($function_name)){
+			              echo json_encode([
+			                'status' => 0,
+			                'answer' => "I am sorry but I could not find that function"
+			              ]);
+			            }else{
+			              echo json_encode([
+			                'status' => 1,
+			                'answer' => str_replace("(($function_name))", $function_name(), $answer)
+			              ]);
+			            }
+			            return;
+		            }
+		        }    
 		    }else{
 
 		    	echo json_encode([
