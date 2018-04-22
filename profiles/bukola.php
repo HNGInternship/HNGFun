58b2e2feed8b0405359c8047b19a6386167f7539
bukola <bukola@wemove.co>
2018-04-22 08:17:22 +0100

    another attempt to fix server error

--- profiles/bukola.php
+++ profiles/bukola.php
@@ -1,4 +1,14 @@
 <?php
+    if(!defined('DB_USER')){
+        require "../../config.php";		
+        try {
+            $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+        } catch (PDOException $pe) {
+            die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+        }
+    }
+    global $conn;
+    
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
         try {
             $sql = 'SELECT * FROM secret_word LIMIT 1';
