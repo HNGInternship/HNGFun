5826763f155828558da15280a0773d40a83cbfa4
Jurshsmith <34186192+Jurshsmith@users.noreply.github.com>
2018-04-21 22:58:59 -0700

    Update jurshsmith.php
    edits

--- profiles/jurshsmith.php
+++ profiles/jurshsmith.php
@@ -1,7 +1,7 @@
-<?php
+<?php 
+		
+	?>
 
-  
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,36 +12,69 @@
 <title>
 HNG 4.0 | Jurshsmith
 </title>
+<script type = "text/javascript" src = "there/resource/jquery.js"></script>
+<script type = "text/javascript" src = "there/resource/jquery.min.js"></script>
+<script type = "text/javascript" src = "there/resource/jquery-ui.min.js"></script>
+<script type = "text/javascript" src = "there/resource/resource/jquery-ui.js"></script>
+<script type = "text/javascript" src = "there/resource/bootstrap.min.js"></script>
+<script type = "text/javascript" src = "there/resource/bootstrap.js"></script>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel = "stylesheet" href = "there/resource/bootstrap.css">
+<link rel = "stylesheet" href = "there/resource/bootstrap.min.css">
+<link rel = "stylesheet" href = "thereresource/bootstrap-theme.min.css">
+<link rel = "stylesheet" href = "there/resource/font-awesome.min.css">
+<link rel = "stylesheet" href = "there/resource/font-awesome.css">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"
+  integrity="sha256-xNjb53/rY+WmG+4L6tTl9m6PpqknWZvRt0rO1SRnJzw="
+  crossorigin="anonymous"></script>
+
+<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/color/jquery.color-2.1.2.js"
+  integrity="sha256-1Cn7TdfHiMcEbTuku97ZRSGt2b3SvZftEIn68UMgHC8="
+  crossorigin="anonymous"></script>
+<script type="text/javascript" src = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 <!--Javascript starts here-->
 <script>
-$('document').ready(function(){
-	$('.mx-auto').hide();
-});
+
 </script>
 
 <!--my css starts here-->
 <style>
+html,body{
+	background-color: #f4f4f4;
+}
 
 #j-background{
 		background-color: #f4f4f4;
 				}
 
 #j-image{
-		border: 7px solid grey;
+		border: 7px solid #0c779e;
 		border-radius: 45px;
 		max-width: 100%;
 }
@@ -56,7 +89,7 @@ $('document').ready(function(){
 }
 .j-header{
 		font-family: montserrat;
-		color: grey;
+		color: #0c779e;
 		font-weight: 370;
 }
 .j-center{
@@ -127,138 +160,319 @@ $('document').ready(function(){
 	
 
 }
-
- /*p:nth-child(even){
-	background: green;
-}*/
-.add-transparency{
-	background-color: #d0d0d0 !important;
-}
-#chatbot-header{
-	background-color: #a39c9c !important;
-	font-size: 16px;
-	position: fixed;
-	z-index: 8000;
-	width:100% ;
-}
-#chatbot-header span{
-	padding : 13px;
-}
-#chatbot-footer{
-	position: fixed;
-	right: 25px;
-	bottom: 35px;
-	background-color: #a39c9c;
-
-}
-#chatbot-footer span{
-
-	padding : 10px;
-
-}
-.chatbot-text-area{
-	position: fixed;
-	bottom:  20px;
-	width: 264px;
-}
-.user{
-
-	margin-top: 2px;
-	margin-bottom: 6px;
-	padding: 2px;
-	background-color: red;
-	color: white;
-	width: 85%;
-	border-radius: 8px;
-	font-family: monospace;
-	font-size: 12px;
-
-
-}
-.bot{
-	position: relative;
-	left: 15%;
-	color: white;
-	margin-top: 2px;
-	margin-bottom: 6px;
-	padding: 2px;
-	background-color: green;
-	border-radius: 8px;
-	width : 85%;
-	font-family: monospace;
-	font-size: 12px;
-
+.blur {
+	-webkit-filter : blur(7px);
+	-moz-filter : blur(7px);
+	-ms-filter : blur(7px);
+	-o-filter: blur(7px);
+	filter: blur(7px);
+	/*opacity: 0.2;*/
 
 }
 
-#j-fullscreen{
-
-}
-
-#j-remove{
-
-}
-
+ 
 
 .chat-area{
 	height: auto;
-}
+}*/
+
+/*new chat bot css starts here*/
 
 </style>
 </head>
 
-<body>
-<!--chatbot html here-->
-<!-- <p class = "chats"></p> -->
-<div id = "j-show-chatbot" style = "color:white" role = "button" >
-<div id = "show-chatbot">
-	<div class = "row">
-		
-		<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
- <span class="glyphicon glyphicon-user"></span> &nbsp;&nbsp;Click toggle chatbot
-		</div>
-	</div>
-</div>
-</div>
-<div id = "chat-interface" style = "background-color: transparent">
+<body style = "overflow-x: hidden">
 
-	<div id = "chatbot-header">
-		<span class="glyphicon glyphicon-fullscreen" id = "j-fullscreen" role = "button"></span>
-	 	<span style = "position: relative; left : 200px" class="glyphicon glyphicon-remove-sign" id = "j-remove" role  = "button"></span>
+
+
+
+
+
+
+<!-- botton for chatbot -->
+<button id = "toggle-bot" style = "background-color: #72beda; border: none;border-radius: 4px;color: white;"><i id = "j-icon" style = "font-size: 55px;padding:5px"class="fab fa-reddit-alien"></i>&nbsp;</button>
+<style>#j-icon:hover{font-size: 53px !important;}#toggle-bot:hover{background-color: #0c779e; !important;}#toggle-bot{position: fixed;bottom: 6px;right:6px;z-index: 800;}.dd{position: fixed;bottom: 36px;right:6px;z-index: 8000;}</style>
+<!-- chatbot new starts here -->
+<br>
+<div class = "dd">
+<div style = "width : 320px; background-color: #72beda;border-radius: 4px;" id = "bot -header">
+<i style = "text-shadow: 2px 2px 4px #000000;color: #f4f4f4; font-size: 24px; margin-left: 20px;" class="fab fa-android"></i>
+<i role = "button" style = "text-shadow: 2px 2px 4px #000000;color: #f4f4f4;font-size: 20px;position: relative; left : 220px;padding: 15px" class="fas fa-times bot-remove"  id = "bot-remove"></i>
+<!-- <span  class="glyphicon glyphicon-remove-sign bot-remove" id = "bot-remove" role  = "button"></span> -->
+</div>
+<style>
+#bot-remove:hover{
+font-size: 15px !important;
+	}</style>
+<div id = "bot-interface" style = "background-color: #f4f4f4; height: 380px;width : 320px;border: 8px solid #04455d;border-bottom: 0px solid transparent; border-radius: 8px;overflow-y: auto">
+<center><div style = "width : 90%; background-color: #a39c9c;margin-top: px;border-radius:3px;font-family: monospace"><b style = "font-family: montserrat"><i style = "font-size: 17px"class="fab fa-reddit-alien"></i>&nbsp;JOBOT</b><br>
+Hello I'm Jobot. Type 'help' to see what i can do.
+</div></center><br>
+<div id = "your-msg" style = "padding-bottom: 170px"></div>
+
+</div>
+ <div class="form-group" style = "width : 320px;position: relative;top: -5px;">
+ 	<div class = "col-sm-10 col-xs-10 col-lg-10 col-md-10" style = "background-color: #04455d;padding-top: 4px; padding-bottom: 9px">
+    <input type="text" class="form-control " id="bot-chat-area">
 	</div>
-	<br><br>
-	<div id = "chat-area" class = 'chat-area'>
-	<div style = "font-size: 10px; font-family: monospace;background-color: grey;color: white">
-		<br>
-		<p><b>Jobot</b><br>Hi, I'm Jobot</p>
-	</div>
-	<div class = "chats"></div>
-	<div id = "bott"></div>
-	</div>
-	<div id = "chatbot-text-area" class = "chatbot-text-area">
-			<div class="form-group">
   
-  <input type="text" class="form-control" id="message">
-				</div>
-	<div id = "chatbot-footer" >
-	
-		<span class="glyphicon glyphicon-send"   id = "j-send" role = "button"></span>
-	</div>
-		</div>
-
-	
-
-
+ <div class = "col-sm-2 col-xs-2 col-lg-2 col-md-2" style = "background-color: #04455d;padding-top: 4px; padding-bottom: 9px">
+  <button id = "send-msg" type="submit" style = "margin-left: -20px;" class="btn btn-default "><span class="glyphicon glyphicon-send"   id = "send-msg" role = "button"></span></button>
 </div>
-<!-- chatbot 
-html 
-ends here -->
+</div>
+</div>
+
+
+
+
+
+<script>
+//hides the chat bot
+$('.dd').hide();
+ // $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+// $("#bot-interface").animate({ scrollTop: $('#bot-interface').prop("scrollHeight")}, 1000);
+
+var countt = 0;
+
+
+$('#send-msg').click(function(){
+var gone = 8;
+var dt = new Date();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+var d = dt.getDate();
+var m =dt.getMonth();
+m += 1;
+var y = dt.getFullYear();
+var theDate = d + "-"+ m + "-" + y;
+var version = "1.0.0";
+var message = $('#bot-chat-area').val();
+
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[0]; // Note: some voices don't support altering params
+msg.voiceURI = 'native';
+msg.volume = 1; // 0 to 1
+msg.rate = 1; // 0.1 to 10
+msg.pitch = 2; //0 to 2
+
+
+//edit the message
+message = message.toLowerCase();
+// message = message.replace(/'?'/g, ' ');
+message = message.replace(/\?/gi,"");
+message = message.replace(/\./gi,"");
+message = message.replace(/\'/gi,"");
+message = message.replace(/\"/gi,"");
+
+message = message.replace(/\\/gi,"");
+
+
+
+message = message.replace(/\s+/gi,' ').trim();
+
+
+//if it doesn't have a value
+if (message.length == 0){
+
+}
+else{
+	//your message
+var strr = '<div style = "background-color: #ade4f8;width:70%;border-radius:4px"><font style = "font-size: 10px; font-family: monospace"><b><i style = "font-size: 17px" class="fas fa-user"></i>&nbsp;You</b><br>' +message + "</font></div><br>";
+$('#your-msg').append(strr);
+}
+
+
+
+
+//time
+if (message.search("time") != -1){
+
+reply = "The time is "+time;
+
+msg.text = reply;
+msg.lang = 'en-US';
+speechSynthesis.speak(msg);
+
+reply = '<div id = "bb'+countt+'" style = "background-color: #72beda;width:70%;position: relative;left: 30%;border-radius:4px "><b><i style = "font-size: 17px"class="fab fa-reddit-alien"></i>&nbsp;Bot</b><br><font class = "" style = "font-size: 10px; font-family: monospace">'+ reply + "</font></div><br>";
+        $("#your-msg").append(reply);  
+        gone = 0;
+}
+
+
+
+
+
+
+//date
+if (message.search("date") != -1){
+
+reply = "The date is "+ theDate;
+msg.text = reply;
+msg.lang = 'en-US';
+speechSynthesis.speak(msg);
+reply = '<div id = "bb'+countt+'" style = "background-color: #72beda;width:70%;position: relative;left: 30%;border-radius:4px "><b><i style = "font-size: 17px"class="fab fa-reddit-alien"></i>&nbsp;Bot</b><br><font class = "" style = "font-size: 10px; font-family: monospace">'+ reply + "</font></div><br>";
+        $("#your-msg").append(reply);
+        gone = 0;
+
+}
+
+
+
+
+//about bot
+if (message.search("aboutbot") != -1){
+reply = "I am dobot "+ version;
+msg.text = reply;
+msg.lang = 'en-US';
+speechSynthesis.speak(msg);
+reply = '<div id = "bb'+countt+'" style = "background-color: #72beda;width:70%;position: relative;left: 30%;border-radius:4px "><b><i style = "font-size: 17px"class="fab fa-reddit-alien"></i>&nbsp;Bot</b><br><font class = "" style = "font-size: 10px; font-family: monospace">'+ reply + "</font></div><br>";
+        $("#your-msg").append(reply);   
+        gone = 0;
+}
+
+
+
+
+
+
+
+//if it contains train and : hashtag and hashtag ...trim and take it to php
+if (message.search("train") != -1){
+	if (message.search(":") != -1){
+		if (message.search("#") != -1){
+			//do php thing
+			var train = 1;
+			$.post('j-replies8.php',{trainValidity : train,chatMessage : message},function(data){
+				reply = data;
+				msg.text = reply;
+				msg.lang = 'en-US';
+				speechSynthesis.speak(msg);
+				reply = '<div id = "bb'+countt+'" style = "background-color: #72beda;width:70%;position: relative;left: 30%;border-radius:4px "><b><i style = "font-size: 17px"class="fab fa-reddit-alien"></i>&nbsp;Bot</b><br><font class = "" style = "font-size: 10px; font-family: monospace">'+ reply + "</font></div><br>";
+				        $("#your-msg").append(reply); 
+			});
+		}
+			else{
+		reply =  "format wrong";
+		msg.text = reply;
+		msg.lang = 'en-US';
+		speechSynthesis.speak(msg);
+			}
+	}
+	else{
+		reply =  "format wrong";
+		msg.text = reply;
+		msg.lang = 'en-US';
+		speechSynthesis.speak(msg);
+	}
+	gone = 0;
+}
+//end of train javascript
+
+
+
+//what it does with other commands
+else if (gone == 8){
+	$.post('j-replies8.php',{chatMessage : message}, function(data)
+{
+
+	reply = data;
+	msg.text = reply;
+	msg.lang = 'en-US';
+	speechSynthesis.speak(msg);
+	reply = '<div id = "bb'+countt+'" style = "background-color: #72beda;width:70%;position: relative;left: 30%;border-radius:4px "><b><i style = "font-size: 17px"class="fab fa-reddit-alien"></i>&nbsp;Bot</b><br><font class = "" style = "font-size: 10px; font-family: monospace">'+ reply + "</font></div><br>";
+	        $("#your-msg").append(reply); 
+
+});
+
+}
+
+
+
+
+
+
+
+
+//end effects
+ var scrollString = "#bb"+countt;
+  // $('#bot-interface').animate({scrollTop : $(scrollString).offset().top},1000);
+countt++;
+$('#bot-chat-area').val('');
+$('#bot-interface').scrollTop($('#bot-interface')[0].scrollHeight);//works for me..just recall you did a lil padding-bottom thing
+
+
+});//end of what happens after you submit
+
+
+
+
+
+//for the enter key
+
+$('#bot-chat-area').keyup(function (e){
+			if (e.which == 13)
+		{
+			$('#send-msg').click();
+			return false;
+		}
+	});
+	
+
+
+
+
+//some css effects
+$('#toggle-bot').click(function(){
+
+	$('.dd').show('slow');
+	$('#j-background').addClass("blur");
+	$('#toggle-bot').hide();
+});
+
+$('.bot-remove').click(
+function(){
+
+$('.dd').fadeOut('slow');
+$('#j-background').removeClass("blur");
+$('#toggle-bot').show();
+
+}
+	);
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div id = "j-background">
 	<div class = "row">
 		<div id = "j-firstdiv" class = "col-lg-5 col-md-5 col-sm-12 col-xs-12">
 			<br><br>
-			<center><img  src = "http://res.cloudinary.com/jurshsmith/image/upload/v1524109896/josh.png" width = "350px" id = "j-image"></center><br><br>
+			<center><img  src = "there/images/jj.png" width = "350px" id = "j-image"></center><br><br>
 			<font class = "j-profile">  <span class="glyphicon glyphicon-user"></span>  &nbsp;</font>
 			<font class = "j-profile" style = "font-weight: 600;">OLADELE JOSHUA</font><br>
 			 <font class = "j-header" style = "font-weight: 250;">
@@ -317,6 +531,7 @@ ends here -->
 </div>
 
 
+
 </body>
 </html>
 <!--css js-->
@@ -354,6 +569,8 @@ else{
 
 
 
+<<<<<<< HEAD
+=======
 <!-- chatbot js -->
 <script type="text/javascript">
 $('#chat-interface').hide();
@@ -494,3 +711,4 @@ var bottString = String('#bott'+a);
 =======
 </script>
 >>>>>>> e19e8621d6637cfb7bcf6fe86ffc52d5536583cb
+>>>>>>> a6663dc26928a9182067c9107843c4bba760a89d
