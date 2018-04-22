6c3c858aa151f3a9f1a9a29939bdc13a903037c7
Travis CI <travis@travis-ci.org>
2018-04-22 04:38:16 +0000

    committed at fe375d2 [ci skip]

--- profiles/ionware.php
+++ profiles/ionware.php
@@ -2,10 +2,6 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 'on');
 
-
-if (!defined(DB_USER))
-    require_once __DIR__."/../config.php";
-
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 } catch (PDOException $pe) {
