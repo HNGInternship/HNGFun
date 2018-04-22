61bc1a24f4b72e4150f9f82a3d2e8f70d78ad081
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-22 00:29:16 -0700

    my bot

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -20,7 +20,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 	    if(!isset($_POST['question'])){
 	      echo json_encode([
 	        'status' => 1,
-	        'answer' => "Please provide a question"
+	        'result' => "Please provide a question"
 	      ]);
 	      return;
 	    }
