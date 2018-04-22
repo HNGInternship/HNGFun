ad7914d1cae9126ea36aaceab15f1b0907047be5
Travis CI <travis@travis-ci.org>
2018-04-22 19:09:17 +0000

    committed at d1d17fb [ci skip]

--- profiles/jurshsmith.php
+++ profiles/jurshsmith.php
@@ -15,7 +15,8 @@ HNG 4.0 | Jurshsmith
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	
+<link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=montserrat">	
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
