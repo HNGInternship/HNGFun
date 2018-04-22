9b852a669af1a719f42f85df67b9b968727d3497
Travis CI <travis@travis-ci.org>
2018-04-22 09:25:01 +0000

    committed at aea6deb [ci skip]

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -39,7 +39,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 	    }
 
 	    //if(!isset($_POST['question'])){
-	    /*$mem = isset($_POST['question']);
+	    $mem = isset($_POST['question']);
 	    $mem = preg_replace('([\s]+)', ' ', trim($mem));
 	    $mem = preg_replace("([?.])", "", $mem);
 		$arr = explode(" ", $mem);
@@ -118,7 +118,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 		    	]);
 		    	return;
 		    }
-	    }*/
+	    }
 	}catch (Exception $e){
 		return $e->message ;
 	}
