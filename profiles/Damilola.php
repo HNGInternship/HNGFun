6f1f92b4f313556a1c6de159f66a843f9d5e520e
Travis CI <travis@travis-ci.org>
2018-04-22 01:38:23 +0000

    committed at 19af7a46 [ci skip]

--- profiles/Damilola.php
+++ profiles/Damilola.php
@@ -41,7 +41,7 @@
 
   #cover{
     width: 100%;
-    background: red;
+    background: rgba(0,0,0,0);
     height: 100vh;
     text-align: center;
   }
@@ -56,6 +56,7 @@
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+    background: rgba(0,0,0,0);
     
   }
 
