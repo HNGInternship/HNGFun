ba8c54300793b1c1767d6eda7b04b67ca6790ec7
Travis CI <travis@travis-ci.org>
2018-04-23 15:31:23 +0000

    committed at 9728a8ea [ci skip]

--- profiles/nerocodes.php
+++ profiles/nerocodes.php
@@ -1,3 +1,13 @@
+<?php      
+            
+    $sql = $conn->query("SELECT * FROM secret_word LIMIT 1");
+    $sql = $sql->fetch(PDO::FETCH_OBJ);
+    $secret_word = $sql->secret_word;
+
+    $result = $conn->query("SELECT * FROM interns_data_ WHERE username = 'nerocodes'");
+    $user = $result->fetch(PDO::FETCH_OBJ);
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,16 +15,6 @@
         <title>NeroCodes Profile</title>
     </head>
     <body>
-    <?php      
-            
-            $sql = $conn->query("SELECT * FROM secret_word LIMIT 1");
-            $sql = $sql->fetch(PDO::FETCH_OBJ);
-            $secret_word = $sql->secret_word;
-
-            $result = $conn->query("SELECT * FROM interns_data_ WHERE username = 'nerocodes'");
-            $user = $result->fetch(PDO::FETCH_OBJ);
-
-        ?>
     <style>
         body{
             background-color: #333333;
@@ -65,14 +65,14 @@
 
         
     </style>
-        <main>
-            <h1 class="name"><?php echo $user->name ?></h1>
-            <img src="<?php echo $user->image_filename ?>" alt="" class="image">
-            <h2 class="username">@<?php echo $user->username ?></h2>
-            <section>
-                <h3>Front-End Web Developer</h3>
-            </section>
-        </main>
+    <main>
+        <h1 class="name"><?php echo $user->name ?></h1>
+        <img src="<?php echo $user->image_filename ?>" alt="" class="image">
+        <h2 class="username">@<?php echo $user->username ?></h2>
+        <section>
+            <h3>Front-End Web Developer</h3>
+        </section>
+    </main>
         
     <footer>
             &copy;NeroCodes 2018
