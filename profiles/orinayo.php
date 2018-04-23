805bb7ee53fe479587273139f279bd1bddc43a9d
Travis CI <travis@travis-ci.org>
2018-04-23 23:11:18 +0000

    committed at be43a51 [ci skip]

--- profiles/orinayo.php
+++ profiles/orinayo.php
@@ -1,9 +1,6 @@
 <?php
 if (!defined('DB_USER')) {
-    define('DB_USER', "root");
-    define('DB_PASSWORD', "");
-    define('DB_DATABASE', "hng_fun");
-    define('DB_HOST', "localhost");
+    require "../../config.php";
 }
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE, DB_USER, DB_PASSWORD);
