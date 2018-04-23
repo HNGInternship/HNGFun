bb49bc78ba9cb7e4cd84edf72514d532cd3abe05
dbeloveth <dbeloveth@gmail.com>
2018-04-23 16:54:34 +0100

    ilesanmi added file

--- hng_fun.sql
+++ hng_fun.sql
@@ -1,3 +1,11 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 23, 2018 at 04:29 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -8,37 +16,31 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-
-
-
 /*!40101 SET NAMES utf8mb4 */;
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET NAMES utf8mb4 */;
-
 
 --
 -- Database: `hng_fun`
 --
 
--- -----------------------------------------------------
+-- --------------------------------------------------------
 
--- Table structure for table `interns_data_`
+--
+-- Table structure for table `interns_data`
 --
 
-CREATE TABLE IF NOT EXISTS `interns_data` (
-  `intern_id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `interns_data` (
+  `intern_id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `image_filename` varchar(255) NOT NULL,
-  PRIMARY KEY (`intern_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+  `image_filename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `interns_data_`
+-- Dumping data for table `interns_data`
 --
 
+INSERT INTO `interns_data` (`intern_id`, `name`, `username`, `image_filename`) VALUES
+(1, 'Ilesanmi Samuel Ayanfejesu', 'dbeloveth', 'https://res.cloudinary.com/dbeloveth/image/upload/v1523700237/pic2.jpg'),
 
 -- --------------------------------------------------------
 
@@ -46,48 +48,32 @@ CREATE TABLE IF NOT EXISTS `interns_data` (
 -- Table structure for table `secret_word`
 --
 
-CREATE TABLE IF NOT EXISTS `secret_word` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `secret_word` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
-
+CREATE TABLE `secret_word` (
+  `id` int(11) NOT NULL,
+  `secret_word` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `secret_word`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `secret_word` (`id`, `secret_word`) VALUES
-(1, 'sample_secret_word');
+--
+-- Indexes for table `interns_data`
+--
+ALTER TABLE `interns_data`
+  ADD PRIMARY KEY (`intern_id`);
 
-
-
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `interns_data`
 --
- 
 ALTER TABLE `interns_data`
-MODIFY `intern_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
-
-
---
--- AUTO_INCREMENT for table `secret_word`
---
-ALTER TABLE `secret_word`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `intern_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
--- phpMyAdmin SQL Dump
--- version 4.7.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Apr 14, 2018 at 08:00 AM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.2.4-1+ubuntu16.04.1+deb.sury.org+1
-
