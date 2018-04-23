eba077680f2edee56bb3260a539b1fa31f2607ce
Travis CI <travis@travis-ci.org>
2018-04-23 11:36:09 +0000

    committed at c70adbf9 [ci skip]

--- profiles/orinayo.php
+++ profiles/orinayo.php
@@ -1,15 +1,6 @@
 <?php
 if (!defined('DB_USER')) {
-    define('DB_USER', "root");
-    define('DB_PASSWORD', "");
-    define('DB_DATABASE', "hng_fun");
-    define('DB_HOST', "localhost");
-}
-try {
-    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE, DB_USER, DB_PASSWORD);
-} catch (PDOException $pe) {
-    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-}
+    require 'db.php';
 
 try {
     $sql = "SELECT * FROM secret_word";
