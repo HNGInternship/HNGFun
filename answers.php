d657316e037aba80708662b3e1fe299aefcd2428
Orinayo <orinayooyelade@gmail.com>
2018-04-24 00:53:27 +0100

    bot

--- answers.php
+++ answers.php
@@ -1350,19 +1350,6 @@ function getCryptoPrice($ticker){
     return $result;
 }
 ## END'S HERE##
-
-///
-function Get_Hotelsng_wikipage()
-{
-    $api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="."hotels.ng"."&format=json&callback=?";
-    $result = file_get_contents($api);
-    $result = substr_replace($result, "", 0, 5);
-    $result = substr_replace($result, "", -1);
-    $result = json_decode($result, true);
-    $result = array("answer"=>"<a href=".$result[3][0].">".$result[1][0]."</a><p>".$result[2][0]."</p>");
-    return $result;
-}
-///
 ?>
 
 
