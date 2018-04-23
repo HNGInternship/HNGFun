97dbfd8f9e6229765606c7f280436c53cf2e4aa4
Travis CI <travis@travis-ci.org>
2018-04-23 02:12:37 +0000

    committed at f8c99be [ci skip]

--- profiles/agatevureglory.php
+++ profiles/agatevureglory.php
@@ -582,8 +582,7 @@ class DBHelper{
          	</div>
      	</div>
     </div>
-</body>
-</html> 
+ 
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -726,3 +725,5 @@ class DBHelper{
     });
 </script>
 
+</body>
+</html>
