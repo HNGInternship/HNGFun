52b7ce0a0e39c09921430dc3460158ce48a1c93a
charlyge <charlyge2@gmail.com>
2018-04-23 01:14:01 +0100

    added my profile back

---
+++ profiles/charlyge.php
@@ -0,0 +1,126 @@
+<Html>
+<title>
+HNG
+</title>
+<head>
+ <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One|Ubuntu" rel="stylesheet">
+</head>
+<style>
+
+body{
+background-color: #ADD8E6;
+	
+}
+#firstH{
+	margin-top:100px;
+	color: #1B5E20;
+	margin-left:100px;
+	clear:both;
+	
+	
+}
+#secondH{
+	color: white;
+	
+}
+#thirdH{
+	color: orange;
+	
+}
+
+
+#img{
+	margin-top:30px;
+	float:left;
+}
+h4{
+	float:right;
+	
+	
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color: #111;
+}
+</style>
+
+<body>
+<nav>
+    <div id="social">
+    
+       
+         <ul>
+            <li>
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li>
+              <a class="nav-link" href="learn.php">Learn</a>
+            </li>
+   
+            <li class="nav-item">
+              <a class="nav-link" href="listing.php">Interns</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php">Register</a>
+            </li>
+            <li>
+              <a href="testimonies.php">Testimonies</a>
+            </li>
+			<li>
+              <a href="https://github.com/charlyge">Github</a>
+            </li>
+			<li>
+				<a href="https://twitter.com/charlyge2">Twitter</a>
+				</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+<header>
+	
+				
+			
+				
+		
+		
+	</header>
+	
+<img id="img" height="280px" src="http://res.cloudinary.com/charlyge/image/upload/v1523623038/HNG/IMG_20150910_093320.jpg" alt="charlyge">
+
+
+	
+ <h1 id="firstH">I'm Uhiara Charles</h1>
+ <h2 id="thirdH">An Android App Developer</h2>
+  <h3 id="secondH">Intern at Hotels.ng</h3>
+   
+    <h4><p id="clock">
+		<?php
+			echo "Current time is </br>" . date("h:i:sa");
+		?> 
+	</p></h4>
+   
+
+</body>
+
+</Html>
