cc0bf4d303ad8d51f282815a826755cd888a4681
Travis CI <travis@travis-ci.org>
2018-04-23 10:04:23 +0000

    committed at 1cef1cc [ci skip]

--- profiles/larry.php
+++ profiles/larry.php
@@ -1,6 +1,5 @@
 <<<<<<< HEAD
 <?php 
- 	require '../db.php';
 	$result = $conn->query("Select * from secret_word LIMIT 1");
     $result = $result->fetch(PDO::FETCH_OBJ);
     $secret_word = $result->secret_word;
