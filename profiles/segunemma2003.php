94da041e088affb6eab2a3474c379318e156da30
Travis CI <travis@travis-ci.org>
2018-04-24 19:05:21 +0000

    committed at d1d1731 [ci skip]

--- profiles/segunemma2003.php
+++ profiles/segunemma2003.php
@@ -352,7 +352,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
 					// alert(responseMessage('I am a little bot'));
 				}
                  $.ajax({
-                     url:"/profiles/oriechinedu.php",
+                     url:"/profiles/segunemma2003.php",
                      type: "POST",
                      dataType: "json",
 		     
