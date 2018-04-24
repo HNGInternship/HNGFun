dc5a00747c79f4b7e253408ec8cec506d563bf96
Umar Mash <mafulun4real@gmail.com>
2018-04-24 02:37:52 +0100

    Fixing DB connection issues

--- profiles/umar.php
+++ profiles/umar.php
@@ -2,7 +2,7 @@
 	include('../answers.php');
 
 	if(!defined('DB_USER')){
-		require "../config.php";     
+		require_once __DIR__."/../../config.php";     
 		try {
 			$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 		} catch (PDOException $pe) {
