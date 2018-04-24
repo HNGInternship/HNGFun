7c8cb22e33583cd13ef27894073da6bcf35b748c
Dennis W. Otugo <otugodennis@gmail.com>
2018-04-24 06:03:29 +0100

    Update dennisotugo.php

--- profiles/dennisotugo.php
+++ profiles/dennisotugo.php
@@ -96,67 +96,331 @@ try {
    }
    else{
 ?>
-
 <!DOCTYPE html>
-<html lang="en-us" xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title>Oracle JET Starter Template - Web Blank</title>
-    <meta http-equiv="x-ua-compatible" content="IE=edge"/>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="viewport-fit=cover, initial-scale=1.0"/>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
 
-<style>
-.profile {height: 100%;text-align: center;position: fixed;position: fixed;position: fixed;width: 50%;right: 0;background-color: #007bff}footer {display: none;padding: 0px !important}h1, h2, h3, h4, h5, h6 {color: white;text-align: center;bottom: 50%;left: 65%;position: fixed;font-family: Lato,'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight: 700}p {position: fixed;bottom: 40%;left: 58%;line-height: 1.5;margin: 30px 0}.bot-body {max-width: 100% !important;position: fixed;margin: 32px auto;position: fixed;width: 100%;left: 0;bottom: 0px;height: 80%}.messages-body {overflow-y: scroll;height: 100%;background-color: #FFFFFF;color: #3A3A5E;padding: 10px;overflow: auto;width: 50%;padding-bottom: 50px;border-top-left-radius: 5px;border-top-right-radius: 5px}.messages-body > div {background-color: #FFFFFF;color: #3A3A5E;padding: 10px;overflow: auto;width: 100%;padding-bottom: 50px}.message {float: left;font-size: 16px;background-color: #007bff63;padding: 10px;display: inline-block;border-radius: 3px;position: relative;margin: 5px}.message: before {position: absolute;top: 0;content: '';width: 0;height: 0;border-style: solid}.message.bot: before {border-color: transparent #9cccff transparent transparent;border-width: 0 10px 10px 0;left: -9px}.color-change {border-radius: 5px;font-size: 20px;padding: 14px 80px;cursor: pointer;color: #fff;background-color: #00A6FF;font-size: 1.5rem;font-family: 'Roboto';font-weight: 100;border: 1px solid #fff;box-shadow: 2px 2px 5px #AFE9FF;transition-duration: 0.5s;-webkit-transition-duration: 0.5s;-moz-transition-duration: 0.5s}.color-change: hover {color: #006398;border: 1px solid #006398;box-shadow: 2px 2px 20px #AFE9FF}.message.you: before {border-width: 10px 10px 0 0;right: -9px;border-color: #edf3fd transparent transparent transparent}.message.you {float: right}.content {display: block;color: #000000}.send-message-body {border-right: solid black 3px;position: fixed;width: 50%;left: 0;bottom: 0px;box-sizing: border-box;box-shadow: 1px 1px 9px 0px rgba(1, 1, 1, 1)}.message-box {width: -webkit-fill-available;border: none;padding: 2px 4px;font-size: 18px}body {overflow: hidden;height: 100%;background: #FFFFFF !important}.container {max-width: 100% !important}.fixed-top {position: fixed !important;}
-</style>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        
+        html{
+            font-family: sans-serif;
+            line-height: 2;
+        }
+        
+        a{
+            color: #fff;
+            text-decoration: none;
+            opacity: .8;
+        }
+        a:hover{
+            opacity: 1;
+        }
+        a.btn{
+            border-radius: 4px;
+            text-transform: uppercase;
+            font-weight: bold;
+            text-align: center;
+            background-color: #3f51b5;
+            opacity: 1;
+        }
+        
+        h3{
+            font-size: 20px;
+            font-family: sans-serif;
+            color: #cccccc;
+        }
+        #connect{
+            display: flex;
+            flex-wrap: wrap;
+            margin-right: -15px;
+            list-style: none;
+            justify-content: center;
+        }
 
-  </head>
-  <body>
-<div class="profile">
-						<h1>Dennis Otugo</h1>
-						<p>Human Being &nbsp;&bull;&nbsp; Cyborg &nbsp;&bull;&nbsp; Never asked for this</p>
+        a{
+            margin: 0 15px;
+        }
+        
+        .landing{
+            position: relative;
+            justify-content: center;
+            text-align: center;
+            min-height: 100%;
+        }
+        
+        body{
+            background-color: #333333;
+            /*background-image: url(http://res.cloudinary.com/ddjpblpib/image/upload/v1523629224/1.jpg);*/
+            /*background-size: cover;*/
+        }
 
-					</div>
-  <div class="bot-body">
-    <div class="messages-body">
-      <div>
-        <div class="message bot">
-          <span class="content">Look alive</span>
-        </div>
-      </div>
-	<div>
-        <div class="message bot">
-          <span class="content">What do you have in mind, Let's talk :) </span>
-        </div>
-      </div>
-    </div>
-    <div class="send-message-body">
-      <input class="message-box" placeholder="Enter your words here..."/>
-    </div>
-  </div>
+        .landing h1{
+            font: bold 30px "Open Sans";
+            text-transform: uppercase;
+            margin-bottom: 20px;
+            color: #cccccc;
+        }
+        .landing h1 span{
+            font-size: 23px;
+            color: #ffffff;
+        }
+        
+        .landing p{
+            color: white;
+            text-align: center;
+            text-shadow: 2px 2px 4px #000000;
+        }
+        
+        .landing_content_area{
+            opacity: 0;
+            margin-top: 108px;
+            animation: 1s slidefade 1s forwards;
+        }
+        
+        @keyframes slidefade{
+            100%{
+                opacity: 1;
+                margin: 0;
+            }
+        }
 
+        .card{
+            max-height: 500px;
+        }
+
+        .panel-heading{
+            background-color: #cccccc;
+            color: #333333;
+            text-align: center;
+        }
+        .frame{
+            background:#ffffff;
+            background-size: contain;
+            height:350px;
+            overflow: hidden;
+            padding:0;
+            color: #333;
+        }
+        .frame > div:last-of-type{
+            position:absolute;
+            bottom:0;
+            width:100%;
+            display:flex;
+        }
+       
   
-  
-  <script>
+        input:focus{
+            outline: none;
+        }        
+        ::-webkit-input-placeholder { 
+            color: #d4d4d4;
+        }
+        ::-moz-placeholder { 
+            color: #d4d4d4;
+        }
+        ::-ms-input-placeholder {
+            color: #d4d4d4;
+        }
+        ::-moz-placeholder {
+            color: #d4d4d5;
+        }
+        .message{
+            position:relative;
+            -webkit-box-shadow:0 -2px 1px rgba(0,0,0,.03);
+            box-shadow:0 -2px 1px rgba(0,0,0,.03)
+            }
+        .message input{
+            font:16px Noto Sans,sans-serif;
+            background-color: #cccccc;
+            border:none;
+            padding:20px 30px;
+            width:100%;
+            color: black;
+            }
+        .message input:focus{outline:0}
+        .message button.msg{
+            background: #cccccc;
+            color:#333333;
+            border: none;
+            /*border-radius:70%;*/
+            width: 50px;
+            height:50px;
+            /*padding:13px 10px 20px;*/
+            position:absolute;
+            font-size:2.2em;
+            text-align:center;
+            right:8px;
+            bottom:6px
+        }
+       
+        /* CSS talk bubble */
+        .talk-bubble {
+            margin: 10px;
+            display: inline-block;
+            position: relative;
+            width: 70%;
+            height: auto;
+            background-color: #333333;
+            color: #ffffff;
+        }
+        .talk-bubble-2 {
+            margin: 10px;
+            display: inline-block;
+            position: relative;
+            width: 80%;
+            height: auto;
+            background-color:#333333;
+            color: #ffffff;
+        }
+
+
+        /* Right triangle placed top left flush. */
+        .tri-right.border.left-top:before {
+            content: ' ';
+            position: absolute;
+            width: 0;
+            height: 0;
+            left: -40px;
+            right: auto;
+            top: -8px;
+            bottom: auto;
+            border-color: #666 transparent transparent transparent;
+        }
+        .tri-right.left-top:after{
+            content: ' ';
+            position: absolute;
+            width: 0;
+            height: 0;
+            left: -20px;
+            right: auto;
+            top: 0px;
+            bottom: auto;
+            border: 22px solid;
+            border-color:#333333 transparent transparent transparent;
+        }
+
+
+
+        /* Right triangle placed top right flush. */
+        .tri-right.border.right-top:before {
+            content: ' ';
+            position: absolute;
+            width: 0;
+            height: 0;
+          left: auto;
+            right: -40px;
+          top: -2px;
+            bottom: auto;
+            /* border: 32px solid; */
+            border-color: #666 transparent transparent transparent;
+        }
+        .tri-right.right-top:after{
+            content: ' ';
+            position: absolute;
+            width: 0;
+            height: 0;
+          left: auto;
+            right: -20px;
+          top: 0px;
+            bottom: auto;
+            border: 20px solid;
+            border-color: #333333 transparent transparent transparent;
+        }
+
+        /* talk bubble contents */
+        .talktext{
+          padding: 1em;
+            text-align: left;
+          line-height: 1.5em;
+        }
+        .talktext p{
+          /* remove webkit p margins */
+          -webkit-margin-before: 0em;
+          -webkit-margin-after: 0em;
+        }
+
+        .card-body{
+            overflow-y: scroll ;
+        }
+        .bot-foot{
+            padding: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-10 col-md-5">
+                <div class="cardd bg-success text-black" style="background-color: #333333 !important">
+                    <!-- <div class="panel-heading">
+                        <h1><span class="fa fa-comments"></span>Chat Bot</h1>
+                    </div>
+                        <div class="col-sm-12 col-md-12 frame"> -->
+                        
+                        <div class="card">
+                            <div class="card-header">
+                            <h1><span class="fa fa-comments"></span>softBot</h1>
+                            </div>
+                            <div class="card-body">
+                                <div class="message-container">
+                                   
+                                    <!-- <div class="sent-message talk-bubble tri-right round border right-top">
+                                        <div class="talktext">
+                                            <p>And finally on the right flush at the top. Uses .round .border and .right-top</p>
+                                        </div>
+                                    </div> -->
+                                     <div class="response-message talk-bubble-2 tri-right left-top">
+                                        <div class="talktext">
+                                            <p>Hey there, to know about me send <code>#aboutbot</code> and I'll tell you who I am</p>
+                                            <p>To train me send your lesson in the format <code>train:question#answer#password</code></p>
+                                            <p>Enjoy your time with me</p>
+                                        </div>
+                                    </div> 
+                                </div>
+                            </div>
+                            <footer class="bot-foot">
+                                    <div class="message">
+                                        <form id="chat-form">
+                                            <input class="form-control chat" name="message" aria-label="With textarea" placeholder="Enter your question here..." name="qstn">
+                                            <div class="input-group-append">
+                                                <button type="submit" class="btn btn-primary btn-sm msg"><span class="fa fa-send-o"></span></button>
+                                            </div>
+                                        </form>
+                                    </div>
+                            </footer>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <script>
         $('documnet').ready(function(){
-            $('#send-message').submit(function(e){
+            $('#chat-form').submit(function(e){
                 e.preventDefault();
 
                
                 var chat = $('.chat');
                 var message = chat.val().trim();
-                 var message_div = $('.messages-container');
+                // alert(message);
+                 var message_div = $('.message-container');
                 if (message != ''){
 
                     if (message.split(':')[0] !='train' && message != "aboutbot"){
                         message_div.append(sentMessage(message));
                         chat.val('');
-                        $('.bot-container').scrollTop($('.bot-container')[0].scrollHeight);
+                        $('.card-body').scrollTop($('.card-body')[0].scrollHeight);
                     }
                 }
                 if (message =="#aboutbot"){
                     chat.val('');
-                    message_div.append(responseMessage('v1.0'));
-                    $('.bot-container').scrollTop($('.bot-container')[0].scrollHeight);
+                    message_div.append(responseMessage('I am a softbot, my verion is 1.0, I live on the internet and I am a friend to all those that have common sense'));
+                    $('.card-body').scrollTop($('.card-body')[0].scrollHeight);
                     return;
                 }
     
@@ -174,12 +438,12 @@ try {
                              if (res.status ===0){
                                 chat.val('');
                                 message_div.append(responseMessage(res.data));
-                                $('.bot-container').scrollTop($('.bot-container')[0].scrollHeight);
+                                $('.card-body').scrollTop($('.card-body')[0].scrollHeight);
                              }
                             if (res.status ===1){
                                 chat.val('');
                                 message_div.append(responseMessage(res.data));
-                                $('.bot-container').scrollTop($('.bot-container')[0].scrollHeight);
+                                $('.card-body').scrollTop($('.card-body')[0].scrollHeight);
                             }
 
                          }
@@ -190,16 +454,16 @@ try {
                  });
                 function responseMessage(query){
 
-                     return   '<div class="message bot">'+
-                                '<div class="content">'+
+                     return   '<div class="response-message talk-bubble-2 tri-right left-top">'+
+                                '<div class="talktext">'+
                                     '<p>'+query+'</p>'+
                                 '</div>'+
                               '</div>';
                 }
 
                 function sentMessage(response){
-                    return   '<div class="message.you">'+
-                                '<div class="content">'+
+                    return   '<div class="sent-message talk-bubble tri-right round border right-top">'+
+                                '<div class="talktext">'+
                                     '<p>'+response+'</p>'+
                                 '</div>'+
                             '</div>';
@@ -207,10 +471,7 @@ try {
                
             });
         });
-</script>
-
-  </body>
+    </script>
+</body>
 </html>
-
-<?php } 
-?>
+    <?php } ?>
