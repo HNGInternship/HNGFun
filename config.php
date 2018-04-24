4438c5bd03e448a748e8bdf0dec431145199d345
Patsoks Patsokari <33361294+patrex@users.noreply.github.com>
2018-04-24 22:05:49 +0100

    Commit n

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 */
 define ('DB_USER', "root");
 define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "hngfun");
+define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "");
 define ('DB_HOST', "localhost");
