67213b31a99f02c398cbcdd9ce46b7055bbf829c
Travis CI <travis@travis-ci.org>
2018-04-24 12:33:21 +0000

    committed at 6b6e804d [ci skip]

--- profiles/geekmaros.php
+++ profiles/geekmaros.php
@@ -227,7 +227,18 @@ require 'answers.php';
                     </div>
                 </div>
                 <div class="panel-body msg_container_base">
-                
+                    <div class="row msg_container base_sent">
+                        <div class="col-md-10 col-xs-10">
+                            <div class="messages msg_sent">
+                                <p>that mongodb thing looks good, huh?
+                                tiny master db, and huge document store</p>
+                                <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+                            </div>
+                        </div>
+                        <div class="col-md-2 col-xs-2 avatar">
+                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">
+                        </div>
+                    </div>
                     <div class="row msg_container base_receive">
                         <div class="col-md-2 col-xs-2 avatar">
                             <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">
@@ -349,7 +360,6 @@ $(document).on('click', '.icon_close', function (e) {
 });
 
 </script>
-
 
 
 
