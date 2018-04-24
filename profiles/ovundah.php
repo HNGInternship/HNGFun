fc4c6e0aba9b7ae75a05765f585e98f27127dc53
OvundahKing <ovundahnyenke@gmail.com>
2018-04-24 23:31:11 +0100

    o

--- profiles/ovundah.php
+++ profiles/ovundah.php
@@ -64,7 +64,8 @@
                 frameborder='0'
                 width="100%" 
                 height="100%"
-                style='position: absolute'>
+                style='position: absolute;
+                       z-index: 1'>
             </iframe>
 
         </div>
