c5047ee13299b7de980517ed6328e2a52c4499ea
Travis CI <travis@travis-ci.org>
2018-04-24 12:43:04 +0000

    committed at b22f5b5a [ci skip]

--- profiles/uncletee.php
+++ profiles/uncletee.php
@@ -74,8 +74,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 
 
-//
-//print_r(askQuestion("hello"));
+
 function askQuestion($string){
     global $botVersion;
     $questionAsked =  prepareInputParams($string);
@@ -104,7 +103,7 @@ function askQuestion($string){
 
 }
 
-performTraining('train:Give me latest news');
+//performTraining('train:Give me latest news');
 function performTraining($string){
 
     $delimeters         = [":","#"];
@@ -112,7 +111,7 @@ function performTraining($string){
 
 
     $isBotTrainnable = isTrainable($trainnigParameters);
-    if(prepareInputParams($trainnigParameters[0]) == "train"){
+    if( prepareInputParams($trainnigParameters[0]) == "train"){
         if ($isBotTrainnable["code"] == 204){
             return  $isBotTrainnable;
         }
