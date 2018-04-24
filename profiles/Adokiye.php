e6014e4c0aacfeacb78cdd121c14c199bebb764c
Adokiye <38386612+Adokiye@users.noreply.github.com>
2018-04-24 10:45:43 +0100

    hh

--- profiles/Adokiye.php
+++ profiles/Adokiye.php
@@ -84,10 +84,9 @@ $input = trim($input);
                 $question = strtolower($input);
                 $question = str_replace('?', '', $question);
                 $question = trim($question);
-                echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "$question";
-                var_dump($question);
-                print_r($result = mysqli_query($conn, "SELECT * FROM chatbot WHERE LOWER(question) like '%$question%'"));
-                print_r($fetched_data = mysqli_fetch_all($result, MYSQLI_ASSOC));
+                echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
+                $result = mysqli_query($conn, "SELECT * FROM chatbot WHERE LOWER(question) like '%$question%'");
+                $fetched_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
                 $row_cnt = $result->num_rows;
                 $rand = rand(0, $row_cnt - 1);
                 if ($row_cnt > 0) {
