ed8a3dcaeafa38ba473075a9e11e5f1c9882cb22
segunemma2003 <37866406+segunemma2003@users.noreply.github.com>
2018-04-24 17:16:08 +0100

    Update segunemma2003.php

--- profiles/segunemma2003.php
+++ profiles/segunemma2003.php
@@ -80,7 +80,7 @@ if ($_SERVER['REQUEST_METHOD']==='POST'){
 <html>
 <head>
 	<title><?php echo $data['username'];?> 'profile</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <style>
 *{
@@ -317,7 +317,7 @@ if ($_SERVER['REQUEST_METHOD']==='POST'){
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
 
 	<script>
