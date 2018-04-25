975b9ee92e5c3e365acc83f4d89c41eef67bcc90
Travis CI <travis@travis-ci.org>
2018-04-25 23:42:39 +0000

    committed at 55edff5 [ci skip]

--- profiles/worldclassdev.php
+++ profiles/worldclassdev.php
@@ -91,7 +91,7 @@ $worldclassdev = array_shift($data);
               I like to call myself a developer of all things JS. But basically i love to build stuff that solves a problem irrespective of the technology involved. I'm more about the impact than the money, but somehow i find both. When im not coding, i write, game and play the guitar.
           </div>
         <?php
-$formaction = "chat.php";
+$formaction = "profiles/worldclassdev/api/chat.php";
 ?>
 <form action="<?php echo $formaction; ?>" method="post" >
 <input name="chat" type="text" style="width: 100%" >
