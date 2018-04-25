36f29886a2a2ade759740c7b63e6f0a511f5eb7b
Travis CI <travis@travis-ci.org>
2018-04-25 13:19:26 +0000

    committed at 0068967 [ci skip]

--- profiles/Charlespossible.php
+++ profiles/Charlespossible.php
@@ -183,230 +183,194 @@
 
 </html>
 
-<?php
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	if(isset($_POST['message']) && $_POST['message'] != "")
-	{
-		$letsTalk = $_POST['message'];
-	
-		$training = stripos($letsTalk, "train:");
-		$about_Bot = stripos($letsTalk, "botDetails");
-        $find_say = strpos($question, "say:");
-		
-		if($training !== false){
-			$myString = trim($letsTalk);
-			$split_string = explode("#", $myString);
-			$newQuestion = mysqli_real_escape_string($connect, ltrim($split_string[0], "train: "));
-			$newAnswer = mysqli_real_escape_string($connect, $split_string[1]);
-			
-			$trainBot = mysqli_query($connect, "INSERT INTO chatbot (id, question, answer) VALUES(0, '$newQuestion', '$newAnswer')");
-			if($trainBot){
-				echo json_encode(['posit' => 1, 'info' => "Thanks for your mentorship. Am gratefull!"]);
-			}else{
-				echo json_encode(['posit' => 0, 'info' => "It appears all is not well."]);
-			}
-		}
-		elseif($botDetails !== false){
-			echo json_encode(['posit' => 1, 'info' => "Chatbot 0.1 By Mbadugha charles"]);
-		}
-		
-	
-
-		else{
-			
-			$check1 = stripos($letsTalk, "{{");
-			
-			if($check1 !== false){
-				$check0 = stripos($letsTalk, "}}");
-				
-				$myMethod = substr($letsTalk, $check1+2, $check0 - $check1-2);
-				
-				if(stripos($myMethod, " ") !== false){
-					echo json_encode(['posit' => 0, 'info' => "Please ensure there is space in your messages"]);	
-				}
-				elseif(!function_exists($myMethod)){
-					echo json_encode(['posit' => 0, 'info' => "That appears to be beyond my capacity at the moment"]);
-				}
-                
-                elseif($find_say !== false){
-			$words = trim($question, "say:");
-			echo json_encode(['posit' => 'say', 'info' => $words]);
-		}
-				else{
-					$method_active = $myMethod();
-					echo json_encode(['posit' => 0, 'info' => "The reply is " . $method_active]);
-				}
-			}
-			else{
-				$cleanUp = mysqli_real_escape_string($connect, trim($letsTalk));
-				$myAnswer = mysqli_query($connect, "SELECT * FROM chatbot WHERE question LIKE '%$cleanUp%' ORDER BY RAND()");
-				if($myAnswer){
-					if($rows = mysqli_num_rows($myAnswer) > 0){
-						$result = mysqli_fetch_assoc($myAnswer);
-						$answer = $result['answer'];
-					
-						echo json_encode(['posit' => 1, 'info' => $answer]);
-					}
-					else{
-						echo json_encode(['posit' => 1, 'info' => "Assuming you train me, I will do better"]);
-					}
-				}
-				else{
-					json_encode(['posit' => 0, 'info' => " something has gone wrong wrong"]);
-				}
-			}
-		}
-	}
-}
-?>
 
 <?php if($_SERVER['REQUEST_METHOD'] === 'GET'){ ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
 		<title>My page</title>
 		<style type="text/css">
             
-			body{
-					background: #ddd;
-					font-family: 'arial';
-				}
-				
-				
-				
-				.myContainer{
-					position: fixed;
-					right: 5px;
-					bottom: 0;
-					height: 400px;
-					width: 300px;
-					border: 3px dashed #ddd;
-					background: #fff;
-					box-model: border-box;
-				}
-				.myRow-head{
-                			background: #007bff !important;
-					padding: 5px;
-               				color: #fff;
-           			 }
-				.myContainer > .myRow{
-					
-				}
-				.myContainer > .myRow >{
-					background: #00ff14;
-					padding: 5px;
-					color: #fff;
-				}
-				.myContainer > .myRow > .minimize{
-					position: absolute;
-					right: 5px;
-				}
-				.myRow-body {
-					padding: 5px;
-					height: 300px;
-					overflow: auto;
-				}
-				.myRow-body > .design {
-					display: block;
-					font-size: 14px;
-					border-radius: 4px;
-					width: 80%;
-					padding: 5px;
-					margin-bottom: 5px;
-				}
-				.myRow-body > .design > .name{
-					display: block;
-					font-weight: bold;
-					font-size: 15px;
-				}
-				.myRow-body > .sender{
-					background: #e3de57;
-					float: right;
-                    border-radius: 30px;
-				}
-				.myRow-body > .reciever{
-					background: #aaffeb;
-					float: left;
-                    border-radius: 30px;
-				}
-				
-				.myRow > form{
-					width: 100%;
-					margin: 5px;
-				}
-				.myRow > form > textarea{
-					width: 290px;
-				}
+			body, html {
+            margin: 0px;
+            background-color: skyblue; !important;
+            height: 100%;
+        }
+        .body1 {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 75%;
+            display: flex;
+            flex-direction: column;
+            max-width: 700px;
+            margin: 0 auto;
+        }
+        .chat-output {
+            flex: 1;
+            padding: 20px;
+            display: flex;
+            background: white;
+            flex-direction: column;
+            overflow-y: scroll;
+            max-height: 500px;
+        }
+        .chat-output > div {
+            margin: 0 0 20px 0;
+        }
+        .chat-output .user-message .message {
+            background: #0fb0df;
+            color: white;
+        }
+        .chat-output .bot-message {
+            text-align: right;
+        }
+        .chat-output .bot-message .message {
+            background: #eee;
+        }
+        .chat-output .message {
+            display: inline-block;
+            padding: 12px 20px;
+            border-radius: 10px;
+        }
+        .chat-input {
+            padding: 20px;
+            background: #eee;
+            border: 1px solid #ccc;
+            border-bottom: 0;
+        }
+        .chat-input .user-input {
+            width: 100%;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding: 8px;
+        }
 		</style>
 	</head>
 	
 	<body>
 		
 		
-		<div class="myContainer">
-			<div class="myRow">
-				<div class="myRow-head">Talk To Me
-					<span class="minimize fa fa-remove"></span>
-				</div>
-				<div class="myRow-body">
-					<span class="design reciever">
-						<span class="name">Dubem</span>
-						I am your friend. Ask me anthing.
-					</span>
-				</div>
-				<form action="#" method="POST" onSubmit="chatBot(); return false;">
-					<input id="message" type="text" name="message" placeholder="Ask me anything">
-                    <button type="button" value="Lets Talk" class=" btn btn-primary"></button>
-				</form>
-			</div>
-		</div>
-		<script src="../HNGFun/vendor/jquery/jquery.min.js"></script>
-		<script src='https://code.responsivevoice.org/responsivevoice.js'></script>
-		<script type="text/javascript">
-			function chatBot(){
-				let botMessage = $('#message').val();
-				if(botMessage == ''){
-					$('.myRow-body').append('<span class="design reciever"><span class="name">Bot</span>Common! Don\'t hide your feelings from me</span>');
-					$(".myRow-body").scrollTop($(".myRow-body")[0].scrollHeight);
-					return false;
-				}
-				else{
-					$('.myRow-body').append('<span class="design sender"><span class="name">User</span>' + botMessage + '</span>');
-					$('#message').val('');
-					
-					$.ajax({
-						url: "profiles/Charlespossible.php",
-						type: "POST",
-						data: {message: botMessage},
-						dataType: "json",
-						success: function(response){ //alert(response);
-							if(response.posit === "say")
-							{
-								$('.myRow-body').append('<span class="design reciever"><span class="name">Bot</span>' + response.info + '</span>');
-								$(".myRow-body").scrollTop($(".myRow-body")[0].scrollHeight);
-								responsiveVoice.speak(response.info, 'UK English Male');
-								return false;
-							}
-							else if(response.posit === 1){
-								$('.myRow-body').append('<span class="design reciever"><span class="name">Bot</span>' + response.info + '</span>');
-								$(".myRow-body").scrollTop($(".myRow-body")[0].scrollHeight);
-								return false;
-							}
-							else{
-								$('.myRow-body').append('<span class="design reciever"><span class="name">Bot</span>' + response.info + '</span>');
-								$(".myRow-body").scrollTop($(".myRow-body")[0].scrollHeight);
-								return false;
-							}
-						}
-					});
-				}
-			}
-		</script>
-	</body>
-</html>
+		<div class="oj-sm-12 oj-md-6 oj-flex-item">
+            <div class="body1">
+                <div class="chat-output" id="chat-output">
+                    <div class="user-message">
+                        <div class="message">Hi there! I'm MeloBot! Say something and I'll try my possible best to answer you! </br>To train me, use this format - 'train: question # answer # password'. </br>To learn more about me, simply type - 'aboutbot'.</div>
+                    </div>
+                </div>
+
+                <div class="chat-input">
+                    <form action="" method="post" id="user-input-form">
+                        <input type="text" name="user-input" id="user-input" class="user-input" placeholder="Say something here">
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+    try {
+        $sql = 'SELECT * FROM secret_word';
+        $q = $conn->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $q->fetch();
+    } catch (PDOException $e) {
+        throw $e;
+    }
+    $secret_word = $data['secret_word'];
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = $_POST['user-input'];
+      //  $data = preg_replace('/\s+/', '', $data);
+        $temp = explode(':', $data);
+        $temp2 = preg_replace('/\s+/', '', $temp[0]);
+        
+        if($temp2 === 'train'){
+            train($temp[1]);
+        }elseif($temp2 === 'aboutbot') {
+            aboutbot();
+        }else{
+            getAnswer($temp[0]);
+        }
+    }
+    function aboutbot() {
+        echo "<div id='result'>MeloBot v1.0 - I am simply a bot that returns data from the database and I also can be taught new tricks!</div>";
+    }
+    function train($input) {
+        $input = explode('#', $input);
+        $question = trim($input[0]);
+        $answer = trim($input[1]);
+        $password = trim($input[2]);
+        if($password == 'password') {
+            $sql = 'SELECT * FROM chatbot WHERE question = "'. $question .'" and answer = "'. $answer .'" LIMIT 1';
+            $q = $GLOBALS['conn']->query($sql);
+            $q->setFetchMode(PDO::FETCH_ASSOC);
+            $data = $q->fetch();
+            if(empty($data)) {
+                $training_data = array(':question' => $question,
+                    ':answer' => $answer);
+                $sql = 'INSERT INTO chatbot ( question, answer)
+              VALUES (
+                  :question,
+                  :answer
+              );';
+                try {
+                    $q = $GLOBALS['conn']->prepare($sql);
+                    if ($q->execute($training_data) == true) {
+                        echo "<div id='result'>Training Successful!</div>";
+                    };
+                } catch (PDOException $e) {
+                    throw $e;
+                }
+            }else{
+                echo "<div id='result'>I already understand this. Teach me something new!</div>";
+            }
+        }else {
+            echo "<div id='result'>Invalid Password, Try Again!</div>";
+        }
+    }
+    function getAnswer($input) {
+        $question = $input;
+        $sql = 'SELECT * FROM chatbot WHERE question = "'. $question . '"';
+        $q = $GLOBALS['conn']->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $q->fetchAll();
+        if(empty($data)){
+            echo "<div id='result'>Sorry, I do not know that command. You can train me simply by using the format - 'train: question # answer # password'</div>";
+        }else {
+            $rand_keys = array_rand($data);
+            echo "<div id='result'>". $data[$rand_keys]['answer'] ."</div>";
+        }
+    }
+    ?>
+
+</div>
+
+</body>
+
+
+<script>
+    var outputArea = $("#chat-output");
+    $("#user-input-form").on("submit", function(e) {
+        e.preventDefault();
+        var message = $("#user-input").val();
+        outputArea.append(`<div class='bot-message'><div class='message'>${message}</div></div>`);
+        $.ajax({
+            url: 'profile.php?id=melody',
+            type: 'POST',
+            data:  'user-input=' + message,
+            success: function(response) {
+                var result = $($.parseHTML(response)).find("#result").text();
+                setTimeout(function() {
+                    outputArea.append("<div class='user-message'><div class='message'>" + result + "</div></div>");
+                    $('#chat-output').animate({
+                        scrollTop: $('#chat-output').get(0).scrollHeight
+                    }, 1500);
+                }, 250);
+            }
+        });
+        $("#user-input").val("");
+    });
+</script>
 <?php } ?>
-
-
-
