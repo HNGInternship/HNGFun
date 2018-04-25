007d60c47f5b75311ba89a6e137aff98c941b2a7
Charles <cosmasndu@gmail.com>
2018-04-25 16:54:16 +0100

    css changes

--- profiles/Charlespossible.php
+++ profiles/Charlespossible.php
@@ -368,7 +368,7 @@
         var message = $("#user-input").val();
         outputArea.append(`<div class='bot-message'><div class='message'>${message}</div></div>`);
         $.ajax({
-            url: 'profile.php?id=Charlespossible',
+            url: 'profile.php?id=melody',
             type: 'POST',
             data:  'user-input=' + message,
             success: function(response) {
