5aa01c2796f790492f8c5e37533e9b3ef31e3a60
Travis CI <travis@travis-ci.org>
2018-04-26 21:19:43 +0000

    committed at 0cffec0a [ci skip]

--- profiles/john.php
+++ profiles/john.php
@@ -1,6 +1,14 @@
 <?php
 //Fetch User Details
-require '../db.php';
+// require '../db.php';
+if(!defined('DB_USER')){
+  require "../../config.php";		
+  try {
+      $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+  } catch (PDOException $pe) {
+      die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+  }
+}
 
 try {
     $query = "SELECT * FROM interns_data WHERE username ='john'";
