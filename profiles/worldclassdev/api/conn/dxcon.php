91905074bfb92703fd8c9481800233de72c7c4e9
Justine Philip <philip.obosi@eng.uniben.edu>
2018-04-26 02:12:43 +0100

    Update dxcon.php

--- profiles/worldclassdev/api/conn/dxcon.php
+++ profiles/worldclassdev/api/conn/dxcon.php
@@ -1,10 +1,8 @@
 <?php
     //error_reporting(0);
-    $DB_USER = 'root';
-	$DB_PASSWORD = '';
-	$DB_HOST = 'localhost';
-	$DB_NAME = 'hng_fun';
+
+require_once('../../../config.php');
 	$dbc = mysqli_connect ($DB_HOST, $DB_USER, $DB_PASSWORD) or $error = mysqli_error($dbc);
-	mysqli_select_db($dbc,$DB_NAME) or $error = mysqli_error($dbc);
+	mysqli_select_db($dbc,$DB_DATABASE) or $error = mysqli_error($dbc);
 	mysqli_query($dbc, "SET NAMES `utf8`") or $error = mysqli_error($dbc);
 	if(@$error){ die($error);}
