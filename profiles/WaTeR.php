883bec287ab144d4869529b086ed5584a25f4af8
tituswater <olowoyeyeoluwafemi93@yahoo.com>
2018-04-26 11:07:36 +0100

    merge profile

--- profiles/WaTeR.php
+++ profiles/WaTeR.php
@@ -80,8 +80,8 @@ $data = $stmt->fetch(PDO::FETCH_ASSOC);
   </style>
 
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ -->
 </head>
 <body>
 
