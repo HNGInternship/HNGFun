96ea8fc037aac8ce1e761dfda28d4e71a2e6525b
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-25 20:23:38 -0700

    my bot

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -1,13 +1,15 @@
 <?php 
-	
+
 if(!defined('DB_USER')){
-	require "../..config.php";
-	try{
-		$conn = new PDO("mysql:host=" . DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-	}catch (PDOException $pe){
-		die("Could not connect to the database" . DB_DATABASE . ": " . $pe->getMessage());
+	require_once __DIR__."/../../config.php";
+	//require_once __DIR__."/../config.php";    
+	try {
+		$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+	} catch (PDOException $pe) {
+		die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
 	}
 }
+
 
 
 $result = $conn->query("Select * from secret_word LIMIT 1");
