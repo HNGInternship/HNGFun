2e2bac087c2c0f06779697f3a1f97eed5c2cdc6a
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-25 21:04:17 -0700

    bot

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -1,15 +1,16 @@
 <?php 
 
-/*if(!defined('DB_USER')){
-	require_once __DIR__."/../../config.php";
+if(!defined('DB_USER')){
+	require '../../config.php';
 	//require_once __DIR__."/../config.php";    
 	try {
 		$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 	} catch (PDOException $pe) {
 		die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
 	}
-}*/
-require '../db.php'
+}
+
+
 
 
 
