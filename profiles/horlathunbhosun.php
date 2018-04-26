64f56d0cd8584e0510a3de7dadb5c346fa31a009
horlathunbhosun <horlathunbhosun@gmail.com>
2018-04-26 17:27:44 +0100

    Edited my profile

--- profiles/horlathunbhosun.php
+++ profiles/horlathunbhosun.php
@@ -78,12 +78,22 @@ p {
 }
 
 
-.chat .user-photo {
+/*.chat .user-photo {
 	width: 60px;
 	height: 60px;
 	background: #ccc;
 	border-radius: 50%;
+	 content:url('http://res.cloudinary.com/horlathunbhosun/image/upload/v1524247081/avatar.png');
 }
+
+
+.chat .user-friend {
+	width: 60px;
+	height: 60px;
+	background: #ccc;
+	border-radius: 50%;
+	 content:url('http://res.cloudinary.com/horlathunbhosun/image/upload/v1524247081/avatar-user-coder-3579ca3abc3fd60f-512x512.png');
+}*/
 
 .chat .chat-message {
 	width: 80%;
@@ -153,13 +163,9 @@ p {
 <?php
 if(!defined('DB_USER')){
   require "../../config.php";
-	// require_once ('../db.php');
+	// require_once ('db.php');
 }
-try {
-  $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-} catch (PDOException $pe) {
-  die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-}
+
 
 global $conn;
 
@@ -175,18 +181,74 @@ global $conn;
    // $user = $result2->fetch();
 
  
-
+ if(isset($_POST['message']))
+    {
+                    array_push($_SESSION['chat_history'], trim($_POST['message']));
+                    if(stripos(trim($_POST['message']), "train") === 0)
+        {
+          
+         $args = explode("#", trim($_POST['message']));
+          $question = trim($args[1]);
+          $answer = trim($args[2]);
+          $password = trim($args[3]);
+          if($password == "password")
+          {
+              // Password perfect
+            $trainQuery = $db->prepare("INSERT INTO chat_bot (question , answer) VALUES ( :question, :answer)");
+            if($trainQuery->execute(array(':question' => $question, ':answer' => $answer)))
+            {
+                array_push($_SESSION['chat_history'], "That works! okay continue chatting");
+            }
+            else
+            {
+                array_push($_SESSION['chat_history'], "Something went wrong somewhere");
+            }
+          }
+          else
+          {
+              // Password not correct
+             array_push($_SESSION['chat_history'], "The password entered was incorrect");
+          }
+        }
+        else
+        {
+            // Not Training
+          $questionQuery = $db->prepare("SELECT * FROM chat_bot WHERE question LIKE :question");
+          $questionQuery->execute(array(':question' => trim($_POST['message'])));
+          $qaPairs = $questionQuery->fetchAll(PDO::FETCH_ASSOC);
+          if(count($qaPairs) == 0)
+          {
+                    $answer = "Sorry, I cant understand your details";
+          } else
+          {
+            $answer = $qaPairs[mt_rand(0, count($qaPairs) - 1)]['answer'];
+            $bracketIndex = 0;
+            while(stripos($answer, "{{", $bracketIndex) !== false)
+            {
+              $bracketIndex = stripos($answer, "{{", $bracketIndex);
+              $endIndex = stripos($answer, "}}", $bracketIndex);
+              $bracketIndex++;
+                  $function_name = substr($answer, $bracketIndex + 1, $endIndex - $bracketIndex -1);
+                  $answer = str_replace("{{".$function_name."}}", call_user_func($function_name), $answer);
+            }
+          }
+          array_push($_SESSION['chat_history'] , $answer);
+        }
+    }
+    if(!isset($_SESSION['chat_history']))
+    {
+                $_SESSION['chat_history'] = array('Hello! How can I help? Ask for my help. To train me, enter the command "train # question # answer # password');
+    }
+    $messages = $_SESSION['chat_history'];
 ?>
 
 
 
 
-<?php 
-   
-  
 
 
- ?>
+
+
 
 
 <div class="container" style="padding-top: 100px;">
@@ -206,39 +268,39 @@ global $conn;
 						
 			</div>
 				</div>
-			<div class="col-md-6">			
+			<div class="col-md-6">	
+				<!-- http://localhost/HNGFun/profiles/horlathunbhosun.php -->
+			<!-- localhost/Hprofile.php?id=horlathunbhosun	 -->
 			<div class="chatbox">
+					<h5 style="font-size: 30px;"><center><span class="">Hi, My name is scoobydoo.</span></center>
+	</h5>
 				<div class="chatlogs">
-			<div class="chat friend">
-				<div class="user-photo"></div>
-				<p class="chat-message">What's up, Brother ..!!</p>	
-			</div>
-			<div class="chat friend">
-				<div class="user-photo"></div>
-				<p class="chat-message">What's up, Brother ..!!</p>	
-			</div>
-			<div class="chat self">
-				<div class="user-photo"></div>
-				<p class="chat-message">What's up ..!!</p>	
-			</div>
-			<div class="chat self">
-				<div class="user-photo"></div>
-				<p class="chat-message">A única área que eu acho, que vai exigir muita atenção nossa, e aí eu já aventei a hipótese de até criar um ministério. É na área de... Na área... Eu diria assim, como uma espécie de analogia com o que acontece na área agrícola.</p>	
-			</div>
-			<div class="chat friend">
-				<div class="user-photo"></div>
-				<p class="chat-message">No meu xinélo da humildade eu gostaria muito de ver o Neymar e o Ganso. Por que eu acho que.... 11 entre 10 brasileiros gostariam. Você veja, eu já vi, parei de ver. Voltei a ver, e acho que o Neymar e o Ganso têm essa capacidade de fazer a gente olhar.
+					
+             <div class="chat friend">
+				 <?php for($index = 0; $index < count($messages); $index++ ) :?>
+				<div class="user-photo <?= ($index % 2 == 0) ? "recieved-message-ctn" : "input-ctn"  ?> "></div>
+				<p class="chat-message <?= ($index % 2 == 0) ? "recieved-message" : "input"  ?>"><?= $messages[$index] ?></p>	
+				 <?php endfor; ?>
 
-				Todos as descrições das pessoas são sobre a humanidade do atendimento, a pessoa pega no pulso, examina, olha com carinho. Então eu acho que vai ter outra coisa, que os médicos cubanos trouxeram pro brasil, um alto grau de humanidade.
-				</p>	
 			</div>
+			
+			<div class="chat self">
+				
+				<div class="user-friend"></div>
+				<!-- <p class="chat-message">What's up ..!!</p>	 -->
+			</div>
+			
+			
 		</div>
+
+		<form action="/profile.php?id=horlathunbhosun" method="POST" >	
+
 		<div class="chat-form">
-			<textarea></textarea>
-			<button>Send</button>
+			<textarea placeholder="Ask/Train me.." name="message"></textarea>
+			<button class="send-query" value="ask Me question">Send</button>
 		</div>
 	</div>
-
+	</form>
 
 </div>
 
@@ -247,3 +309,49 @@ global $conn;
 	</div>	
 </body>
 </html>
+<script type="text/javascript">
+<script>
+    var sampleConversation = [
+        "Hi",
+        "My name is [name]",
+        "Where is Hotels.ng?",
+        "Where is  Nigeria",
+        "Bye",
+        "What is the time"
+        
+    ];
+    var config = {
+        botName: 'Horlathunbhosun',
+        inputs: '#humanInput',
+        inputCapabilityListing: true,
+        engines: [ChatBot.Engines.duckduckgo()],
+        addChatEntryCallback: function(entryDiv, text, origin) {
+            entryDiv.delay(200).slideDown();
+        }
+    };
+    ChatBot.init(config);
+    ChatBot.setBotName("Horlathunbhosun");
+    ChatBot.addPattern("^hi$", "response", "Hello, friend", undefined, "Say 'Hi' to be greeted back.");
+    ChatBot.addPattern("^What is the time$", "response", "The Time is getTime()", undefined, "Say 'What is the time' to be greeted back.");
+    ChatBot.addPattern("^bye$", "response", "See you later...", undefined, "Say 'Bye' to end the conversation.");
+    ChatBot.addPattern("(?:my name is|I'm|I am) (.*)", "response", "hi $1, thanks for talking to me today", function (matches) {
+        ChatBot.setHumanName(matches[1]);
+    },"Say 'My name is [your name]' or 'I am [name]' to be called that by the bot");
+    ChatBot.addPattern("(what is the )?meaning of life", "response", "42", undefined, "Say 'What is the meaning of life' to get the answer.");
+    ChatBot.addPattern("compute ([0-9]+) plus ([0-9]+)", "response", undefined, function (matches) {
+        ChatBot.addChatEntry("That would be "+(1*matches[1]+1*matches[2])+".","bot");
+    },"Say 'compute [number] plus [number]' to make the bot your math calculator");
+</script>  
+
+<script>
+    
+  $(function(){
+$("#addClass").click(function () {
+          $('#qnimate').addClass('popup-box-on');
+            });
+          
+            $("#removeClass").click(function () {
+          $('#qnimate').removeClass('popup-box-on');
+            });
+  })
+</script>
