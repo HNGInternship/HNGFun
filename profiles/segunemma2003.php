2f64e3d6950e9352266df8077540ae613d04dfc8
segunemma2003 <37866406+segunemma2003@users.noreply.github.com>
2018-04-26 20:02:03 +0100

    Update segunemma2003.php

--- profiles/segunemma2003.php
+++ profiles/segunemma2003.php
@@ -351,7 +351,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 					// alert(responseMessage('I am a little bot'));
 				}
                  $.ajax({
-                     url:"/profiles/segunemma2003",
+                     url:"/profiles/segunemma2003.php",
                      dataType: "json",
 		     type:"POST",
                      data : {message: message},
