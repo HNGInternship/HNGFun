0565b07d1f07b9738b5cd5c84a456ef6c614807b
matilbella <dikethelma55@gmail.com>
2018-04-26 08:45:22 +0100

    sponsors page

---
+++ sponsors.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/sponsors.css">
+   <link href="https://fonts.googleapis.com/css?family=Lato|Work+Sans" rel="stylesheet">
+    <title>Sponsors</title>
+</head>
+<body>
+    <div class="container sponsor-container">        
+            <h3 class="sponsorsbg-text pt-5">Our Sponsors</h3><hr/>
+            <p class="sponsors-text text-center pb-4 pt-3">HNG 4.0 has been a life-transforming journey for interns across Africa.<br/>
+        Don’t take our word for it...take theirs.</p>
+        
+        <div class="row sponsor-row">
+            <div class="col-md-4 pl-0">
+                <img src="img/figma2.png" alt="figma" srcset="" class="img-fluid">
+            </div>
+            <div class="col-md-8 pt-4">
+                <h5 class="sponsorbg-text font-weight-bold">Figma Design</h5>
+                <p class="sponsor-subbg-text">Introducing you to a world of interface design with real-time collaboration using figma. First of it's kind, Figma enables teams carry outprojects in one page, while keeping all feedback changes and updates constantly in sync.</p>
+            </div>
+        </div><br/>
+       
+         <div class="row sponsor-row">
+           <div class="col-md-4 pl-0">
+                <img src="img/oracle2.png" alt="oracle" srcset="" class="img-fluid">
+            </div>
+            <div class="col-md-8 pt-4">
+                <h5 class="sponsorbg-text font-weight-bold">Oracle</h5>
+                <p class="sponsor-subbg-text">Introducing you to a world of interface design with real-time collaboration using figma. First of it's kind, Figma enables teams carry outprojects in one page, while keeping all feedback changes and updates constantly in sync.</p>
+            </div>
+        </div><br/>
+
+        <div class="row sponsor-row">
+            <div class="col-md-4 pl-0">
+                <img src="img/bluechip2.png" alt="bluechip" srcset="" class="img-fluid">
+            </div>
+            <div class="col-md-8 pt-4">
+                <h5 class="sponsorbg-text font-weight-bold">Bluechip Technologies</h5>
+                <p class="sponsor-subbg-text">Introducing you to a world of interface design with real-time collaboration using figma. First of it's kind, Figma enables teams carry outprojects in one page, while keeping all feedback changes and updates constantly in sync.</p>
+            </div>
+        </div><br/>
+
+        <div class="row sponsor-row">
+            <div class="col-md-4 pl-0">
+                <img src="img/hotelsng.png" alt="hotelsng" srcset="" class="img-fluid">
+            </div>
+            <div class="col-md-8 pt-4">
+                <h5 class="sponsorbg-text font-weight-bold">Hotels.ng</h5>
+                <p class="sponsor-subbg-text">Introducing you to a world of interface design with real-time collaboration using figma. First of it's kind, Figma enables teams carry outprojects in one page, while keeping all feedback changes and updates constantly in sync.</p>
+            </div>
+        </div>
+
+
+        <p class="sponsors-text text-center pt-4">Do you want to join us in developing the next generation of African tech talents?<br/>
+Feel free to send an email to <span class="font-weight-bold">mark@hotels.ng</span></p>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
