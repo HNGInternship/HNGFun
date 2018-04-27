9f13ba769f431540da0a56b360838a617881bd83
jonddon <37102658+jonddon@users.noreply.github.com>
2018-04-27 09:52:43 +0100

    Update john.php

--- profiles/john.php
+++ profiles/john.php
@@ -1,15 +1,15 @@
 <?php
-require 'db.php';
+
 //Fetch User Details
 
-// if(!defined('DB_USER')){
-//   require "../../config.php";		
-//   try {
-//       $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-//   } catch (PDOException $pe) {
-//       die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-//   }
-// }
+if(!defined('DB_USER')){
+  require "../../config.php";		
+  try {
+      $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+  } catch (PDOException $pe) {
+      die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+  }
+}
 
 try {
     $query = "SELECT * FROM interns_data_ WHERE username ='john'";
