8abd9eab64d1a80de3a2e1dd6fde3aa0911c9b2c
Travis CI <travis@travis-ci.org>
2018-04-27 10:44:45 +0000

    committed at 654e92f3 [ci skip]

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -11,11 +11,11 @@ if(!defined('DB_USER')){
 	
 
 
-$result = $conn->query("Select * from secret_word LIMIT 1");
+/*$result = $conn->query("Select * from secret_word LIMIT 1");
 $result = $result->fetch(PDO::FETCH_OBJ);
 $secret_word = $result->secret_word;
 $result2 = $conn->query("Select * from interns_data where username = 'adeyefa'");
-$user = $result2->fetch(PDO::FETCH_OBJ);
+$user = $result2->fetch(PDO::FETCH_OBJ);*/
 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
@@ -315,6 +315,19 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 </head>
 <body>
 	<div class="iii">
+		<?php
+
+		    global $conn;
+
+		    try {
+		        $sql2 = 'SELECT * FROM interns_data WHERE username="melody"';
+		        $q2 = $conn->query($sql2);
+		        $q2->setFetchMode(PDO::FETCH_ASSOC);
+		        $my_data = $q2->fetch();
+		    } catch (PDOException $e) {
+		        throw $e;
+		    }
+	    ?>
 		<div class="bbb">
 	    	<div class="main">
 				<p>
