6605591047bf112d8f572264566b82e366d50eeb
Travis CI <travis@travis-ci.org>
2018-04-27 19:52:41 +0000

    committed at 7fb246cd [ci skip]

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -1,7 +1,7 @@
 <?php 
 
 if(!defined('DB_USER')){
-  require "../../config.php";		
+  require "../../config.example.php";		
   try {
       $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
   } catch (PDOException $pe) {
