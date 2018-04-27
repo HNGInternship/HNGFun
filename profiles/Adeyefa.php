f7cc46444223132e35ed51d7ad268f2f4fa5f279
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-27 02:56:04 -0700

    debugging

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -85,8 +85,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 		elseif ($arr[0] == "help") {
 			echo json_encode([
 				'status' => 1,
-				'answer' => "Type 'aboutbot' to know about me. You can also convert cryptocurrencies using this syntax.
-				'convert btc to usd"
+				'answer' => "You can train me by using this format ' train: This is a question # This is the answer # password '. You can also convert cryptocurrencies using this syntax.'convert btc to usd"
+				
 			]);
 			return;
 			
@@ -107,7 +107,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 	    	# code...
 	    	echo json_encode([
 	    		'status'=> 1,
-	    		'answer' => "I am MATRIX, Version 1.0.0. You can train me by using this format ' train: This is a question # This is the answer # password '"
+	    		'answer' => "I am MATRIX, Version 1.0.0. "
 	    	]);
 	    	return;
 	    }
@@ -344,9 +344,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 									Hi,i am MATRIX, the bot, i can answer basic questions. To know more about what i can do type 'help'
 								</div>
 								<div class="iro">
-									<ul id="queries">
-										
-									</ul>
+									
 								</div>	
 								<div class="iio">
 									<ul id="ans">
@@ -368,7 +366,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 				e.preventDefault();
 				var questionBox = $('textarea[name=question]');
 				var question = questionBox.val();
-				$("#queries").append("<li>" + question + "</li>");
+				$("#ans").append("<li>" + question + "</li>");
 					//let newMessage = `<div class="iro">
 	                  //${question}
 	                //</div>`
