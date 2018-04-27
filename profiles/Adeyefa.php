5f2df91d9afbd04968d5c82064da28b877ec9e44
Travis CI <travis@travis-ci.org>
2018-04-27 20:30:56 +0000

    committed at 1b243dfa [ci skip]

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -353,7 +353,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			        
 					},
 					error: function(error){
-						//console.log(error);
+						console.log(error);
 				        alert(error);
 					}
 				})	
