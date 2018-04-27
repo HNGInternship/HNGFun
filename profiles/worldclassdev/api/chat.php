f5dee400916815c52a428cfc774929c8c05b7f58
Travis CI <travis@travis-ci.org>
2018-04-27 03:06:30 +0000

    committed at fdc5ee10 [ci skip]

--- profiles/worldclassdev/api/chat.php
+++ profiles/worldclassdev/api/chat.php
@@ -11,7 +11,7 @@
 // echo fopen("db.php");
 require_once("./conn/dxcon.php");
 require_once("regex.php");
-var_dump("Conzz ". $dbc);
+// var_dump($dbc);
 $regex = new regex($dbc); 
 $emails = $regex->fetchanswer($_POST['chat']);
 $dd = json_encode($emails);  
