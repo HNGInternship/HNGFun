bfa464f546a02155c4a7a5c31ebb29a3bc85ab61
nedy <nedyudombat@gmail.com>
2018-04-27 23:35:58 +0100

    reworking

--- profiles/nedy.php
+++ profiles/nedy.php
@@ -1,12 +1,7 @@
 <?php
 require '../../config.php';
 
-    try {
-        $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-      
-    } catch (PDOException $pe) {
-        die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-    }
+    
 //      try {
 //          $intern_data = $conn->prepare("SELECT * FROM interns_data WHERE username = 'nedy'");
 //          $intern_data->execute();
