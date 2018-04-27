15c95c9c8b8e742109de69bb248ff2c0d3044477
VNEwere7 <vewere@localhost.localdomain>
2018-04-27 15:47:39 +0100

    fix merge issues

--- profiles/vewere.php
+++ profiles/vewere.php
@@ -18,42 +18,12 @@
   <link href="https://static.oracle.com/cdn/jet/v4.0.0/default/css/alta/oj-alta-min.css" rel="stylesheet" type="text/css">
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-  
-  <style>
-    /* General */
-    #toggle-visibility {
-      padding-top: 5px;
-      padding-bottom: 5px;
-      border-radius: 5px;
-      border-style: solid;
-      border-width: thin;
-      border-color: #ffffff;
-    }
-=======
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	
-  <style type="text/css">
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-
-
-    /* General Styles */
-
-    body {
-			margin: 0px;
-			background-color: #958080;
-			height: 100%;
-		}
-=======
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	
   <style type="text/css">
 
 
     /* General Styles */
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
 
     body {
 			margin: 0px;
@@ -67,23 +37,6 @@
 
     .text {
       font-family: "Rajdhani", sans-serif;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      color: #ffffff;
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
-      text-align: center;
-      display: vertical;
-		}
-
-    h1 {
-      padding-bottom: 0;
-    }
-
-<<<<<<< HEAD
-    .white {
-=======
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
       color: #ffffff;
       text-align: center;
       display: vertical;
@@ -107,41 +60,6 @@
       margin-right: auto;
     }
 
-<<<<<<< HEAD
-    /* profile-area */ 
-    #top {
-      margin-top: 4%;
-      margin-bottom: 0;
-      padding-top: 20px;
-      padding-bottom: 80px;
-      height: 432px;
-      background: #513E3E;
-      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
-    }
-=======
-    #toggle-visibility:hover {
-      cursor: pointer;
-    }
-
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-
-    /* Profile Styles */
-
-=======
-    h2 {
-      padding-top: 0;
-    }
-
-    #toggle-visibility {
-      border-radius: 5px;
-      border-style: solid;
-      border-width: thin;
-      border-color: #ffffff;
-      width: 20px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-
     #toggle-visibility:hover {
       cursor: pointer;
     }
@@ -149,7 +67,6 @@
 
     /* Profile Styles */
 
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
 		#profile {
       background-color: #513e3e;
       margin-top: 3%;
@@ -163,19 +80,10 @@
     img {
       border-radius: 50%;
       border: 6px solid #958080;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
       display: block;
       margin-left: auto;
       margin-right: auto;
       margin-top: 10%;
-<<<<<<< HEAD
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
     }
 
 
@@ -191,42 +99,18 @@
     }
 
     #chat-area {
-<<<<<<< HEAD
-<<<<<<< HEAD
-      margin-bottom: 0;
-      height: 382px;
-      background: #513E3E;
-      overflow-y: auto;
-      scroll-behaviour: auto;
-      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
-=======
       background-color: #513e3e;
       height: 427px;
       overflow-y: auto;
       scroll-behaviour: auto;
       box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
       padding: 15px;
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-=======
-      background-color: #513e3e;
-      height: 427px;
-      overflow-y: auto;
-      scroll-behaviour: auto;
-      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
-      padding: 15px;
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
     }
 
     #input-area {
       margin-top: 0;
       height: 53px;
       background: #000000;
-<<<<<<< HEAD
-<<<<<<< HEAD
-      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
-=======
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
       margin-bottom: 3%;
       box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
     }
@@ -251,10 +135,6 @@
       border:none; 
       padding-top: 5px; 
       padding-bottom: 5px;
-<<<<<<< HEAD
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
     }
 
     #bot-bubble {
@@ -328,37 +208,6 @@
 
 </head>
 <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<!-- Profile Div -->
-<div class="container" id="profile">
-  <div class="row">
-    <div class="col-md-offset-4 col-md-4" id="top">
-      <div id="image-div">
-        <img src="<?php echo $user->image_filename; ?>" height=180px width=180px>
-      </div>
-      <h1 class="text white"><?php echo $user->name; ?></h1>
-      <h3 class="text black"><strong>@<?php echo $user->username; ?></strong></h3>
-      <br><br>
-      <h4 class="text white">Problem Solver | Student at</h4>
-      <h4 class="text white">University of Ibadan</h4>
-    </div>
-  </div>    
-  
-</div>
-
-<!-- Chat Div -->
-<div class="container hidden" id="chat">
-  <div class="row">
-    <div class="col-md-offset-4 col-md-4" id="chat-area">
-      <div id="bot-bubble">
-        <p>Hi there!</p>
-      </div>
-      <div id="bot-bubble">
-        <p>My name is Bot :p</p>
-=======
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
 
   <div class="oj-flex" id="outer-profile">
     <div id="profile" class="oj-flex-item oj-sm-10 oj-md-6 oj-lg-4"> 
@@ -370,10 +219,6 @@
         </div>            
         <div class="oj-flex-item oj-sm-2">
         </div>
-<<<<<<< HEAD
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
       </div>
       <div class="oj-flex">
         <div class="oj-flex-item oj-sm-3 oj-md-3 oj-lg-3">
@@ -406,39 +251,19 @@
       </div>
       <div id="input-area"> 
         <div class="oj-flex">
-<<<<<<< HEAD
-            <input id="request" placeholder="Ask a question" class="oj-padding-horizontal oj-flex-item oj-sm-9 oj-md-9 oj-lg-9"  type="text" >
-            <button id="send" class="oj-flex-item oj-sm-2 oj-md-2 oj-lg-2" ><i class="fa fa-paper-plane"></i></button> 
-=======
           <!-- <form class="oj-flex"> -->
             <input id="request" placeholder="Ask a question" class="oj-padding-horizontal oj-flex-item oj-sm-9 oj-md-9 oj-lg-9"  type="text" >
             <button id="send" class="oj-flex-item oj-sm-2 oj-md-2 oj-lg-2" ><i class="fa fa-paper-plane"></i></button> 
           <!-- </form> -->
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
         </div>
       </div>
     </div>
   </div>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<br>
-
-<!-- Switch from Profile to Chatbot button -->
-<div class="row">  
-  <div class="col-md-offset-5 col-md-2" id="">
-    <div id="toggle-visibility"><h4 class="text white" id="toggle-text">TEST MY BOT</h4></div>
-=======
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
   <div class="oj-flex">
     <div class="oj-flex-item oj-sm-6 oj-md-4 oj-lg-2" id="toggle-visibility">
       <h4 class="text white" id="toggle-text">TEST MY BOT</h4>
     </div>
-<<<<<<< HEAD
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-=======
->>>>>>> 73fcc20bf8ca275b329d164d67a366c777a9b174
   </div>
 
 </body>
