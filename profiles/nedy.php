f2e16ad7807d4883300a60454226fefba2463e0e
nedy <nedyudombat@gmail.com>
2018-04-27 23:05:15 +0100

    reworking

--- profiles/nedy.php
+++ profiles/nedy.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 if($_SERVER['REQUEST_METHOD']==='GET'){
  try {
      $intern_data = $conn->prepare("SELECT * FROM interns_data WHERE username = 'nedy'");
@@ -99,10 +98,7 @@ if($_SERVER['REQUEST_METHOD']==='GET'){
 //       return ;
 //     }
 // }
-=======
 
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
-?>
 
 <!DOCTYPE html>
 <html lang="en">
