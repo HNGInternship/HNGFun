752b39545201bf0916ed144f7379c8d9cd508cef
Travis CI <travis@travis-ci.org>
2018-04-27 01:54:02 +0000

    committed at af25d0b0 [ci skip]

--- profiles/worldclassdev/api/conn/dxcon.php
+++ profiles/worldclassdev/api/conn/dxcon.php
@@ -2,10 +2,10 @@
     //error_reporting(0);
 
 require_once("../../../config.php");
-	$dbc = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD) or $error = mysqli_error($dbc);
+	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or $error = mysqli_error($dbc);
 	mysqli_select_db($dbc,$DB_DATABASE) or $error = mysqli_error($dbc);
 	mysqli_query($dbc, "SET NAMES `utf8`") or $error = mysqli_error($dbc);
 	var_dump("error:". $error);
 	var_dump("DBC: ". $dbc);
-	var_dump($DB_HOST." ". $DB_USER." ". $DB_PASSWORD);
+	var_dump(DB_HOST." ". DB_USER." ". DB_PASSWORD);
 	if(@$error){ die($error);}
