2894581c2e0edf56a80aae46cfc2521b27eda17c
Travis CI <travis@travis-ci.org>
2018-04-27 13:09:59 +0000

    committed at e8210559 [ci skip]

--- profiles/segunemma2003.php
+++ profiles/segunemma2003.php
@@ -78,10 +78,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 			}
 			else
 			{
-				echo json_encode(['status'=>0, 'data'=>'sorry I can\'t give you an answer at the moment but you can as well teach me <br> 
-				just use the following pattern train: what is the time? # The time is#password']);
+				echo json_encode(['status'=>0, 'data'=>'sorry I can\'t give you an answer at the moment but you can as well teach me <br>just use the following pattern train: what is the time? # The time is#password']);
 			}
 	}
+}
 	else
 	{ 
 		
