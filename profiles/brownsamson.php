d02566c1bfdfa36eb2514b1715f69e1d384827f8
Travis CI <travis@travis-ci.org>
2018-04-28 00:36:11 +0000

    committed at e4309fc4 [ci skip]

--- profiles/brownsamson.php
+++ profiles/brownsamson.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <?php
     require_once 'db.php';
+
     $result = $conn->query("Select * from secret_word LIMIT 1");
     $result = $result->fetch(PDO::FETCH_OBJ);
     $secret_word = $result->secret_word;
-
     $result2 = $conn->query("SELECT * FROM interns_data WHERE username = 'brownsamson'");
     $user = $result2->fetch(PDO::FETCH_OBJ);
 ?>
@@ -27,7 +27,6 @@
                 -webkit-box-sizing: border-box;
                 box-sizing: border-box;
             }
-
             img{
                 user-drag: none;
             user-select: none;
@@ -36,17 +35,13 @@
             -webkit-user-select: none;
             -ms-user-select: none;
             }
-
             p{
                 margin: 20px 0;
                 line-height: 30px;
             }
-
-
             a{
                 text-decoration: none;
             }
-
             .dark{ background-color: #33648B; color: #ffffff;}
             .darker{ background-color: #212939;  color: #ffffff;}
             .darkest{ background-color: #212129;  color: #ffffff;}
@@ -54,50 +49,39 @@
             .fullwidth{width: 100%; display:block;}
             .float-right{float: right;}
             .float-left{float: left;}
-
             .container{
                 width: 100%;
                 /*padding: 0 8.33%;*/
                 display: table;
             }
-
             .body{
                 padding-top: 30px;
                 padding-bottom: 30px;
             }
-
-
-
             .body{
                 font-family: 'Roboto', 'Rubik', sans-serif;
                 font-size: 12pt;
                 width: 100%;
                 background-color: #212939;
             }
-
             #footer{
                 height: 60px;
                 padding: 15px 0;
                 text-align: center;
             }
-
             .socials-container{
                 max-width: 200px;
                 text-align: center;
                 margin-top: 0px;
                 display: inline-block;
             }
-
             .socials{
                 margin: 0 10px;
             }
-
-
             .banner{
               margin-top: 50px;
                 /*height: 10px;*/
             }
-
             /*============== BANNER ANIMATION ======================*/
             .banner-item-container{
                 max-width: 350px;
@@ -106,32 +90,25 @@
                 padding-top: 20px;
                 background: rgba(33,41,57, 0.6);
             }
-
             .banner-item{
                 border-left: 2px solid #8CDEF7;
                 padding-left: 15px;
                 padding-top: 10px;
             }
-
             .banner-item>h4{
                 display: inline;
             }
-
-
             .banner-item>h3{
                 margin-bottom: 5px;
             }
-
             .hello{
                 margin-bottom: 5px;
                 font-weight: 100 !important;
             }
-
             .banner-item>p{
                 margin-bottom: 25px;
                 font-weight: 100 !important;
             }
-
             .banner {
               margin:0px;
               text-align:left;
@@ -145,7 +122,6 @@
               background: url(http://res.cloudinary.com/samsondappa/image/upload/v1523983445/samsonnewautonew.png) no-repeat bottom right;
                 background-color: #212939;
             }
-
             #dropping-texts {
               display: inline-block;
               max-width: 350px;
@@ -157,14 +133,11 @@
               padding: 0 5px 0 0 ;
             /*  border-right: 2px solid rgba(225, 204, 41, 1);*/
               /* height: 25px; */
-
             }
-
             #aboutme{
                 max-width: 650px;
                 margin: auto;
             }
-
             button{
                 border: none;
                 margin: 5px;
@@ -196,24 +169,20 @@
                 font-size: 16px;
                 border-radius: 7px;
             }
-
             .send-button:hover, .button:hover{
                 color: #ffffff !important;
                 background-color: #33648B;
                 cursor: pointer;
                 transition: .5s;
             }
-
             #personal-body{
               margin-top: 90px;
               padding: 30px 0;
             }
-
             .space{
               width: 100%;
               height: 30px;
             }
-
             #chat-icon-con{
               padding: 5px;
               height: 60px;
@@ -225,29 +194,26 @@
               bottom: 40px;
               transition: 1s;
             }
-
             #chat-icon-con:hover{
               background-color: #FFC916;
               cursor: pointer;
               transition: 1s;
             }
-
               #icon-img{
                 width: 100%;
               }
-
               #chatcon{
-
                 width: 350px;
                 height: 85vh;
                 background: #F2F2F3;
                 position: fixed;
                 bottom:30px;
                 right:-100%;
-                padding: 15px 0 10px 15px;
+                padding: 15px 15px 10px 15px;
                 border-radius: 5px;
+                /*display: none;*/
                 transition: right 1s ease-in-out 0.05s;
-
+                /*transition: right 1s ease-in-out 0.05s;*/
               }
               .message-con{
                 width: 100%;
@@ -255,25 +221,22 @@
                 background-color: #FFFFFF;
                 padding: 20px 10px 10px 10px;
                 overflow-y: scroll;
-                display:flex;
-                flex-direction: column-reverse;
-
+                /*display:flex;
+                flex-direction: column-reverse;*/
               }
               #conversation{
-
               }
               .form-group{
                 margin: 10px 0;
-                padding-right: 15px;
+                /*padding-right: 15px;*/
               }
               #message{
                 height: 100%;
                 width: 100%;
                 border-radius: 0;
               }
-
               .textarea-con{
-                padding: 10px;
+                /*padding: 10px;*/
                 width: 100%;
                 height: 14vh;
               }
@@ -291,7 +254,6 @@
                 font-size: .8em;
                 position: relative;
               }
-
               .arrow-left{
                 width: 0;
                 height: 0;
@@ -299,10 +261,9 @@
                 border-bottom:  10px solid transparent;
                 border-right:  10px solid #0D2147;
                 position: absolute;
-                left: -5%;
+                left: -2.5%;
                 bottom: 20%;
               }
-
               .guest{
                 width: 65%;
                 background-color: #FFC916;
@@ -313,7 +274,6 @@
                 font-size: .8em;
                 position: relative;
               }
-
               .arrow-right{
                 width: 0;
                 height: 0;
@@ -321,10 +281,41 @@
                 border-bottom:  10px solid transparent;
                 border-left:  10px solid #FFC916;
                 position: absolute;
-                right: -5%;
+                right: -2.5%;
                 bottom: 20%;
               }
+              @media only screen and (max-width: 572px){
+                #chatcon{
+                  width: 100%;
+                  display: table;
+                  height: 550px;
+                  position: static;
+                  bottom:30px;
+                  padding: 15px 15px 10px 15px;
+                  margin: 20px 0 20px 0;
+                  border-radius: 5px;
+                  display: none;
+                  transition: right 1s ease-in-out 0.05s;
+                }
+                .message-con{
+                  width: 100%;
+                  height: 75%;
+                  background-color: #FFFFFF;
+                  padding: 20px 10px 10px 10px;
+                  overflow-y: scroll;
+                  display: static;
 
+                }
+                .textarea-con{
+                  padding: 0px;
+                  width: 100%;
+                  height: 14vh;
+                }
+                .form-group{
+                  margin: 10px 0;
+                  padding-right: 0px;
+                }
+              }
         </style>
 
     </head>
@@ -349,7 +340,7 @@
         </div>
 
         <div id="chatcon">
-          <div class="message-con">
+          <div class="message-con" id="messageCon">
               <div id="conversation">
             <!-- <div class="bot"><div class="arrow-left"></div>Hi! Good to have you here. My name is Samson Jnr, but you can call me Codmax</div>
             <div class="bot"><div class="arrow-left"></div>Sorry i did't catch your name</div> -->
@@ -427,15 +418,12 @@
             var m = "#749E40"
             var skill = ["I Design   ", "I Code   ", "And I Write too   "];
             var x = 0;
-
-
             function printSentence(id, sentence, speed) {
               var index = 0;
                   timer = setInterval(function() {
                     var char= sentence.charAt(index);
                     if(char === '<') index= sentence.indexOf('>',index);
                     document.getElementById(id).innerHTML= sentence.substr(0,index);
-
                     if(index++ === sentence.length){
                         clearInterval(timer);
                         removeSentence('dropping-texts', skill[x], 50);
@@ -445,14 +433,12 @@
                     }
                   }, speed);
             } //printSentence
-
             function removeSentence(id, sentence, speed) {
               var index = sentence.length;
                   timer = setInterval(function() {
                     var char= sentence.charAt(index);
                     if(char === '<') index= sentence.indexOf('>',index);
                     document.getElementById(id).innerHTML= sentence.substr(0,index);
-
                     if(index-- === 0){
                         clearInterval(timer);
                         x++;
@@ -460,34 +446,35 @@
                     }
                   }, speed);
             } //removeSentence
-
         //    printSentence('intro', intro, 30);
             printSentence('dropping-texts', skill[x], 150);
-
             blink = setInterval(function(){
             document.getElementById('dropping-texts').style.borderRight = "2px solid rgba(225, 204, 41, " + y + ")";
-
                 if (y == 1){
                     y = 0;
                 } else if (y == 0){
                     y = 1;
                 }
             }, 150);
-
-
+            var scrollingElement = (document.scrollingElement || document.body);
+            function scrollToBottom () {
+               scrollingElement.scrollTop = scrollingElement.scrollHeight;
+            }
             var messageBottonBlink = setInterval(function(){
               var chatIco = document.getElementById('chat-icon-con');
               chatIco.style.backgroundColor = m;
-
                 if (m == "#FFC916"){
                     m = "#749E40";
                 } else if (m == "#749E40"){
                     m = "#FFC916";
                 }
-
             }, 800);
         }
 
+        function scrollToBottom(id){
+           var div = document.getElementById(id);
+           div.scrollTop = div.scrollHeight - div.clientHeight;
+        }
   // ------------.....-------......------ AJAX ---------..........---------------------------
         var guestSend = document.getElementById('guest-send');
         var conversation = document.getElementById('conversation');
@@ -501,10 +488,15 @@
         chatIcon.addEventListener("click", function(){
           if (this.innerHTML == realMessageCancel){
             this.innerHTML = realMessageIcon;
+            document.getElementById('chatcon').style.display = "none";
             document.getElementById('chatcon').style.right = "-100%";
+            scrollingElement.scrollTop = 0;
           } else {
             this.innerHTML = realMessageCancel;
+            document.getElementById('chatcon').style.display = "table";
             document.getElementById('chatcon').style.right = "10%";
+            scrollingElement = (document.scrollingElement || document.body)
+            scrollingElement.scrollTop = 600;
             if (firstMessage == 0){
               getMessageSam('intro');
               getMessageSam('request name');
@@ -512,10 +504,8 @@
             }
           }
         });
-
         guestSend.addEventListener('click', function(){
           message = textarea.value;
-
           if (message != ""){
             conversation.innerHTML += '<div class="guest"><div class="arrow-right"></div>' + message + '</div>';
             textarea.value = "";
@@ -528,8 +518,6 @@
               }
             }
         });
-
-
 // var res = str.replace("Microsoft", "W3Schools"); trainpwforhng
         function getMessageSam(messageSam) {
           if (messageSam.length == 0) {
@@ -544,15 +532,14 @@
                       if (last8 == 'givename'){
                         message = senderName;
                       }
-
                       conversation.innerHTML += '<div class="bot"><div class="arrow-left"></div>' + message + '</div>';
+                      scrollToBottom('messageCon');
                   }
               };
               messageSam = messageSam.replace("#", "%23");
               messageSam = messageSam.replace("#", "%23");
               xmlReq.open("GET", "answers.php?qsam=" + messageSam, true);
               xmlReq.send();
-
           }
       }
 </script>
