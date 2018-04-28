57b9e9517ef38cfa66a9c3b7ba60bdc4b49933a2
AdeyefaToba <36008722+sainttobs@users.noreply.github.com>
2018-04-27 21:07:07 -0700

    my bot

--- profiles/Adeyefa.php
+++ profiles/Adeyefa.php
@@ -1,7 +1,7 @@
 <?php 
 
 if(!defined('DB_USER')){
-  require "../../config.example.php";		
+  require "../../config.php";		
   try {
       $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
   } catch (PDOException $pe) {
@@ -12,10 +12,10 @@ if(!defined('DB_USER')){
 
 
 $result = $conn->query("Select * from secret_word LIMIT 1");
-$result = $result->fetch(PDO::FETCH_OBJ);
+$result = $result->fetch(PDO::FETCH_ASSOC);
 $secret_word = $result->secret_word;
 $result2 = $conn->query("Select * from interns_data where username = 'adeyefa'");
-$user = $result2->fetch(PDO::FETCH_OBJ);
+$user = $result2->fetch(PDO::FETCH_ASSOC);
 
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
@@ -290,12 +290,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 			left: -3px;
             background-color: #00b0ff;
 		}
-		.iro{
-			float: right;
-			color: red;
-			font-size: 15px;
-			font-family: Ubuntu;
-		}
 		.iio{
 			float: left;
 			margin-right: 90px;
@@ -349,9 +343,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 								<div class="irr">
 									Hi,i am MATRIX, the bot, i can answer basic questions. To know about my functions type 'help'
 								</div>
-								<div class="iro">
-									
-								</div>	
 								<div class="iio">
 									<ul id="ans">
 											
