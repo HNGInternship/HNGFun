286e08d418374b26e2613edbceb9dd789586262c
Travis CI <travis@travis-ci.org>
2018-04-28 10:27:39 +0000

    committed at 78ffde2 [ci skip]

--- profiles/lexmill.php
+++ profiles/lexmill.php
@@ -13,6 +13,11 @@ $image_filename = $user['image_filename'];
 <!DOCTYPE html>
 <html>
 <head>
+	<script src="http://www.oracle.com/webfolder/technetwork/jet/js/libs/require/require.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
+	
+ <link id="css" rel="stylesheet" href="https://static.oracle.com/cdn/jet/v5.0.0/default/css/alta/oj-alta-min.css" type="text/css"/>
 <!meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body{
