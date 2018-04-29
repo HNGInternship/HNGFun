6c531f214e8e2df9893d7c79e3ffce96414a4778
Comurule <38665978+Comurule@users.noreply.github.com>
2018-04-29 04:27:52 -0700

    vvv
    aaa

--- db.php
+++ db.php
@@ -5,20 +5,17 @@ DO NOT MODIFY THIS FILE!!!
 
 */
 
-<<<<<<< HEAD
+
 require_once 'config.php';
 
-=======
-require_once '../config.php';
 /*
 
 */
->>>>>>> 6b14b11843aade32f1a22dd411259d2b04d4fc3d
 
 try {
-    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-  
-} catch (PDOException $pe) {
+    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD); 
+}
+ catch (PDOException $pe) {
     die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
 }
 
