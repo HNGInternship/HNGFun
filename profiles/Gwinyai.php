a4ea9175cb758d4875d7ddedbabeba0fae177ef3
Travis CI <travis@travis-ci.org>
2018-04-30 20:16:39 +0000

    committed at 18281b33 [ci skip]

--- profiles/Gwinyai.php
+++ profiles/Gwinyai.php
@@ -1168,8 +1168,8 @@ function getTime(){
                 </section>
     
 </section>
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
      <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.6/typed.min.js"></script>
        <script>
          /* Configure auto typing*/
