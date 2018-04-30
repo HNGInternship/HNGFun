f3b0d624e5a5d213cd5aba9b5119ca17c874bd24
Maerryham <lawalmariamyetunde@gmail.com>
2018-04-30 15:27:56 +0100

    My commit

--- profiles/Maerryham.php
+++ profiles/Maerryham.php
@@ -3,11 +3,11 @@
 
 // require '../config.php';
  
- try {
-    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-} catch (PDOException $pe) {
-    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-}
+// try {
+//    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+//} catch (PDOException $pe) {
+//    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+//}
 
  try {
      $sql = "SELECT * FROM secret_word";
