ece6b0cbbe454dec689facd884b46979cff79147
tridax <tri.dax@yahoo.com>
2018-04-30 01:49:05 +0100

    up

--- profiles/tridax.php
+++ profiles/tridax.php
@@ -553,7 +553,7 @@ p { margin: 0; }
 </div>
 
 <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 
     <script>
