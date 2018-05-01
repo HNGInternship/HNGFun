1d607c2bc0b48fe08811cb2d7ca25625a4f086db
Travis CI <travis@travis-ci.org>
2018-05-01 18:27:31 +0000

    committed at d1db3cbe [ci skip]

--- profiles/femi_dd.php
+++ profiles/femi_dd.php
@@ -257,6 +257,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
    </footer>
 </body>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript">
 
 function newElementsForUser(userRequest) {
