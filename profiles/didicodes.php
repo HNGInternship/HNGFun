0cbd753e1ea1f55276aacf640c204c6118e79e3a
edidiong <edidiongasikpo@gmail.com>
2018-05-02 09:58:27 +0100

    UI update on my chatbot

--- profiles/didicodes.php
+++ profiles/didicodes.php
@@ -286,86 +286,72 @@ font-size: 20px;
 </div>
       <div class="chat-form"><input id="input" type="text" placeholder="Press enter after typing" autocomplete="off" />
   </div>
-<script type="text/javascript">
-var trigger = [
-  ["hi","hey","hello"], 
-  ["how are you", "how is life", "how are things"],
-  ["what are you doing", "what is going on"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "Edidiong"],
-  ["your name please",  "your name", "may i know your name", "what is your name"],
-  ["i love you"],
-  ["happy", "good"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice", "thanks", "thank you"],
-  ["bye", "good bye", "goodbye", "see you later"]
-];
-var reply = [
-  ["Hi","Hey","Hello"], 
-  ["Fine", "Pretty well", "Fantastic"],
-  ["Nothing much", "About to go to sleep", "Can you guest?", "I don't know actually"],
-  ["I am 1 day old"],
-  ["I am just a bot", "I am a bot. What about you?"],
-  ["Kani Veri", "My God"],
-  ["I am nameless", "I don't have a name"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["I will", "What about?"],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself", "You are welcome"],
-  ["Bye", "Goodbye", "See you later"]
-];
-var alternative = ["Haha...", "Eh..."];
-document.querySelector("#input").addEventListener("keypress", function(e){
-  var key = e.which || e.keyCode;
-  if(key === 13){ //Enter button
-    var input = document.getElementById("input").value;
-    document.getElementById("user").innerHTML = input;
-    output(input);
-  }
-});
-function output(input){
-  try{
-    var product = input + "=" + eval(input);
-  } catch(e){
-    var text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, ""); //remove all chars except words, space and 
-    text = text.replace(/ a /g, " ").replace(/i feel /g, "").replace(/whats/g, "what is").replace(/please /g, "").replace(/ please/g, "");
-    if(compare(trigger, reply, text)){
-      var product = compare(trigger, reply, text);
-    } else {
-      var product = alternative[Math.floor(Math.random()*alternative.length)];
+<script>
+    window.addEventListener("keydown", function(e){
+    if(e.keyCode ==13){
+        if(document.querySelector("#user-message").value==""||document.querySelector("#user-message").value==null){
+            //console.log("empty box");
+        }else{
+            //this.console.log("Unempty");
+            sendMsg();
+        }
     }
-  }
-  document.getElementById("chatbot").innerHTML = product;
-  speak(product);
-  document.getElementById("input").value = ""; //clear input value
-}
-function compare(arr, array, string){
-  var item;
-  for(var x=0; x<arr.length; x++){
-    for(var y=0; y<array.length; y++){
-      if(arr[x][y] == string){
-        items = array[x];
-        item =  items[Math.floor(Math.random()*items.length)];
-      }
+    });
+    function sendMsg(){
+    var ques = document.querySelector("#user-message");
+    displayOnScreen(ques.value, "sent");
+    if(ques.value === 'aboutbot'){
+        displayOnScreen('Name: BOTLER<br>Version: 1.0 beta*', 'received');
+        return;
     }
-  }
-  return item;
-}
-function speak(string){
-  var utterance = new SpeechSynthesisUtterance();
-  utterance.voice = speechSynthesis.getVoices().filter(function(voice){return voice.name == "DidiBot";})[0];
-  utterance.text = string;
-  utterance.lang = "en-US";
-  utterance.volume = 1; //0-1 interval
-  utterance.rate = 1;
-  utterance.pitch = 2; //0-2 interval
-  speechSynthesis.speak(utterance);
-}
+    //console.log(ques.value);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState ==4 && xhttp.status ==200){
+            processData(xhttp.responseText);
+        }
+    };
+    xhttp.open("POST", "/profiles/nedy.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("ques="+ques.value);
+
+    }
+    function processData (data){
+    data = JSON.parse(data);
+    console.log(data);
+    var answer = data.response;
+    //Choose a random response from available
+    if(Array.isArray(answer)){
+        if(answer.length !=0){
+            var res = Math.floor(Math.random()*answer.length);
+            displayOnScreen(answer[res][0], "received");
+        }else{
+            displayOnScreen("Ooops!! I don't understand what you just said<br>To teach me use this  format<br>train# question # answer # password","received");
+        }
+    }else{
+        displayOnScreen(answer,"received");
+    }
+
+
+
+    }
+    function displayOnScreen(data,align){
+    console.log(data);
+
+    var main= document.querySelector(".messages-area");
+    var div = document.createElement("div");
+    div.classList = align+"-message text-left";
+    var p = document.createElement("p");
+
+    p.classList = "message "+align;
+    p.innerHTML = data;
+    div.appendChild(p);
+    main.appendChild(div);
+    //console.log(data);
+    }
+</script>
 </script>
 </div>
 </div>
 </body>
-</html>
+</html>s
