8f7627e0953cc92c0421762524a914a419771c79
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-05-02 12:57:11 +0100

    Update iambeejayayo.php

--- profiles/iambeejayayo.php
+++ profiles/iambeejayayo.php
@@ -229,7 +229,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
 </div>
 
-<div class="row justify-content-center chatbox">
+<div class=" bg-dark row justify-content-center chatbox">
         <div class="col-lg-3 bg-light">
             <header class="row justify-content-center chatbox-header">
             <div class="card" align="center">
