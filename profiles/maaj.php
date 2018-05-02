4aac238b4ae9b4f81883b55089d2f3971558942e
Travis CI <travis@travis-ci.org>
2018-05-02 09:04:38 +0000

    committed at 2d5b72b [ci skip]

--- profiles/maaj.php
+++ profiles/maaj.php
@@ -278,7 +278,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 					<form method="POST" action="" id= "chat">
 					<input type="text" id="textbox"></input>
 					<input id="send" type="submit" value="Send"></input>
-					new head
+					new head1
 					</form>
 
 				</div>
@@ -297,6 +297,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-json/2.6.0/jquery.json.min.js"></script>
  <script>
+ $(document).ready(function(){
     var message = $("#contain");
 		
 	    $("#chat").on("submit", function(e) {
@@ -312,14 +313,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 				type: 'POST',
 				data: {text_in: text_in},
 				dataType: 'json',
-				success: (response) => {
+				success: function(response) => {
 					
 			        response.answer = response.answer.replace(/(?:\r\n|\r|\n)/g, '<br />'); 
 			        let response_answer = response.answer;
 			        message.append("<div class='bot'><div class='message'><img src='https://res.cloudinary.com/maaj/image/upload/v1524822457/bot.png' width='30px'/>" +response_answer+ "</div></div>");      
 			       	$('#contain').animate({scrollTop: $('#contain').get(0).scrollHeight}, 1100);     
 				},
-				error: (error) => {
+				error: function(error) => {
 	          		alert('error occured')
 						console.log(error);
 				}
@@ -328,6 +329,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			$("#textbox").val("");
 			}
 		});
+		
+	});
 
 </script>
   </body>
