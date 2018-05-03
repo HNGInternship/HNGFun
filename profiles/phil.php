b883048510874c8dabd2e1ce9e4bde465027c957
tobividz <tobividz@github.com>
2018-05-03 17:42:37 +0100

    eas

--- profiles/phil.php
+++ profiles/phil.php
@@ -21,7 +21,6 @@ $secret_word = $result['secret_word'];
 <html>
 <head>
 
-
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
