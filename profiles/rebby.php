1e907529c8594cf17875b95ae1b15747ca63ffe8
Reebby <31336721+Reebby@users.noreply.github.com>
2018-05-04 11:14:58 +0100

    fileupdated

--- profiles/rebby.php
+++ profiles/rebby.php
@@ -451,7 +451,7 @@ class='message'><img src='https://res.cloudinary.com/rebby/image/upload/v1525427
             success: function(response) {
                 var result = $($.parseHTML(response)).find("#result").text();
                 setTimeout(function() {
-                    outputArea.append("<div class='user-message'><div class='message'> <img src='http://res.cloudinary.com/rebby/image/upload/v1525095822/rebby.jpg' alt='Avatar' style='width:20px' class='avater'> " + result + "</div></div>");
+                    outputArea.append("<div class='user-message'><div class='message'><img src='http://res.cloudinary.com/rebby/image/upload/v1525095822/rebby.jpg' alt='Avatar' style='width:20px' class='avater'> " + result + "</div></div>");
                     $('#chat-output').animate({
                         scrollTop: $('#chat-output').get(0).scrollHeight
                     }, 1500);
