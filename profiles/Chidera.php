4f5c150eda8aaf79f24ef9ffc86c190aa9a58c32
je-ni <jenniferolibie@gmail.com>
2018-05-05 20:27:14 +0100

    adds her chatbot

--- profiles/Chidera.php
+++ profiles/Chidera.php
@@ -418,8 +418,8 @@
                 
         </section>    
     </section>
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 <script>
     $(function(){
         $('.chidera_bot-hi').fadeIn(400); //fadeIn the tooltip message
