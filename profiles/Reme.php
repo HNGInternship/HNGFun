34cd949145241c962cffa7e15dba6e11022cc539
Reme <remeajayi@gmail.com>
2018-05-07 05:53:09 +0100

    corrected file

--- profiles/Reme.php
+++ profiles/Reme.php
@@ -1,4 +1,6 @@
 <?php
+ require_once "../../db.php";
+//remove line 2 for it to work online
 $result = $conn->query("Select * from secret_word LIMIT 1");
 $result = $result->fetch(PDO::FETCH_OBJ);
 $secret_word = $result->secret_word;
@@ -12,6 +14,7 @@ $user = $result2->fetch(PDO::FETCH_OBJ);
 <!DOCTYPE html><html class=''>
 <head><script src='//production-assets.codepen.io/assets/editor/live/console_runner-079c09a0e3b9ff743e39ee2d5637b9216b3545af0de366d4b9aad9dc87e26bfd.js'></script><script src='//production-assets.codepen.io/assets/editor/live/events_runner-73716630c22bbc8cff4bd0f07b135f00a0bdc5d14629260c3ec49e5606f98fdd.js'></script><script src='//production-assets.codepen.io/assets/editor/live/css_live_reload_init-2c0dc5167d60a5af3ee189d570b1835129687ea2a61bee3513dee3a50c115a77.js'></script><meta charset='UTF-8'><meta name="robots" content="noindex"><link rel="shortcut icon" type="image/x-icon" href="//production-assets.codepen.io/assets/favicon/favicon-8ea04875e70c4b0bb41da869e81236e54394d63638a1ef12fa558a4a835f1164.ico" /><link rel="mask-icon" type="" href="//production-assets.codepen.io/assets/favicon/logo-pin-f2d2b6d2c61838f7e76325261b7195c27224080bc099486ddd6dccb469b8e8e6.svg" color="#111" /><link rel="canonical" href="https://codepen.io/emilcarlsson/pen/ZOQZaV?limit=all&page=74&q=contact+" />
 <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,300' rel='stylesheet' type='text/css'>
+
 <script src="https://use.typekit.net/hoy3lrg.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
 <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'><link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
@@ -356,13 +359,12 @@ body {
 		</div>
 		<div class="messages">
 			<ul>
-				<li class="sent">
-					<img src="http://emilcarlsson.se/assets/mikeross.png" alt="" />
+            <li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
 					<p>Welcome!</p>
 				</li>
-
-				<li class="sent">
-					<img src="http://emilcarlsson.se/assets/mikeross.png" alt="" />
+				<li class="replies">
+					<img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
 					<p>Search a famous person or topic like you would on Google e.g "Christiano Ronaldo",  "Ngozi Iweala", "Edo people" </p>
 				</li>
 			</ul>
@@ -378,95 +380,7 @@ body {
 </div>
 <script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script><script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 <script >$(".messages").animate({ scrollTop: $(document).height() }, "fast");
-=======
-<!DOCTYPE hmtl>
-<html>
-<head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">   
-    
-    
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-<style>
-
-body, h1, h2, h3, h4, h5, h6, p, nav, div, span
-{
-    font-family: "Comfortaa", cursive; 
-}
-.wrapper {
-    display: flex;
-    align-items: stretch;
-    position: absolute;
-    left: 0;
-    top: 0;
-}
-
-#side-nav {
-    background-color: #2b0b30;
-    color: #ffffff;
-    min-width: 250px;
-    max-width: 250px;
-    min-height: 100vh;
-    
-   }
-
-#side-nav.active {
-    margin-left: -250px;
-}
-@media (max-width: 768px) {
-    #side-nav {
-        margin-left: -250px; 
-/*hides the side nav on smaller screens*/
-    }
-    #side-nav {
-        margin-left: 0;
-    }
-}
-#side-nav ul li
-    {
-        list-style-type: none;
-    }
-    
-#image-frame
-    {
-        padding: 0 50px;
-    }
-#sidenavCollapse
-    {
-     text-align: left; !important
-    }
-</style>
-</head>
-    
-<body>
-<div id="wrapper">
-<div class="row">
-<nav id="side-nav" class="col-lg-3">
-<br><br><br><br>
-<div id="image-frame" >
-<?php echo '<img src="'.$user->image_filename.'" width="150px" class="rounded">'; ?>
-</div>
-<ul>
-<li><i class="fa fa-user"></i> <?php echo $user->name; ?></li>
-<li><i class="fa fa-slack"></i> @<?php echo $user->username; ?></li>
-<li><i class="fa fa-lock"></i> <?php echo $secret_word?></li>
-</ul>
-</nav>
-    
-<div id="content" class="col-lg-9">
-<button type="button" id="sidenavCollapse" class="btn btn-info navbar-btn">
-<i class="glyphicon glyphicon-align-left"></i>
-Toggle Sidebar
-</button>
-</div>
-</div>
-</div>    
-<script>
-$(document).ready(function () {
 $("#profile-img").click(function() {
 	$("#status-options").toggleClass("active");
 });
@@ -475,6 +389,7 @@ $(".expand-button").click(function() {
   $("#profile").toggleClass("expanded");
 	$("#contacts").toggleClass("expanded");
 });
+
 
 
 function newMessage() {
@@ -501,4 +416,3 @@ $(window).on('keydown', function(e) {
 //# sourceURL=pen.js
 </script>
 </body></html>
-
