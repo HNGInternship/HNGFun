53b725ab8a2cec3da0a0bfe9e94010805ee775d2
Victor Jonah <victor.junior40@yahoo.com>
2018-05-07 03:05:10 +0100

    chatbot updated

--- profiles/vectormike.php
+++ profiles/vectormike.php
@@ -1,5 +1,48 @@
 <?php
 
+function getBotInfo() {
+    $bot_version="1.0.1";
+    return "Heyo! I'm Vectormike's smiggle. I'm version " .$bot_version;
+}
+function getBotManual() {
+    return  "Send 'location' to know your location. \n
+    Send 'time' to get the time. \n
+    Send 'about' to know me. \n
+    Send 'help' to see this again. \n
+    To train me, send in this format: \n
+    'train: question # answer # password'";
+}
+function getAge() {
+    $bot_version="1.0.1";
+    return "Vectormike is just 20 years old. As for me, I have got no idea of age. Still " .$bot_version;
+}
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+
+    $PublicIP = get_client_ip();
+    $json  = file_get_contents("https://freegeoip.net/json/$PublicIP");
+    $json  =  json_decode($json ,true);
+    $country =  $json['country_name'];
+    $region= $json['region_name'];
+    $city = $json['city'];
+}
+
 if(!defined('DB_USER')){
     require "../../config.php";		
     try {
@@ -32,7 +75,7 @@ if(!defined('DB_USER')){
   
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        require "../answers.php";
+        //require "../answers.php";
 
         date_default_timezone_set("Africa/Lagos");
 
@@ -57,7 +100,7 @@ if(!defined('DB_USER')){
             }
 
             // Getting Bot Menu or Manual
-            if(preg_replace('([\s])', ' ', trim(strtlower($question))) === 'help') {
+            if(preg_replace('([\s])', ' ', trim(strtolower($question))) === 'help') {
                 echo json_encode([
                     'status' => 1,
                     'answer' => getBotManual()
