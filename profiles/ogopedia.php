b99b8bb4900d7fa1e24f5f6e5e30809a1225686c
Ogoluwa Ojewale <simeondgenius@gmail.com>
2018-05-07 14:18:58 +0100

    added ogopedia profile

--- profiles/ogopedia.php
+++ profiles/ogopedia.php
@@ -1,7 +1,6 @@
 <?php 
 
   if (!defined('DB_USER')) {
-    include_once("../answers.php");
     require '../../config.php';
     try {
       $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
