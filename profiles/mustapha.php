7b2e519256eaa2b887562cc4d355fcf7f2424a34
Mustapha Yusuff <mustaphee94@gmail.com>
2018-05-07 17:07:53 +0100

    Update mustapha.php

--- profiles/mustapha.php
+++ profiles/mustapha.php
@@ -1,5 +1,20 @@
 <?php
 
+include_once("../answers.php"); 
+
+if (!defined('DB_USER')){
+            
+  require "../../config.php";
+}
+try {
+  $sql = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+} catch (PDOException $pe) {
+  die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+}
+
+ global $sql;
+
+
     //fetch-store results
     try {
 
