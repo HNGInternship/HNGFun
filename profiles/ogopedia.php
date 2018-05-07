b31e0f3c771f42d94394271ef4198992e0c87744
Ogoluwa Ojewale <simeondgenius@gmail.com>
2018-05-07 14:37:02 +0100

    added ogopedia prof

--- profiles/ogopedia.php
+++ profiles/ogopedia.php
@@ -1,52 +1,34 @@
 <?php 
-require 'db.php';
+
 try {
-    $intern_data = $conn->prepare("SELECT * FROM interns_data WHERE username = 'ogopedia'");
-    $intern_data->execute();
-    $result = $intern_data->setFetchMode(PDO::FETCH_ASSOC);
-    $result = $intern_data->fetch();
-  
-    $secret_code = $conn->prepare("SELECT * FROM secret_word");
-    $secret_code->execute();
-    $code = $secret_code->setFetchMode(PDO::FETCH_ASSOC);
-    $code = $secret_code->fetch();
-    $secret_word = $code['secret_word'];
- } catch (PDOException $e) {
-     throw $e;
-    }
-  $result = $conn->query("SELECT * from secret_word LIMIT 1");
-  $result = $result->fetch(PDO::FETCH_OBJ);
-  $secret_word = $result->secret_word;
-  $result2 = $conn->query("Select * from interns_data where username = 'ogopedia");
-  $user = $result2->fetch(PDO::FETCH_OBJ);
+    $query = "SELECT * FROM interns_data_ WHERE username='ogopedia'";
+    $resultSet = $conn->query($query);
+    $result = $resultSet->fetch(PDO::FETCH_ASSOC);
+} catch (PDOException $e){
+    throw $e;
+}
+$username = $result['username'];
+$name = $result['name'];
+$picture = $result['image_filename'];
 
 
-// try {
-//     $query = "SELECT * FROM interns_data_ WHERE username='ogopedia'";
-//     $resultSet = $conn->query($query);
-//     $result = $resultSet->fetch(PDO::FETCH_ASSOC);
-// } catch (PDOException $e){
-//     throw $e;
-// }
-// $username = $result['username'];
-// $name = $result['name'];
-// $picture = $result['image_filename'];
+try{
+    $querySecret =  "SELECT * FROM secret_word LIMIT 1";
+    $resultSet   =  $conn->query($querySecret);
+    $result  =  $resultSet->fetch(PDO::FETCH_ASSOC);
+    $secret_word =  $result['secret_word'];
+}catch (PDOException $e){
+    throw $e;
+}
 
-
-// try{
-//     $querySecret =  "SELECT * FROM secret_word LIMIT 1";
-//     $resultSet   =  $conn->query($querySecret);
-//     $result  =  $resultSet->fetch(PDO::FETCH_ASSOC);
-//     $secret_word =  $result['secret_word'];
-// }catch (PDOException $e){
-//     throw $e;
-// }
-// $secret_word =  $result['sample_secret_word'];
-
-// $secret_word = "sample_secret_word";
 
   ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Ogopedia's Profile</title>
 	<link rel="stylesheet" href="../vendor/font-awesome/css/font-awesome.min.css">
 	<style>
 		@import url(https://fonts.googleapis.com/css?family=Montserrat:400,500,300,200,700);
