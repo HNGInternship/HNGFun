544eeb73ae4de2b268a66da435daf5f13e848d99
Reme <remeajayi@gmail.com>
2018-05-07 05:53:34 +0100

    removed test code

--- profiles/Reme.php
+++ profiles/Reme.php
@@ -1,6 +1,5 @@
 <?php
- require_once "../../db.php";
-//remove line 2 for it to work online
+
 $result = $conn->query("Select * from secret_word LIMIT 1");
 $result = $result->fetch(PDO::FETCH_OBJ);
 $secret_word = $result->secret_word;
