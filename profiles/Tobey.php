1b36239cf905aa15701c75cf496ea9826cb109b8
Tobenna Ezike <tobeydaniels@gmail.com>
2018-05-08 10:44:34 +0100

    profile

--- profiles/Tobey.php
+++ profiles/Tobey.php
@@ -1,148 +1,127 @@
-
-<?php   
-include '../config.php';
-global $conn;
-
-$result = $conn->query("Select * from secret_word LIMIT 1");
-  $result = $result->fetch(PDO::FETCH_OBJ);
-  $secret_word = $result->secret_word;
-
-  $result2 = $conn->query("Select * from interns_data where username = 'Tobey'");
-  $user = $result2->fetch(PDO::FETCH_OBJ);
-
-  try {
-    $sql = 'SELECT * FROM secret_word';
-    $q = $conn->query($sql);
-    $q->setFetchMode(PDO::FETCH_ASSOC);
-    $data = $q->fetch();
-} catch (PDOException $e) {
-    throw $e;
-}
-
-
-if($_SERVER['REQUEST_METHOD'] === 'POST')
-{
-          if (!defined('DB_USER')){
-              require '../config.php';
-          }
-          try {
-              $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-            } catch (PDOException $pe) {
-              die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-            }
-     $mesuu = $_POST['message'];
-     $message=strtolower($mesuu);
-     trim($message);
-     $statusTrain = stripos($message, "train:");
-     if($statusTrain)
-     {
-       $newstring=str_replace("train:","","$message");
-        $sets = explode("#", $newstring);
-             $mQuestion= $sets[0];
-             $mAns= $sets[1];
-             $mPwd= $sets[2];
-             if($mPwd=='passcode'){
-             $resultIns = $conn->query("insert into chatbot (`question`, `answer`) values ('$mQuestion','$mAns')" );
-             if($resultIns)
-             {
-               echo json_encode([
-                'status' => 1,
-                       'answer' => "thanks for enlarging my knowledge base"
-                       ]);
-return;
-}
-else {
-echo json_encode([
-  'status' => 1,
-  'answer' => "sorry something went wrong"
-]);
-return;
- // code...
-}
-             }
-             else {
-               echo json_encode([
-                  'status' => 1,
-                  'answer' => "sorry wrong password"
-                ]);
-               // code...
-             }
-return;
-     }
-     
-     if ($message==""){
- echo json_encode([
-    'status' => 1,
-    'answer' => "enter a question  you can also train me "
-  ]);
-return;
-}
-if ($message==""){
-echo json_encode([
-'status' => 1,
-'answer' => "enter a question or you can train me "
-]);
-return;
-}
-     if($message=='aboutbot'){
-       echo json_encode([
-          'status' => 1,
-          'answer' => "TundeChat v1.0"
-        ]);
-return;
-     }
-    if ($message!=''){
-$result2 = $conn->query("select * from chatbot where question = '$message' order by rand()");
-$user = $result2->fetch(PDO::FETCH_OBJ);
-if($user){
-$rows=$user->answer;
-echo json_encode([
-  'status' => 1,
-  'answer' => $rows
-]);
-return;
-}
-else
-{
- echo json_encode([
-    'status' => 1,
-    'answer' =>"sorry i have no answer to that yet .......but you can train me to annswer questions "
-  ]);
-return;
-}
-if ($message==""){
- echo json_encode([
-    'status' => 1,
-    'answer' => "enter a question  or train me"
-  ]);
-return;
-}
-}
-   return;
-}
- ?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
+    <link href='https://fonts.googleapis.com/css?family=Alegreya|Allura|Almendra SC|Romanesco|Source+Sans+Pro:400,700' rel='stylesheet'>
+    <link href="https://static.oracle.com/cdn/jet/v4.0.0/default/css/alta/oj-alta-min.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <style type="text/css">
 
-<!-- jQuery library -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        
+        .body0 {
+            height: 100%;
+        }
 
-<link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+        span {
+            display: inline-block;
+            vertical-align: middle;
+            line-height: normal;
+        }
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+        .main {
+            position: relative;
+            /*top:20px;*/
+            width: 100%;
+            /*padding-top: 300px;*/
+            max-height: 230px;
+            font-family: "Romanesco";
+            line-height: 150px;
+            font-size: 96px;
+            text-align: center;
+        }
+        .text {
+            background: -webkit-linear-gradient(0deg, #FF0F00, rgba(17, 26, 240, 0.55), #EC0F13);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        .under {
+            position: relative;
+            /*top:450px;*/
+            max-height: 100px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .under1 {
+            position: relative;
+            /*top:500px;*/
+            height: 40px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .under2 {
+            position: relative;
+            /*top:540px;*/
+            height: 49.71px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+      
+        .body1 {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 75%;
+            display: flex;
+            flex-direction: column;
+            max-width: 600px;
+            margin: 0 auto;
+        }
+        .chat-output {
+            flex: 1;
+            padding: 10px;
+            display: flex;
+            background: #e0e7e8;
+            font-size:14px;
+            color: ;
+            flex-direction: column;
+            overflow-y: scroll;
+            max-height: 500px;
+        }
+        .chat-output > div {
+            margin: 0 0 20px 0;
+        }
+        .chat-output .user-message .message {
+            background: #34495e;
+            color: white;
+        }
+        .chat-output .bot-message {
+            text-align: right;
+        }
+        .chat-output .bot-message .message {
+            background: #eee;
+        }
+        .chat-output .message {
+            display: inline-block;
+            padding: 12px 20px;
+            margin:3px;
+            border-radius: 10px;
+        }
+        .chat-input {
+            padding: 14px;
+            background: #eee;
+            font-size:14px;       
+            border: 1px solid #ccc;
+            border-bottom: 0;
+        }
+        .chat-input .user-input {
+            width: 98%;
+            border: 1px solid #ccc;
+            border-radius: 20px;
+            padding: 9px;
+            margin-right:10px;
+        }
 
-<title>Ezike Tobenna</title>
-
-<style>
-	
-body {
+        body {
     font:normal 12px/1.6em Arial, Helvetica, sans-serif;
 	color:#2a3845;
 	background:#ffffff;
@@ -296,14 +275,20 @@ ul {
 	font:normal 18px Georgia, serif;
 	color:#300820;
 }
-
+.panel-body{
+    height:350px;
+    position:relative;
+    overflow-y:auto;
+    background:#34495e;
+    padding: 10px;
+}
 .bot{
     height:300px;
     width:305px;
     background:white;
     position: fixed;
     right:0;
-    bottom:38%;
+    bottom:30%;
     border: 1px solid #8e44ad;
     margin-right:10%;   
 }
@@ -311,7 +296,7 @@ ul {
 
 .top-bar {
   background:#e0e7e8;
-  height:38px;
+  height:32px;
   color: #34495e;
   padding: 10px;
   position: relative;
@@ -321,74 +306,13 @@ ul {
    
 }
 
-.input{
-    height:50px;
-    width:100%;
-}
-
-
-.panel-body{
-    height:350px;
-    position:relative;
-    overflow-y:auto;
-    background:#34495e;
-    padding: 10px;
-}
-
-
-.human-message{
-    background:#e0e7e8; 
-    margin: 10px 10px;
-    margin-left:65px;
-    font-size: 16px;
-    color: #34495e;
-    padding:15px;
-    border-radius:4%;
-}
-
-.human-message:before{
-    width: 0;
-    height: 0;
-    content:"";
-    top:0px;
-    margin-left:60px;
-    margin-right:50px;
-    left:-4px;
-    position:absolute;
-    font-size:15px;
-    border-style: solid;
-    border-width: 13px 13px 0 0;
-    border-color: whitesmoke transparent transparent transparent;  
-}
-
-.bot-message{
-    background:#e0e7e8;
-    color: blue;
-    margin: 10px 10px;
-    font-size: 16px;
-    margin-right:60px;
-    margin-left:10px;
-    border-radius:4%;
-    padding:4px;
-}
-
-.bot-message:after{
-    width: 0;
-    height: 0;
-    content:"";
-    position:absolute;
-    top:-5px;
-    right:0;
-    border-style: solid;
-    border-width: 13px 13px 0 0;
-    border-color: transparent transparent transparent #e0e7e8;  
-}
-
-
 </style>
 </head>
 <body>
-<div id="wrapper">
+<div class="container">
+   
+            <div class="body0">
+            <div id="wrapper">
     
     <div id="header">
 		<div id="nav"><a href="index.html">Home</a> | <a href="#">About Me</a> | <a href="#">Contact Me</a> | <a href="#">My Photos</a> | <a href="#">My Videos</a></div>
@@ -443,55 +367,166 @@ ul {
 	</div>
 </div>
 
+        </div>
         <div class="bot panel panel-default">
             <div class="panel-heading top-bar">TundeChat</div>
-            <div class="panel-body">
-                <div class ="bot-message row">Hi, my name is Tunde. Type <br> <span style="color: red">help</span> to see what I can do.</div>
-                <div class="bot-message row">To train me use this format <br> <span style="color: red">train: question #answer #password</span></div>
-                <div class ="bot-message row">Ask me anything</div>
-            </div>
-            <div class="input" id="async">
-            <form action="" class="form-inline" method="POST" id="form">
-                    <div class="input-group mb-2 mr-sm-2">
-                        <input type="text" class="form-control question-input message" name = "message" id="input" placeholder="type your message">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                            <input class="btn-primary" id="send" type="submit"></div>
-                        </div>
+        <div class="panel-body">
+        <div class="oj-sm-12 oj-flex-item">
+            <div class="body1">
+                <div class="chat-output" id="chat-output">
+                    <div class="user-message">
+                        <div class="message">Hey there! it's TundeBot at your service. </div>
+                        <div class="message">To teach me new stuff, use this format - <span style="color: yellow">'train: question # answer # password'.</span> </div>
+                    
+                    <div class="message">To learn more about me, simply type - <span style="color: orange">'aboutbot'.</span></div>
+                  
                     </div>
-            </form>
+                   
+                </div>
+
+                <div class="chat-input">
+                    <form action="" method="post" id="user-input-form">
+                        <input type="text" name="user-input" id="user-input" class="user-input" placeholder="Say something here">
+                    </form>
+                </div>
 
             </div>
         </div>
-   
-<script>
+    </div>
+    </div>
+    </div>
+    <?php
 
-    $(document).ready(function(){
-        $('#form').on('submit', function(e){
-            e.preventDefault();
-			var input = $("#input").val();
-            var resusr = '</center><div class = "panel-body"><p> ';
-            $("#async").append(resusr+" "+input+" </p></div>);
-            $.ajax({
-                type:'POST',
-                url:'profiles/Tobey.php',
-                dataType:'json',
-                data:{'message':input},
-                success:function(response){
-                    console.log(response);
-            var resbot='<div class="panel-body" ><p> ';
-             $("#async").append(resbot+" "+response.answer+" </p></div>");
-              $("#input").val('');
-        },
-        error: function(error){
-          console.log(error);
+    try {
+        $sql = 'SELECT * FROM secret_word';
+        $q = $conn->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $q->fetch();
+    } catch (PDOException $e) {
+        throw $e;
+    }
+    $secret_word = $data['secret_word'];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = $_POST['user-input'];
+      //  $data = preg_replace('/\s+/', '', $data);
+        $temp = explode(':', $data);
+        $temp2 = preg_replace('/\s+/', '', $temp[0]);
+        
+        if($temp2 === 'train'){
+            train($temp[1]);
+        }elseif($temp2 === 'aboutbot') {
+            aboutbot();
+        }else{
+            getAnswer($temp[0]);
         }
-             });
-        })
-    });
+    }
 
-</script>
+    function aboutbot() {
+        echo "<div id='result'> TundeBot v1.0</div>";
+    }
+    function train($input) {
+        $input = explode('#', $input);
+        $question = trim($input[0]);
+        $answer = trim($input[1]);
+        $password = trim($input[2]);
+        if($password == 'password') {
+            $sql = 'SELECT * FROM chatbot WHERE question = "'. $question .'" and answer = "'. $answer .'" LIMIT 1';
+            $q = $GLOBALS['conn']->query($sql);
+            $q->setFetchMode(PDO::FETCH_ASSOC);
+            $data = $q->fetch();
+
+            if(empty($data)) {
+                $training_data = array(':question' => $question,
+                    ':answer' => $answer);
+
+                $sql = 'INSERT INTO chatbot ( question, answer)
+              VALUES (
+                  :question,
+                  :answer
+              );';
+
+                try {
+                    $q = $GLOBALS['conn']->prepare($sql);
+                    if ($q->execute($training_data) == true) {
+                        echo "<div id='result'>Training Successful!</div>";
+                    };
+                } catch (PDOException $e) {
+                    throw $e;
+                }
+            }else{
+                echo "<div id='result'>I already understand this. Teach me something new!</div>";
+            }
+        }else {
+            echo "<div id='result'>Invalid Password, Try Again!</div>";
+
+        }
+    }
+
+    function getAnswer($input) {
+        $question = $input;
+        $sql = 'SELECT * FROM chatbot WHERE question = "'. $question . '"';
+        $q = $GLOBALS['conn']->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $q->fetchAll();
+        if(empty($data)){
+            echo "<div id='result'>Sorry, I do not know that command. You can train me simply by using the format - 'train: question # answer # password'</div>";
+        }else {
+            $rand_keys = array_rand($data);
+            echo "<div id='result'>". $data[$rand_keys]['answer'] ."</div>";
+        }
+    }
+    ?>
+
 </div>
 
 </body>
-</html>
+
+
+<script>
+    var outputArea = $("#chat-output");
+
+    $("#user-input-form").on("submit", function(e) {
+
+        e.preventDefault();
+
+        var message = $("#user-input").val();
+
+        outputArea.append(`<div class='bot-message'><div class='message'>${message}</div></div>`);
+
+
+        $.ajax({
+            url: 'profile.php?id=Tobey',
+            type: 'POST',
+            data:  'user-input=' + message,
+            success: function(response) {
+                var result = $($.parseHTML(response)).find("#result").text();
+                setTimeout(function() {
+                    outputArea.append("<div class='user-message'><div class='message'>" + result + "</div></div>");
+                    $('#chat-output').animate({
+                        scrollTop: $('#chat-output').get(0).scrollHeight
+                    }, 1500);
+                }, 250);
+            }
+        });
+
+
+        $("#user-input").val("");
+
+    });
+</script>
+
+ <?php
+
+global $conn;
+
+try {
+    $sql2 = 'SELECT * FROM interns_data WHERE username="melody"';
+    $q2 = $conn->query($sql2);
+    $q2->setFetchMode(PDO::FETCH_ASSOC);
+    $my_data = $q2->fetch();
+} catch (PDOException $e) {
+    throw $e;
+}
+?>
+
