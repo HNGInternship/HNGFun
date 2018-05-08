b1fe640197e5cb362d584df1d7b01a01c4513834
Kabiru Saka <sakakabiru@gmail.com>
2018-05-08 20:33:25 +0100

    Adding my profile page

--- profiles/SKA.php
+++ profiles/SKA.php
@@ -1,17 +1,5 @@
 <!DOCTYPE html>
 <?php
-
-	define ('DB_USER', "root");
-	define ('DB_PASSWORD', " ");
-	define ('DB_DATABASE', "hng_fun");
-	define ('DB_HOST', "localhost");
-
-	try {
-		$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-	} catch (PDOException $pe) {
-		die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-	}
-	// $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
 	
 	$sql = "SELECT * FROM secret_word LIMIT 1";
     $q = $conn->query($sql);
