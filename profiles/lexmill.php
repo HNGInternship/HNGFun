a355339c9bfa5eed978ecb11273b3850149d2bab
Leke-Ariyo <37749047+Leke-Ariyo@users.noreply.github.com>
2018-05-09 12:18:16 +0100

    Update lexmill.php

--- profiles/lexmill.php
+++ profiles/lexmill.php
@@ -44,6 +44,115 @@ function gettTime(){
     );
     return $joke[$random];
 }
+session_start();
+if (!isset($_SESSION["all"])){
+    $_SESSION["all"] = [];
+}if(!defined('DB_USER')){
+    require_once "../../config.php";
+    $servername = DB_HOST;
+    $username = DB_USER;
+    $password = DB_PASSWORD;
+    $dbname = DB_DATABASE;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }}
+global $conn;
+$solution = '';
+if (isset($_POST['restart'])){
+    session_destroy();
+}if (isset($_POST['button'])) {
+    if (isset ($_POST['input']) && $_POST['input'] !== "") {
+        $function = $_POST['input'];
+        $functionName = explode("(", $function);
+        if (function_exists($functionName[0])&&strpos($function,"(")&&strpos($function,")")) {
+            $functionVariable = explode(')',$functionName[1],2);
+            if (!strpos($functionVariable[0],",")){
+                $response = $functionName[0]($functionVariable[0]);
+            }else
+                $functionVariable1 = explode(',',$functionVariable[0],2);
+            $response = $functionName[0]($functionVariable1[0],$functionVariable1[1]);
+            // Dynamic call using variable value as function name.
+        }else{$asked_question_text = $_POST['input'];
+            $solution = askQuestion($asked_question_text) . "<br/>";
+            $_SESSION["all"][] = array($solution, $asked_question_text);
+        }
+    }
+}
+function askQuestion($input)
+{$input = strtolower($input);
+    $input = trim($input);
+    $action = "train:";
+    $time = "time";
+    global $conn;
+    $train = strpos($input,$action);
+    if ($input!==""||$input!==" ") {
+        if ($train === 0) {
+            $explode = explode(':', $input, 2);
+            if (isset($explode[1])) {
+                $explode2 = explode('#', $explode[1], 2);
+                if (isset($explode2[1])) {
+                    $explode3 = explode('#', $explode2[1], 2);
+                    if (isset($explode3[1])){
+                        if (  $explode3[1] == "password") {
+                            $query = $conn->query("SELECT question, answer FROM chatbot WHERE LOWER(question) ='" . $explode2[0] . "' and LOWER(answer) =  '" . $explode3[0] . "'");
+                            $row_cnt = $query->rowCount();
+                            if ($row_cnt > 0) {
+                                return "QUESTION ALREADY EXISTS ";
+                            } else
+                                $the_queried = $conn->query("INSERT INTO chatbot(question, answer) VALUES ('" . $explode2[0] . "', '" . $explode3[0] . "')");
+                            if ($the_queried) {
+                                $saved_message = "Saved " . $explode2[0] . " -> " . $explode3[0];
+                                return $saved_message;
+                            } else
+                                return "Please try again";
+                        } else
+                            return "Please enter the right password";
+                    }else
+                        return "Please enter the password after your answer";
+                } else
+                    return "The right format is train:yourquestion#youranswer#password";
+            } else
+                return "The right format is train:yourquestion#youranswer#password";
+        } else {
+            if (preg_match('/\baboutbout\b/',$input)) {
+                return "Adokiye v1.0";
+            } else if (preg_match("/\b($time)\b/",$input)) {
+                return gettTime();
+            } else if (preg_match('/\bhelp\b/',$input)) {
+                return "Enter train:yourquestion?#youranswer#password to add more questions to dummy me<br/>Click on restart to clear our conversation and start again<br/>";
+            }else if($input=="you are mad"||$input == "you're mad"){
+                return "YOUR FATHER";
+            }else if(preg_match("/\bquote\b/",$input)){
+                return getMyquote();
+            }else if(preg_match("/\bjoke\b/",$input)){
+                return getmyJoke();
+            }
+            else {
+                $input = $_POST['input'];
+                $question = strtolower($input);
+                $question = str_replace('?', '', $question);
+                $question = trim($question);
+                $query = "SELECT * FROM chatbot WHERE LOWER(question) like '$question'";
+                $result = $conn->query($query);
+                $row_cnt = $result->rowCount();
+                $records = $result->fetchAll(PDO::FETCH_ASSOC);
+                $rand = rand(0, $row_cnt - 1);
+                if ($row_cnt > 0) {
+                    return $records[$rand]['answer'];
+                } else
+                    return "Am sorry, this question wasn't found,Please ENTER TRAIN:QUESTION#ANSWER#password to make me smarter";
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
