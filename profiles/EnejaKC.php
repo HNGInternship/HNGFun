2cab7b62e6a3adc5c84b32d79e3de7db890f67c1
chingsley <chingsleychinonso@gmail.com>
2018-05-10 06:50:23 +0100

    Eneja KC commits with TB_help

--- profiles/EnejaKC.php
+++ profiles/EnejaKC.php
@@ -21,14 +21,12 @@
                 
             }
             .profile_background{
-                /*text-align: center;*/
                 background-color: rgb(0, 0, 0);
                 width: 800px;
                 height: 550px;
                 padding-top: 5px;
                 margin-left: 300px;
                 margin-top: 10px;
-                /*border-radius: 10px;*/
 	            box-shadow: 5px 50px 50px rgb(9, 15, 13);
                 text-align: center;
                 
@@ -59,14 +57,18 @@
             }
             div > figure > img{
                 border-radius: 150px;
-	            /*box-shadow: 5px 5px 10px rgb(9, 15, 13);*/
             }
             
         </style>
     </head>
     <body>
             <?php
-
+            $result = $conn->query("SELECT * from secret_word LIMIT 1");
+            $result = $result->fetch(PDO::FETCH_OBJ);
+            $secret_word = $result->secret_word;
+            $result2 = $conn->query("Select * from interns_data where username = 'EnejaKC'");
+            $user = $result2->fetch(PDO::FETCH_OBJ);
+/*
             // include('../../htconfig/config.php');
             $db = array(
                 'hostname'=> 'localhost',
@@ -83,27 +85,13 @@
              }
 
             if($dbSuccess) {
-                /*___________________________________________________________________
-                This block queries the interns_data table in the hng_fun database
-                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
-              //echo "<h1>SUCCESS! </h1>". " <h2>Congratulations, you have successfully connected to the database.</h2>";
                $get_my_data = "SELECT name, username, image_filename FROM interns_data";
                $my_data = mysql_query($get_my_data);
                $row = mysql_fetch_array($my_data, MYSQL_ASSOC);
-
-               /*_____________________________________________________________________
-                This block queries the sectret_word table in the hng_fun database
-                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
                $get_secret_word = "SELECT secret_word FROM secret_word";
                $secret_word = mysql_query($get_secret_word);
-               /*______________________________________________________________________________________________
-               This block is used to check if the secret_word query is successful (not needed for the project)
-               if($secret_word != "") {
-                   echo "<br /><br />secret_word QUERY - SUCCESSFUL!";
-               } else {
-                   echo "<br /><br />secret_word QUERY - FAILED";
-               }_______________________________________________________________________*/
              }
+*/
              
              ?>
         <div class="profile_background">
@@ -118,8 +106,8 @@
             <div class="line_division"></div>
 
             <div class="details">
-                    <h2>Username: <?php echo "<b>$row[username]</b></h2>"?>;
-                    <h2>Full Name: <?php echo "<b>$row[name]</b></h2>"?>;
+                    <h2>Username: EnejaKC</h2>;
+                    <h2>Full Name: Eneja Kingsley Chinonso</h2>;
                     <h2>Level: <b>Stage 3 </b></h2>
                     <h2>email: <a href="eneja.kc@gmail.com">eneja.kc@gmail.com</a></h2>
             </div>
