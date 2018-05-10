62500abcf62705264aa66ab5ca5609dc5e321340
codedman <obasiuche62@gmail.com>
2018-05-10 01:13:37 +0100

    ch

--- profiles/obate.php
+++ profiles/obate.php
@@ -4,21 +4,30 @@
     <script type="text/javascript" src="vendor/jquery/jquery.js"></script>
   	<script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
   	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
   	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <script type="text/javascript">
+  		$(document).ready(function() {
+  			$('#send').on("click", function(){
+  				var value = $('#question').val();
+  				$.ajax({
+  					url: 'answer.php',
+  					data: {'question': value},
+  					type: 'post',
+  					success: function(res){
+  						$(".chat_bot_conserv").append('<li style = "text-shadow: 1px 1px #333;">'+ res +'</li><br>') ;
+  					}
+  				});
+  			});
+  		});
+  </script>
 
   <?php
 
-  if(!defined('DB_USER')){
-        require_once "../../config.php";
-
-        try {
-            $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-        } catch (PDOException $e) {
-            die("Could not connect to the database " . DB_DATABASE . ": " . $e->getMessage());
-        }
-    }
+  if(!isset($_GET['id'])){
+     require '../db.php';
+   }else{
+      require 'db.php';
+   }
 
     try {
       $sql = "SELECT * FROM secret_word";
@@ -41,131 +50,20 @@
     $image_url = $intern_db_result['image_filename'];
   ?>
 
-  <?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  	$question = $_POST['input_text'];
-  	$question = preg_replace('([\s]+)', ' ', trim($question));
-  	$question = preg_replace("([?.])", "", $question);
-      if(preg_replace('([\s]+)', ' ', trim(strtolower($question))) === 'aboutbot'){
-        echo json_encode([
-          'status' => 1,
-          'answer' => "Hi dear! My name is obabot-"
-        ]);
-        return;
-      };
-      //Check if user want to train the bot or ask a normal question
-  	$check_for_train = stripos($question, "train:");
-      if($check_for_train === false){
-
-  	$question = preg_replace('([\s]+)', ' ', trim($question));
-  	$question = preg_replace("([?.])", "", $question);
-
-  	$question = $question;
-          $sql = 'SELECT * FROM chatbot WHERE question = "'. $question . '"';
-          $q = $GLOBALS['conn']->query($sql);
-          $q->setFetchMode(PDO::FETCH_ASSOC);
-          $data = $q->fetchAll();
-          if(empty($data)){
-              echo json_encode([
-          		'status' => 1,
-         			 'answer' =>  "your question cannot be answered! Please train me by typing-->  train: question #answer #password"
-       		 ]);
-            return;
-          }else {
-              $rand_keys = array_rand($data);
-              $answer = $data[$rand_keys]['answer'];
-              echo json_encode([
-          		'status' => 1,
-         			 'answer' => $answer,
-       		 ]);
-             return;
-          	};
-
-
-  	}else{
-  		//train the chatbot to be more smarter
-  	    $train_string  = preg_replace('([\s]+)', ' ', trim($question));
-  	    $train_string  = preg_replace("([?.])", "",  $train_string);
-  	    $train_string = substr( $train_string, 6);
-  	    $train_string = explode("#", $train_string);
-
-          $user_question = trim($train_string[0]);
-  	        if(count($train_string) == 1){
-  		        echo json_encode([
-  		          'status' => 1,
-  		          'answer' => "invalid training format. The correct format is-->  train: question #answer #password"
-  		        ]);
-  	        return;
-  	        };
-  	        $user_answer = trim($train_string[1]);
-  	        if(count($train_string) < 3){
-  		        echo json_encode([
-  		          'status' => 1,
-  		          'answer' => "Please enter training password to train me. The password is--> password"
-  		        ]);
-  	        return;
-  	        };
-
-  		    $user_password = trim($train_string[2]);
-  	        //verify if training password is correct
-  	        define('TRAINING_PASSWORD', 'password');
-  	        if($user_password !== TRAINING_PASSWORD){
-  		        echo json_encode([
-  		          'status' => 1,
-  		          'answer' => "password incorrect! Please enter the correct password which is-->  password "
-  		        ]);
-  	     	return;
-  	    	};
-  		    //check database if answer exist already
-  		    $user_answer = "$user_answer";
-  		    $sql = "select * from chatbot where answer like :user_answer";
-  		    $stmt = $conn->prepare($sql);
-  		    $stmt->bindParam(':user_answer', $user_answer);
-  		    $stmt->execute();
-  		    $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  		 	$rows = $stmt->fetchAll();
-  		    if(empty($rows)){
-  			    $sql = "insert into chatbot (question, answer) values (:question, :answer)";
-  			    $stmt = $conn->prepare($sql);
-  			    $stmt->bindParam(':question', $user_question);
-  			    $stmt->bindParam(':answer', $user_answer);
-  			    $stmt->execute();
-  			    $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-  			    echo json_encode([
-  			    	'status' => 1,
-  			        'answer' => " Thanks! for training me "
-  			      ]);
-  	     	return;
-
-  	     	}else{
-  	     		 echo json_encode([
-  			    	'status' => 1,
-  			        'answer' => "Sorry! Answer already exist. Try train me with a new question and a new answer."
-  			      ]);
-  			return;
-  	     	};
-  	    return;
-  	 	};
-
-  } else {
-  ?>
-
-
 
 
     <style>
 
-    .container {
-      padding: 30px;
-      width: 500px;
+    #container {
+      padding: 100px;
+      width: 800px;
       height: 400px;
       margin: 10px auto;
-      box-sizing: border-box;
-      box-shadow: -3px 3px 5px gray;
+      box-shadow: 4px 4px #a8b2c1;
+      position:relative;
       color: black;
       overflow-y: scroll;
-      background-color: #F3F3F3;
+      background-image:url('https://cdn.pixabay.com/photo/2018/02/18/20/29/computer-3163436_960_720.png');
       background-size:contain;
     }
     #controls {
@@ -178,7 +76,20 @@
       height: 120px;
       margin-bottom: 20px;
     }
-
+    textarea {
+      resize: none;
+      width: 400px;
+      float: left;
+      height: 50px;
+      font-family: arial-bold;
+      padding: 7px;
+      /*box-shadow: 4px 2px  10px black;*/
+      border: 0px;
+      font-size: 30px;
+      border-radius: 1px;
+      background-color: white;
+      color: black;
+    }
     #send {
       border: none;
       color: white;
@@ -191,41 +102,14 @@
       /*box-shadow: 4px 4px 2px #a8b2c1;*/
       border-radius: 10px;
     }
-
-    .message{
-			font-size: 15px;
-			width: 200px;
-			display: inline-block;
-          		padding: 15px;
-			margin: 7px;
-          		border-radius: 30px;
-            		line-height: 20px;
-		}
-		.bot_chat{
-			text-align: left;
-		}
-		.bot_chat .message{
-			background-color: #34495E;
-			color: white;
-			opacity: 1.9;
-			box-shadow: 5px 5px 8px gray;
-		}
-    #chat_showcase{
+    .chat_bot_conserv{
       list-style-type: none;
-      flex-direction: column;
+      display: block;
     }
-
-    .user_chat{
-			text-align: right;
-		}
-		.user_chat .message{
-			background-color: #E0E0E0;
-			color: black;
-			box-shadow: 3px 3px 5px gray;
-		}
     .card{
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-      max-width: 200px;
+      max-width: 300px;
+      margin: auto;
       text-align: center;
     }
 
@@ -277,7 +161,10 @@
   </head>
 
 
+  <!-- the php code that works with the Chatbot -->
+  	<?php
 
+  	 ?>
 
 
   <body>
@@ -304,52 +191,23 @@
         </div>
 
 <h1 style="text-align: center; color: black; padding-top: 20px;">My Chatbot</h1>
-    <div class="container">
-      <div id="chat_showcase">
-        <div class="bot_chat">
-          <div class="message">Hello! My name is obabot.<br>You can ask me questions and get answers.<br>Type <span style="color: #90CAF9;"><strong> Aboutbot</strong></span> to know more about me.
+    <div id="container">
+      <div class="chat_bot">
+        <ul class="chat_bot_conserv">
+          <div class="message">Hello! My name is Lolly.<br>You can ask me questions and get answers.<br>Type <span style="color: #FABF4B;"><strong> Aboutbot</strong></span> to know more about me.
           </div>
-          <div class="message">You can also train me to be smarter by typing; <br><span style="color: #90CAF9;"><strong>train: question #answer #password</strong></span></div>
-        </div>
+          <div class="message">You can also train me to be smarter by typing; <br><span style="color: #FABF4B;"><strong>train: question #answer #password</strong></span><br><strong>NOTE: </strong></span> I don't accept functions. Keep it simple!</div>
+        </ul>
       </div>
     </div>
     <div id="controls">
-      <div class="input-text-container" style="text-align: center;">
-        <form action="" method="post" id="chat-input-form">
-          <input type="text" name="input_text" id="input_text" required class="input_text" placeholder="Type your question here...">
-          <button type="submit" class="send_button" id="send">Send</button>
-        </form>
+      <div class="form-group" style="text-align: center;">
+        <input type="text" class="form-control" name="question" id="question" placeholder="Type Here.........">
       </div>
-      <script>
-          var chat_output = $("#chat_showcase");
-            $("#chat-input-form").on("submit", function(e) {
-                e.preventDefault();
-                var input_text = $("#input_text").val();
-                chat_output.append("<div class='user_chat'><div class='message'>"+input_text+"</div></div>");
-                chat_output.scrollTop(chat_output[0].scrollHeight);
-            //send question to server
-            $.ajax({
-              url: 'profiles/obate.php',
-              type: 'POST',
-              data: {input_text: input_text},
-              dataType: 'json',
-              success: (response) => {
-                    response.answer = response.answer.replace(/(?:\r\n|\r|\n)/g, '<br />');
-                    let response_answer = response.answer;
-                    chat_output.append("<div class='bot_chat'><div class='message'>" +response_answer+ "</div></div>");
-                    $('#chat_showcase').animate({scrollTop: $('#chat_showcase').get(0).scrollHeight}, 1100);
-              },
-              error: (error) => {
-                      alert('error occured')
-                  console.log(error);
-              }
+      <button id="send" style="float:right;" class="btn btn-lg btn-primary btn-hover" name="send">Send</button>
+      </div>
 
-            });
-            document.getElementById("chat-input-form").reset();
-          });
-      </script>
 
 
   </body>
 </html>
-<?php } ?>
