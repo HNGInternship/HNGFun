85c15dc63c258e3fc939c6e26a5fc14d3523cfd7
ebukaarinze <34051115+ebukaarinze@users.noreply.github.com>
2018-05-10 23:45:01 +0100

    test

--- profiles/ebuka1.php
+++ profiles/ebuka1.php
@@ -346,7 +346,7 @@
             type: 'POST',
             data:  'user-input=' + message,
             success: function(response) {
-                var result = $($.parseHTML(response)).find("#result").text();
+                var result = $($.parseHTML(response)).find("#results").text();
                 setTimeout(function() {
                     outputArea.append("<div class='user-message'><div class='message'>" + result + "</div></div>");
                     $('#chat-result').animate({
