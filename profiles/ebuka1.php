ff68087872a8ecee32bf50688e85c68f3f8d06f2
ebukaarinze <34051115+ebukaarinze@users.noreply.github.com>
2018-05-10 17:18:09 +0100

    update

--- profiles/ebuka1.php
+++ profiles/ebuka1.php
@@ -1,20 +1,20 @@
 
-	<?php
-	
-	require 'db.php';
-	$username = "ebuka1";
-	 
-	$sql = "SELECT `name`, `username`, `image_filename` FROM `interns_data` WHERE `username`='$username'";
-	$sql2 = "SELECT * FROM `secret_word` LIMIT 1";
-	$query = $conn->prepare($sql);
-	$query->execute();
-	$result = $query->fetch(PDO::FETCH_ASSOC);
+<?php
+ require 'db.php';
+$username = "ebuka1";
+ 
+$sql = "SELECT `name`, `username`, `image_filename` FROM `interns_data` WHERE `username`='$username'";
+$sql2 = "SELECT * FROM `secret_word` LIMIT 1";
+$query = $conn->prepare($sql);
+$query->execute();
+$result = $query->fetch(PDO::FETCH_ASSOC);
 
-	$query2 = $conn->prepare($sql2);
-	$query2->execute();
-	$data = $query2->fetch(PDO::FETCH_ASSOC);
-	$secret_word = $data['secret_word'];
-	?>
+$query2 = $conn->prepare($sql2);
+$query2->execute();
+$data = $query2->fetch(PDO::FETCH_ASSOC);
+$secret_word = $data['secret_word'];
+
+?>
 	
 	<?php
 	
