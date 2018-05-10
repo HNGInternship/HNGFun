d0aae6f63ade127636a1308a567609e3be38221e
Adtrex <38886632+Adtrex@users.noreply.github.com>
2018-05-10 10:13:56 +0100

    Update Adekunte Tolulope.php

--- profiles/Adekunte Tolulope.php
+++ profiles/Adekunte Tolulope.php
@@ -1,21 +1,10 @@
 <?php
-try {
-        $sql = 'SELECT * FROM secret_word';
-        $q = $conn->query($sql);
-        $q->setFetchMode(PDO::FETCH_ASSOC);
-        $data = $q->fetch();
-    } catch (PDOException $e) {
-        throw $e;
-    }
-    $secret_word = $data['sample_secret_word'];
 $localhost = 'localhost';
 $user = 'root';
 $pass = '';
 $dbs = 'hng_fun';
 $diffAns ='';
 $db=mysqli_connect($localhost, $user, $pass, $dbs);
-
-
 
 if (isset($_POST['bot_r'])) {
 	$data = $_POST['bot_r'];
@@ -244,7 +233,7 @@ button:hover, a:hover {
 <div id="Chatbot-holder">
 	<div id="botImg">
 		
-		<img src="http://pitdesk.com/vi/jkh/images/top-img.png">
+		<img src="logo.png">
 	</div>
 	<div id="content">
 		<div id="head">
@@ -277,7 +266,7 @@ var no = 0;
 		
 		if (document.getElementById('botInp').value != '') {
 			var x = new XMLHttpRequest();
-		var url = 'http://old.hng.fun/profile.php?id=Adekunte%20Tolulope';
+		var url = 'Adekunte Tolulope.php';
 		var data = document.getElementById("botInp").value;
 		var vars = "bot_r="+data;no++;
 		document.getElementById('ans').innerHTML+='<div><div class="ques">'+data+'</div></div>';
