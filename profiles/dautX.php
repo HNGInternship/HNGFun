e3d4c093110661f07fe2dc25f3e430b004306c54
Patsoks Patsokari <33361294+patrex@users.noreply.github.com>
2018-05-11 00:34:34 +0100

    Moved config.php

--- profiles/dautX.php
+++ profiles/dautX.php
@@ -2,8 +2,15 @@
 
 	session_start(); 
 
-    require '../db.php';	//require connection file
-    //require '../config.php';
+    if(!defined('DB_USER')){
+        require "../../config.php";     
+        try {
+            $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+        } catch (PDOException $pe) {
+            die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+        }
+    }
+
     $_SESSION['count'] = 0;     //set count so user can enter name only once per session
 
     if ( isset($_POST['message']) ) {
