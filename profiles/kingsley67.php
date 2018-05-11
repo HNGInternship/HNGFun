e1cca72ce12cb04036e34c8379f9140c93c59a73
Kingsley <kingsleyche67@yahoo.com>
2018-05-11 21:04:19 +0100

    update

--- profiles/kingsley67.php
+++ profiles/kingsley67.php
@@ -216,7 +216,7 @@ echo json_encode([
     .you{
         background-color:#E8EEEF;
         width:500px;
-        padding-left: 150px;
+        padding-left: 290px;
         border: 1px solid black;
        
         overflow-wrap: break-word;
