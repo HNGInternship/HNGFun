edbb1673dbb668a387d5a58beac6d76b6743cfd4
Amiable41 <uddyabia@gmail.com>
2018-05-11 17:15:11 +0100

    my bot

--- profiles/Uddy.php
+++ profiles/Uddy.php
@@ -19,7 +19,7 @@
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     
             //die('Hi');
-            $conn = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+            $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
             
             if(!$conn){
                 die('Unable to connect');
