efe77958ac53ac4ee680143e88c718f7f1a35314
Adtrex <38886632+Adtrex@users.noreply.github.com>
2018-05-11 17:01:33 +0100

    Update Adekunte Tolulope.php

--- profiles/Adekunte Tolulope.php
+++ profiles/Adekunte Tolulope.php
@@ -1,10 +1,18 @@
 <?php
-
-
+if (!defined('DB_USER'))
+	{
+	require "../../config.php";
+	}
+try
+	{
+	$conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
+	}
+catch(PDOException $pe)
+	{
+	die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+	}
+global $conn;
 $diffAns ='';
-
-
-
 if (isset($_POST['bot_adekunte'])) {
 	
 	$data = $_POST['bot_adekunte'];
