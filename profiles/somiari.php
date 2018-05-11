0b1a23e5fbd14bc9c4221d073e04a11f2a3fc52c
temipet <talk2oluwasarah@gmail.com>
2018-05-11 14:40:27 +0100

    Fix AJAX

--- profiles/somiari.php
+++ profiles/somiari.php
@@ -450,7 +450,7 @@
 			</section>
 
 			<form class="chat-box" id="ajax-contact" method="post" action="">
-				<span class="chat-box-header">Alan is not a bot</span>
+				<span class="chat-box-header">Alan is <del>not</del> a bot</span>
 				<div class="chat-msgs">
 					<p class="alan">Hello! My name is Alan, and I am <del>not</del> a bot.</p>
 					<p class="alan">I'm a fast learner. To teach me something, just type and send: train: question # answer # password</p>
