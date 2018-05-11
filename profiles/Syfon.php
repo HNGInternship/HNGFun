d2ac1ddda18cbf82bc80d4e123040513235cab73
syfon01 <syfonisaac@gmail.com>
2018-05-11 07:59:59 +0100

    chatbot

--- profiles/Syfon.php
+++ profiles/Syfon.php
@@ -5,7 +5,6 @@
 
 // }
 
-<<<<<<< HEAD
 // try
 // {
 // $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
@@ -15,14 +14,6 @@
 // {
 // die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
 // }
-=======
-// $result = $conn->query("Select * from secret_word");
-// $result = $result->fetch(PDO::FETCH_OBJ);
-// $secret_word = $result['secret_word'];
-
-// $result2 = $conn->query("Select * from interns_data where username = 'Syfon'");
-// $user = $result2->fetch(PDO::FETCH_OBJ);
->>>>>>> 04aca8b09c0ed45a68683dab137d091fe6ac4f10
 
 // global $conn;
 
@@ -36,7 +27,6 @@
 // ?>
 
 
-<<<<<<< HEAD
 
 <?php
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
@@ -51,7 +41,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     require '../../config.php';
     
   $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE );
-// $conn = new mysqli('localhost', 'root', '', 'hng_fun');
 
     
     if(!$conn){
@@ -62,7 +51,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $pos = strpos($question, 'train:');
 
     if($pos === false){
-        $sql = "SELECT answers FROM chatbot WHERE questions like '$question' ";
+        $sql = "SELECT answer FROM chatbot WHERE question like '$question' ";
         $query = $conn->query($sql);
         if($query){
             echo json_encode([
@@ -79,7 +68,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         if($data[2] == 'password'){
 
-            $sql = "INSERT INTO chatbot (questions, answers)
+            $sql = "INSERT INTO chatbot (question, answer)
             VALUES ('$data[0]', '$data[1]')";
 
 
@@ -106,7 +95,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     }
     
     echo json_encode([
-        'reply'=>  'Good to go'
+        'results'=>  'Good to go'
     ]);
     
 return ;
@@ -116,16 +105,6 @@ return ;
 
 
 ?>
-=======
-$result2 = $conn->query("Select * from interns_data where username = 'Syfon'");
-$user = $result2->fetch(PDO::FETCH_OBJ);
-
-
-?>
-
-
-
->>>>>>> 04aca8b09c0ed45a68683dab137d091fe6ac4f10
 <!doctype html>
 <html lang="en">
   <head>
@@ -135,32 +114,33 @@ $user = $result2->fetch(PDO::FETCH_OBJ);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="c:\Users\Jnr\Desktop\bootstrap4beta\css\bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    
     <title>My portfolio</title>
     <style>
 .card-img-top{
-    height:38rem;
-}
+    height:30rem;
 
+}
 .card-body{
     background-color: rgb(1, 1, 41);
 }
 .rounded-circle{
     border-radius:50%;
-    height: 300px;
-    width:300px;
+    height: 20rem;
+    width:20rem;
     position: absolute;
     top:40px;
-    left: 40%;
+    left: 30em;
 }
 .fa {
-    padding: 50px;
-    font-size: 40px;
-    width: 50px;
+    padding:20px ;
+    font-size: 50px;
+    /* width: 50px; */
     text-decoration: none;
-<<<<<<< HEAD
 }
 #chatbot{
     background-color:rgb(1, 1, 53);
@@ -171,7 +151,7 @@ $user = $result2->fetch(PDO::FETCH_OBJ);
 #chat-area{
     height: 50vh;
     background-color:white;
-    overflow-y:auto;
+    /* overflow-y:auto; */
 }
 .details, h1{
     color: white;
@@ -185,35 +165,30 @@ $user = $result2->fetch(PDO::FETCH_OBJ);
     background-color: rgba(14, 12, 12, 0.89);
 }
 
-=======
- 
-}
->>>>>>> 04aca8b09c0ed45a68683dab137d091fe6ac4f10
 
-    </style>
+</style>
+
     
 </head>
   <body>
-    <div id="container">
-                <div class="card" style="width: ; height: 44rem;">
+
+
+    <section id="profile">
+        <div id="container">
                         <img class="card-img-top" src="https://res.cloudinary.com/syfon/image/upload/v1523631972/pic.jpg" alt="Card image cap">
                             <img src="https://res.cloudinary.com/syfon/image/upload/v1523630065/syfon.jpg" class="rounded-circle">
-                        
-                          <center> 
-                            <h5>Sifon Isaac</h5><br>
+                        </div>     
+    <section id="About" >                   
+        <div class="container">
+            <div class="content">
+                <div class="row">
+                    <div class="col-md-6">     
+                        <div class="details">     
+                            <h3>Sifon Isaac</h3><br>
                             <p>Sifon Isaac is a Nigerian from Akwa Ibom State.<br> A web developer and an intern of the HNG program.<br> A graduate of Biochemistry but has a burning desire toward web and digital technology  </p> 
-                        </center>
-                            <div class="card-body">
-                                <div class="row">
-                                  <a href="https://www.facebook.com/sifon.isaac.3" class="fa fa-facebook"></a>
-                                    <a href="https://twitter.com/syfonisaac" class="fa fa-twitter"></a>
-                                    <a href="https://www.linkedin.com/in/sifon-isaac-5a107a79/" class="fa fa-linkedin-square"></a>
-                                  <a href="https://github.com/Syfon01" class="fa fa-github"></a>
-                                  <a href="" class="fa fa-slack"></a>
-                                    
-                              </div>
+                           
                     </div>
-<<<<<<< HEAD
+                    </div>
     <div class="col-md-6">
          <div id="chatbot">
             <h1>Syfon's Bot</h1>              
@@ -302,6 +277,3 @@ $('#chatbot').animate({
 </script>
 
 
-=======
-    </div>
->>>>>>> 04aca8b09c0ed45a68683dab137d091fe6ac4f10
