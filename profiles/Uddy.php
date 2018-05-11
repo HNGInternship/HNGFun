fe00004174ecc3197b01369b106ac94051c1a3d6
Amiable41 <uddyabia@gmail.com>
2018-05-11 17:01:04 +0100

    Uddychatbot

--- profiles/Uddy.php
+++ profiles/Uddy.php
@@ -211,7 +211,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 
                 } 
             };
-            xhttp.open("POST", "Uddy.php", true);
+            xhttp.open("POST", "/profiles/Uddy.php", true);
             xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             xhttp.send("message="+message.value);
         }
