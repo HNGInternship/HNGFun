15809eaf863eaf9ccd3fc32f9d77af777ff25c60
jasmineblinks <36607778+jasmineblinks@users.noreply.github.com>
2018-05-11 20:23:27 -0700

    Update puenehfaith.php

--- profiles/puenehfaith.php
+++ profiles/puenehfaith.php
@@ -102,14 +102,12 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>pueneh</title><link rel="stylesheet" type="text/css" href="https://static.oracle.com/cdn/jet/v4.1.0/default/css/alta/oj-alta-min.css">
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <title>pueneh</title><link rel="stylesheet" type="text/css" href="https://static.oracle.com/cdn/jet/v4.1.0/default/css/alta/oj-alta-min.css">
        <script type="text/javascript" src="https://static.oracle.com/cdn/jet/v4.1.0/3rdparty/require/require.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"> </script>
       <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"> </script>
       <script src="../js/jquery.min.js"></script>
-      <script src="../js/bootstrap.min.js"></script>
-      
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="../js/bootstrap.min.js"></script>   
         <style type="text/css">       
  body{
   padding-top: 60px;
