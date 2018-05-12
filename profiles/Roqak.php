bc1581d93c375f0898a7f4ff87fa73d60fb982aa
roqak <roqak1@gmail.com>
2018-05-11 20:02:18 -1100

    a

--- profiles/Roqak.php
+++ profiles/Roqak.php
@@ -201,7 +201,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     height: 90%;
     width: 400px;
     float: right;
-    margin-top: 20%;
+    margin-top: 10%;
     margin-left: 5%;
   }
   .padedd{
