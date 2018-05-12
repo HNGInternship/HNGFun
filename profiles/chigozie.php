0e9fd1e200c0ddb54f56f1f71610c8ab8c882d50
Chigozie Ekwonu <chygoz2@users.noreply.github.com>
2018-05-12 13:01:09 +0100

    Update chigozie.php

--- profiles/chigozie.php
+++ profiles/chigozie.php
@@ -634,7 +634,7 @@
     </div>
 </div>
 
-<script src="../vendor/jquery/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
 </body>
 <script>
