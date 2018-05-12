50901a998a191aaeb976bad3c41a30ad389cf811
nenedear <nnenedeangel@gmail.com>
2018-05-12 13:47:52 +0100

    emem bot added

--- profiles/nene.php
+++ profiles/nene.php
@@ -179,7 +179,7 @@
             
             }
         };
-        xhttp.open("POST", "/profiles/nene.php", true);
+        xhttp.open("POST",  "/profiles/nene.php", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send("message="+message.value);
     }
