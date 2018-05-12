a9445999f7bad0936ae1c660458ddd8df12ef102
Adtrex <38886632+Adtrex@users.noreply.github.com>
2018-05-12 20:25:11 +0100

    Update Adekunte Tolulope.php

--- profiles/Adekunte Tolulope.php
+++ profiles/Adekunte Tolulope.php
@@ -307,7 +307,7 @@ var no = 0;
 		
 		if (document.getElementById('botInp').value != '') {
 			var x = new XMLHttpRequest();
-		var url = 'profile?id=Adekunte Tolulope';
+		var url = 'profiles/Adekunte Tolulope.php';
 		var data = document.getElementById("botInp").value;
 		var vars = "bot_adekunte="+data;no++;
 		document.getElementById('ans').innerHTML+='<div><div class="ques">'+data+'</div></div>';
