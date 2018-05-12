e65ce2e73d32bb7db91e2c08a171c521f0379608
iam-dharmy <soyombooluwadamilola@gmail.com>
2018-05-12 10:56:54 +0100

    edited my profile

--- profiles/iam__dharmy.php
+++ profiles/iam__dharmy.php
@@ -167,6 +167,10 @@
 <head>
 	<link href="https://static.oracle.com/cdn/jet/v4.0.0/default/css/alta/oj-alta-min.css" rel="stylesheet" type="text/css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <style type="text/css">
       body {
 			
