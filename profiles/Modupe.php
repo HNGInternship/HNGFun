c881eb4d5fc22f0fcaa5a93ed9e0f4e06603ace9
Molyktech <36078695+Molyktech@users.noreply.github.com>
2018-05-13 05:29:36 -0700

    Update Modupe.php

--- profiles/Modupe.php
+++ profiles/Modupe.php
@@ -50,7 +50,7 @@ $result = $stmt->fetch(PDO::FETCH_ASSOC);
             overflow: hidden;
             border-radius: 10px;
             box-shadow: 0 20px 20px rgba(0,0,0,0.2);
-            display: none;
+            
 
         }
         .myprofile{
