cc989fcbd886f6ccd22f8b8b4d805863903e3011
Oluwapelumi Olaoye <Ifeoluwakita@bitbucket.org>
2018-05-13 14:41:18 +0100

    Finalized work on my bot

--- answers.php
+++ answers.php
@@ -1059,6 +1059,12 @@ function strrevpos($instr, $needle)
 
             }
 
+            function getLocationAPI() {
+                $ip = gethostbyname(gethostname());
+                $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+                return $query;
+            }
+
 
 
     function currencyConverter($from_currency, $to_currency, $amount) {
