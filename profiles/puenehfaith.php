91bd4067c4cabe1a2552d5ddbf5cb28339ec8fe8
jasmineblinks <36607778+jasmineblinks@users.noreply.github.com>
2018-05-15 21:37:47 -0700

    Update puenehfaith.php

--- profiles/puenehfaith.php
+++ profiles/puenehfaith.php
@@ -102,7 +102,8 @@
     <head>
         <meta charset="utf-8">
         <title>pueneh</title>
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link rel="stylesheet" type="text/css" href="../vendor/bootstrap/3.3.4/css/bootstrap.css">
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/3.3.4/css/bootstrap.css">
        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 
        <link rel="stylesheet" type="text/css" href="https://static.oracle.com/cdn/jet/v4.1.0/default/css/alta/oj-alta-min.css">
