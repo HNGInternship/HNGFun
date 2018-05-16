504d9aa715bae8b465fb1756171f4a375ee5f993
dhamie <dhamie.soyemi@gmail.com>
2018-05-16 18:58:42 +0100

    commit

--- profiles/Damilola.php
+++ profiles/Damilola.php
@@ -368,7 +368,7 @@ h5{
                     return false;
                     } else{
                         $.ajax({
-                        url: 'profiles/Damilola.php',
+                        url: '/profiles/Damilola.php',
                         dataType: "text",
                         method: 'POST',
                         cache: 'false',
