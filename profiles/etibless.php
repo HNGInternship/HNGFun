7545fbe871fb9537b08531fcb0ede58f0478ac01
Etibless <etiblessbiz@gmail.com>
2018-05-17 00:27:48 +0100

    my bot updated

--- profiles/etibless.php
+++ profiles/etibless.php
@@ -1,77 +1,88 @@
 <?php
+// require 'conn.php';
+
+// try{
+//    $sql = 'SELECT * FROM secret_word';
+//    $q = $conn->query($sql);
+//    $q->setFetchMode(PDO::FETCH_ASSOC);
+//    $data = $q->fetch();
+//    $secret_word= $data['secret_word'];
+// } catch (PDOException $e){
+//        throw $e;
+//    }
+
+
+// $result2 = $conn->query("Select * from interns_data where username = 'Uddy'");
+// $user = $result2->fetch(PDO::FETCH_OBJ);
+
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        $data = preg_replace("([?.!])", "", $data);
-        $data = preg_replace("(['])", "\'", $data);
-        return $data;
-    }
-    require '../../config.php';
-    
-  $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE );
-
-    
-    if(!$conn){
-        die('Unable to connect');
-    }
-    $question = $_POST['message'];
-
-    $pos = strpos($question, 'train:');
-
-    if($pos === false){
-        $sql = "SELECT answer FROM chatbot WHERE question like '$question' ";
-        $query = $conn->query($sql);
-        if($query){
-            echo json_encode([
+            require '../../config.php';
+            //die('Hi');
+            $conn = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+            
+            if(!$conn){
+                echo json_encode([
                 'results'=> $query->fetch_all()
-            ]);
-            return;
-        }
-    }else{
-        $trainer = substr($question,6 );
-        $data = explode('#', $trainer);
-        $data[0] = trim($data[0]);
-        $data[1] = trim($data[1]);
-        $data[2] = trim($data[2]);
-
-        if($data[2] == 'password'){
-
-            $sql = "INSERT INTO chatbot (question, answer)
-            VALUES ('$data[0]', '$data[1]')";
-
-
-            $query = $conn->query($sql);
-            if($query){
-                echo json_encode([
-                    'results'=> 'Successfully trained'
-                ]);
-                return;
-            }else{
-                echo json_encode([
-                    'results'=> 'Training error'
                 ]);
                 return;
             }
+            $question = $_POST['message'];
+            $pos = strpos($question, 'train:');
+    
+            if($pos === false){
+                $sql = "SELECT answer FROM chatbot WHERE question like '$question' ";
+                $query = $conn->query($sql);
+                if($query){
+                    echo json_encode([
+                        'results'=> $query->fetch_all()
+                    ]);
+                    return;
+                }
+            }else{
+                $trainer = substr($question,6 );
+                $data = explode('#', $trainer);
+                $data[0] = trim($data[0]);
+                $data[1] = trim($data[1]);
+                $data[2] = trim($data[2]);
+    
+                if($data[2] == 'password'){
+    
+                    $sql = "INSERT INTO chatbot (question, answer)
+                    VALUES ('$data[0]', '$data[1]')";
+    
+    
+                    $query = $conn->query($sql);
+                    if($query){
+                        echo json_encode([
+                            'results'=> 'Trained Successfully'
+                        ]);
+                        return;
+                    }else{
+                        echo json_encode([
+                            'results'=> 'Error training'
+                        ]);
+                        return;
+                    }
+                    
+                }else{
+                    echo json_encode([
+                        'results'=> 'Wrong Password'
+                    ]);
+                    return;
+                }
+                
+            }
             
-        }else{
             echo json_encode([
-                'results'=> 'Wrong Password'
+                'results'=>  'working'
             ]);
-            return;
+            
+        return ;
+        }else{
+            //echo 'HI';
+            //return;
         }
-        
-    }
-    
-    echo json_encode([
-        'results'=>  'Good to go'
-    ]);
-    
-return ;
-
-}
-
 
 
 ?>
