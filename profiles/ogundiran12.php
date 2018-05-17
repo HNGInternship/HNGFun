1dfe6b1f40bc342718e4b0d05ad664be5390e983
Alameen688 <ogundiran12@gmail.com>
2018-05-17 21:37:38 +0100

    adjust code

--- profiles/ogundiran12.php
+++ profiles/ogundiran12.php
@@ -3,6 +3,19 @@
    function makeSafe($data){
     return htmlspecialchars(stripslashes(trim($data)));
     }
+
+    require "../../config.php"; 
+    
+    try {
+    
+    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+    
+    } catch (PDOException $pe) {
+    
+    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+    
+    }
+    
    if(isset($_GET['answer'])){
 		
 		$question = makeSafe($_GET['question']);
