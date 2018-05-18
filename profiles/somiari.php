df8e449ff0f6009687af10b17f08c3bc7387ab06
Somiari Lucky <somiarilucky@gmail.com>
2018-05-18 11:09:02 +0100

    Update somiari.php

--- profiles/somiari.php
+++ profiles/somiari.php
@@ -219,7 +219,7 @@
 
 			.contained {
 				margin: 0 auto;
-				height: 100%;
+				height: auto !important;
 				width: 95%;
 				max-width: 2000px;
 				display: flex;
@@ -510,19 +510,6 @@
 					</button>
 				</div>
 			</form>
-
-			<footer class=".footer">
-				<?php
-				date_default_timezone_set('Africa/Lagos');
-			?>
-				<span class="date">
-					<?php echo date("D. M d, Y"); ?>
-				</span>
-				<i class="icon fa fa-fw fa-clock-o"></i>
-				<span class="time">
-					<?php echo date("h:i a"); ?>
-				</span>
-			</footer>
 
 		</div>
 		<script src="vendor/jquery/jquery.min.js"></script>
