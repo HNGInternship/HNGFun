78cd6dd7405926485a47dc60adfbfe97c1e380e4
Leke-Ariyo <37749047+Leke-Ariyo@users.noreply.github.com>
2018-05-18 21:11:44 +0100

    Update listing.php

--- listing.php
+++ listing.php
@@ -46,7 +46,7 @@ $data = $q->fetchAll();
                     <hr>
                     <span class="align-middle">
                     <a href="profile.php?id=<?=$list['username']?>">
-                       <button class="btn btn-success"><object align="center">View Profile</object></button>
+                       <button class="btn btn-success">View Profile</button>
                     </a>
                   </span>
                 </div>
