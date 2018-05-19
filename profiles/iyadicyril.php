6551c5d63809778668f3712ae003ef3ff438d940
see-why <iyadicy@gmail.com>
2018-05-19 23:59:50 +0100

    improved

--- profiles/iyadicyril.php
+++ profiles/iyadicyril.php
@@ -744,8 +744,10 @@ hr{
 
 	    		if (position == 'sent') {	    			
 	    			div.classList = position + "-message";
+					$(".message-area").scrollTop($(".message-area")[0].scrollHeight);
 	    		}else if (position == 'received'){
 	    			div.classList = position + "-message text-left"
+					$(".message-area").scrollTop($(".message-area")[0].scrollHeight);
 	    		}
 	    		
 	    		par.classList = "message " + position;
