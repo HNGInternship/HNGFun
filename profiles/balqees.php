5a25523b8aa619db5dd7afcd2ac452c04c8c0fbc
alabi olanike Temitayo <olanikealabi@rocketmail.com>
2018-05-23 02:46:36 +0100

    Add files via upload

--- profiles/balqees.php
+++ profiles/balqees.php
@@ -1,5 +1,10 @@
 
-<!-- head here  -->
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,6 +35,28 @@
       <link href="css/learn.css" rel="stylesheet">
 <!--    <link href="css/carousel.css" rel="stylesheet">-->
       <link href="css/landing-page.min.css" rel="stylesheet">
+
+   <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="stylesheet" href="css/custom.css" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style2.css" rel="stylesheet">
+    <link href="css/style1.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- <link href="css/custom.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/signout.css">
+    <link href="css/landing-page.min.css" rel="stylesheet">
+    <link href="css/shield-invite.css" rel="stylesheet">
+    <link href="css/404.css" rel="stylesheet">
+    <link href="css/contact.css" rel="stylesheet">
+    <!-- <link href="css/carousel.css" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Qwigley" rel="stylesheet">
+    <!-- for the dropdown in particular -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
 
 
   </head>
@@ -84,14 +111,12 @@
   border-radius: 50%;
   font-family: serif;
   font-weight: bolder;
-  opacity: 0.2px;
-  filter: alpha;
-
+  
   }
   img{
     width: 10rem;
     border-radius: 30%;
-    margin-left: 100px;
+    margin-left: 300px;
 
   }
 </style>
@@ -109,7 +134,7 @@
       <img src="https://res.cloudinary.com/dycw8sy0/image/upload/v1524458808/26285103.jpg" alt="ima">
       <h1>ALABI OLANIKE BALQEES</h1>
       <h2>@balqees</h2>
-     <p class="titler"> Writer, Web developer, Design Enthusiast</p>
+     <p class="titler"> Web developer, Design Enthusiast</p>
       <p>Lagos,Nigeria</p>
      <div style="margin: 24px 0;">
       
@@ -127,10 +152,217 @@
    </div>
 
 
+
+
+
+<style type="text/css">
+  
+  
+
+      body {
+      
+      background-size: cover;
+    }
+    p{ color:black
+    
+    }
+    h1{ color: blue
+    }
+    h3{ color: blue
+    }
+    h5{ color: white
+    
+    }
+    .container{
+            width: 40%;
+            min-height: 100%
+        }
+        .body0 {
+            height: 100%;
+        }
+
+        span {
+            display: inline-block;
+            vertical-align: middle;
+            line-height: normal;
+        }
+    
+    .chat-frame {
+      border-color: #cccccc;
+      color: #333333;
+      background-color: #ffffff;
+      height: 350px;
+     margin-left: 800px;
+      margin-bottom: 50px;
+      border-radius: 10px;
+    }
+
+    .chat-messages {
+      background-color: #f0b300;
+      padding: 5px;
+      height: 300px;
+      overflow-y: auto;
+      margin-left: 15px;
+      margin-right: 15px;
+      border-radius: 6px;
+      
+    }
+
+    .single-message {
+      margin-bottom: 5px; 
+      border-radius: 5px;
+      min-height: 30px;
+      
+    }
+
+    .single-message-bg {
+      background-color: #D35400;
+      
+      
+    }
+
+    .single-message-bg2 {
+      background-color: #95A5A6;
+      
+    }
+
+    input[name=question] {
+      height: 50px;
+    }
+
+    button[type=submit] {
+      height: 50px;
+      background-color: darkblue;
+      color: black
+    }
+
+    .circle {
+      width: 30%;
+      margin-left: 20%;
+      border-radius: 50%;
+    }
+    .f-icon {
+      font-size: 40px;
+    }
+   
+
+</style>
+
+
+
+<!--  -->
+
+
+    <div class="col-md-4 offset-md-1 chat-frame">
+            <div class="row chat-messages" id="chat-messages">
+                <div class="col-md-12" id="message-frame">
+                    <div class="row single-message">
+                        <div class="col-md-12 single-message-bg">
+                           <div class="style1"><h5>Talk to  <span style="font-weight: bold">iBot</span></h5></div> 
+                        </div>
+                    </div>
+                    <div class="row single-message">
+                        <div class="col-md-12 single-message-bg">
+                            <div class="style2"><h5>Ask me!</h5></div>
+                        </div>
+                    </div>
+                    <div class="row single-message">
+                        <div class="col-md-12 single-message-bg">
+                            
+                            <div class="style3"><h5>You Can train me, just type <br/><b>train: question #answer #password</b><h5></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <div class="row">
+                <form class="form-inline col-md-12 col-sm-12" id="question-form">
+                    <div class="col-md-12 col-sm-12 col-12">
+                        <input class="form-control w-100" type="text" name="question" placeholder="Your question..." />
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-12" style="margin-top: 20px">
+                        <button type="submit" class="btn btn-info w-100" >Enter</button>
+                    </div>
+                </form> 
+            </div>
+        </div>
+    </div>
+
+
+<body class = 'profile'>
+
+
+
 </body>
 
 
 
+<!--<script src="../vendor/jquery/jquery.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>-->
+<!--
+    <link href="https://static.oracle.com/cdn/jet/v4.0.0/default/css/alta/oj-alta-min.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>-->
+<script src="https://code.jquery.com/jquery-3.0.0.js" integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo=" crossorigin="anonymous"></script>
+
+
+
+
+
+
+<script>
+    
+    $(document).ready(function(){
+        var questionForm = $('#question-form');
+        questionForm.submit(function(e){
+            e.preventDefault();
+            var questionBox = $('input[name=question]');
+            var question = questionBox.val();
+            
+            //display question in the message frame as a chat entry
+            var messageFrame = $('#message-frame');
+            var chatToBeDisplayed = '<div class="row single-message">'+
+                        '<div class="col-md-12 offset-md-2 single-message-bg2">'+
+                            '<h5>'+question+'</h5>'+
+                        '</div>'+
+                    '</div>';
+            
+            messageFrame.html(messageFrame.html()+chatToBeDisplayed);
+            $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+            //send question to server
+            $.ajax({
+                url: "/profiles/balqees.php",
+                type: "post",
+                data: {question: question},
+                dataType: "json",
+                success: function(response){
+                    if(response.status == 1){
+                        var chatToBeDisplayed = '<div class="row single-message">'+
+                                    '<div class="col-md-12 single-message-bg">'+
+                                        '<h5>'+response.answer+'</h5>'+
+                                    '</div>'+
+                                '</div>';
+                        messageFrame.html(messageFrame.html()+chatToBeDisplayed);
+                        questionBox.val("");    
+                        $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+                    }else if(response.status == 0){
+                        var chatToBeDisplayed = '<div class="row single-message">'+
+                                    '<div class="col-md-12 single-message-bg">'+
+                                        '<h5>'+response.answer+'</h5>'+
+                                    '</div>'+
+                                '</div>';
+                        messageFrame.html(messageFrame.html()+chatToBeDisplayed);
+                        $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+                    }
+                },
+                error: function(error){
+                    console.log(error);
+                }
+            })
+        });
+    });
+</script>
 
 
 
@@ -139,9 +371,17 @@
 
 
 
+ <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js/jquery.form.min.js"></script>
+    <script type="text/javascript" src="js/mail.js"></script>
+    <!--script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script-->
+
+    <!-- Custom scripts for this template -->
+    <script src="js/hng.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
 </body>
 </html>
-
-
