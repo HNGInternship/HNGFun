bee25edaa886144158a7d792bf39ee94b919acab
Rythae <auxano42@gmail.com>
2018-05-24 08:40:14 +0100

    Update Rita12.php

--- profiles/Rita12.php
+++ profiles/Rita12.php
@@ -1,7 +1,7 @@
 <?php
 
 	if(!defined('DB_USER')){
-	  require_once __DIR__."/../../config.php";		
+	  require "../config.php";		
 	  try {
 	      $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 	  } catch (PDOException $pe) {
